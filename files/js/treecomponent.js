94453914de5dad441b0eb53147bd60971809d2e9
Tomas Machalek <tomas.machalek@gmail.com>
2012-08-14 14:48:43 +0200

    Druha verze tree komponenty - nyni umi naparsovat specificky format hodnot v selectboxu (/folder1/folderA/.../leaf) a vytvorit z nich html odsazeny seznam.

--- files/js/treecomponent.js
+++ files/js/treecomponent.js
@@ -1,11 +1,11 @@
 (function (context) {
     'use strict';
 
-    var createTreeComponent = function (rootUlId, replaceId) {
-        console.log('id: ' + replaceId);
+    var createTreeComponent = function (rootUlId, selectBoxId) {
+        console.log('id: ' + selectBoxId);
         var treeComponent = {
 
-            subtreeMap : {},
+            hiddenInput : null,
 
             findSubtree : function (elm) {
                 var i;
@@ -61,12 +61,74 @@
                         treeComponent.switchSubtree(subtree, expSymbolId);
                     }
                 });
+            },
+
+            buildRootUl : function () {
+                var ul = Element.extend(document.createElement('ul'));
+            },
+
+            findUlPath : function (items, rootElm) {
+                var srch = items.shift();
+                var foundElm, newLi, newUl, newLink;
+
+                rootElm.childElements().each(function (item) {
+                    if (item.readAttribute('class') == srch) {
+                        foundElm = item;
+                        return;
+                    }
+                });
+                if (foundElm === undefined) {
+                    newLi = Element.extend(document.createElement('li'));
+                    newLi.writeAttribute('class', srch);
+                    rootElm.insert(newLi);
+
+                    if (items.length > 0) {
+                        newUl = Element.extend(document.createElement('ul'));
+                        newLi.insert(srch);
+                        newLi.insert(newUl);
+                        treeComponent.findUlPath(items, newUl);
+
+                    } else {
+                        newLink = Element.extend(document.createElement('a'));
+                        newLink.writeAttribute('href', '#');
+                        newLink.insert(srch);
+                        newLink.observe('click', function (event) {
+                            treeComponent.hiddenInput.setValue(srch);
+                        });
+                        newLi.insert(newLink);
+                    }
+
+                } else {
+                    treeComponent.findUlPath(items, foundElm.firstDescendant());
+                }
+            },
+
+            parseSelectOptions : function () {
+                var i, j, splitPath;
+                var rootUl = Element.extend(document.createElement('ul'));
+                $(rootUlId).insert(rootUl);
+                $(selectBoxId).childElements('option').each(function (item) {
+                    var path = item.readAttribute('value');
+                    if (path.indexOf('/') === 0) {
+                        path = path.substring(1);
+                    }
+                    splitPath = path.split('/');
+                    treeComponent.findUlPath(splitPath, rootUl);
+                });
             }
         };
+
+        var inputName = $(selectBoxId).readAttribute('name');
+        treeComponent.hiddenInput = Element.extend(document.createElement('input'));
+        treeComponent.hiddenInput.writeAttribute('type', 'hidden');
+        treeComponent.hiddenInput.writeAttribute('name', inputName);
+        // '<input id="' + inputName +'-id" type="hidden" name="' + inputName + '" />'
+
+        treeComponent.parseSelectOptions();
         treeComponent.init(rootUlId);
-        if (replaceId !== undefined) {
-            Element.replace($(replaceId), $(rootUlId));
-        }
+
+        Element.replace($(selectBoxId), $(rootUlId));
+        $(rootUlId).insert({ before : treeComponent.hiddenInput });
         return treeComponent;
     };
 
