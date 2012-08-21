88416af8f8c5d057bdf09e5e48e63dabd585f745
Tomas Machalek <tomas.machalek@gmail.com>
2012-08-21 16:23:14 +0200

    Aktualizace deployment skriptu - pridana minifikace JS

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,3 +5,15 @@ do
     cheetah compile --odir cmpltmpl --idir templates $(basename $f)
 done
 
+DEFAULT_YUICOMP_PATH=/usr/local/lib/YUI/yuicompressor.jar
+read -p "Where is your yui compressor jar located? [$DEFAULT_YUICOMP_PATH]:" yuiloc
+if [ -z $yuiloc ]; then
+  yuiloc=$DEFAULT_YUICOMP_PATH
+fi
+
+for f in `find . -name "*.js"`; do
+  echo $f
+  if echo $f | grep -Eqv '/[^/]+min[^/]+$'; then
+    java -jar $yuiloc $f -o $f
+  fi
+done
