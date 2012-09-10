b9bc23e13818f0516bda6f34bdd3ca70d466a790
Tomas Machalek <tomas.machalek@gmail.com>
2012-09-10 10:36:59 +0200

    Pridan Cheetah filtr k formatovani ciselnych retezcu

---
+++ lib/templating/filters.py
@@ -0,0 +1,21 @@
+"""
+Custom Cheetah filters for the Bonito 2 interface
+"""
+import locale
+from Cheetah.Filters import Filter
+
+class IntegerFormatter(Filter):
+    """
+    Formats integer numbers according to the locales currently set
+    """
+    def filter(self, val, **kw):
+        return locale.format('%d', val, True)
+
+
+class FloatFormatter(Filter):
+    """
+    Formats float numbers according to the locales currently set
+    """
+
+    def filter(self, val, **kw):
+        return locale.format('01.2f%', val, True)
