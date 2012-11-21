c98e1d732bc31dd4a9ca1b3b570600de5cad3beb
Tomas Machalek <tomas.machalek@gmail.com>
2012-11-21 10:57:47 +0100

    Upraven skript deploy.sh tak, aby bylo mozno predat cestu k JS/CSS minifieru jako parametr z prikazove radky

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,9 +6,14 @@ do
 done
 
 DEFAULT_YUICOMP_PATH=/usr/local/lib/YUI/yuicompressor.jar
-read -p "Where is your yui compressor jar located? [$DEFAULT_YUICOMP_PATH]:" yuiloc
-if [ -z $yuiloc ]; then
-  yuiloc=$DEFAULT_YUICOMP_PATH
+
+if [ -z $1 ]; then
+  read -p "Where is your yui compressor jar located? [$DEFAULT_YUICOMP_PATH]:" yuiloc
+  if [ -z $yuiloc ]; then
+    yuiloc=$DEFAULT_YUICOMP_PATH
+  fi
+else
+  yuiloc=$1
 fi
 
 for f in `find . -name "*.js"`; do
