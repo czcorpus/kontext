4f3a358f3f983445c784cac3ac9b864d19257398
Tomas Machalek <tomas.machalek@gmail.com>
2012-12-17 11:49:40 +0100

    Unit testing module 'conf' now defines fake translation function '_' which just prepends string 'cs: ' to make testing easier and gettext-free.

--- tests/conf.py
+++ tests/conf.py
@@ -3,8 +3,10 @@ import os
 import shutil
 sys.path.insert(0, '../lib')
 import settings
+import __builtin__
 
 def init():
+    __builtin__.__dict__['_'] = lambda s : 'cs: %s' % s
     settings.load('default', './config.test.xml')
     if os.path.exists(settings.get('corpora', 'tags_cache_dir')):
         shutil.rmtree(settings.get('corpora', 'tags_cache_dir'))
