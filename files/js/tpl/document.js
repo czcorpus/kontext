3a6014552c72efb4774a429ea2dbaab44a6ae2bb
Tomas Machalek <tomas.machalek@gmail.com>
2013-02-19 11:07:43 +0100

    1) Refactored JavaScript in subcorp_form.tmpl 2) improved displaying of error/notification messages 3) query escaping (this is untested)

--- files/js/tpl/document.js
+++ files/js/tpl/document.js
@@ -7,6 +7,32 @@ define(['win', 'jquery', 'hideelem', 'multiselect', 'tagbuilder', 'popupbox', 'j
 
     var lib = {};
 
+
+    lib.updForm = function (item) {
+        var formAncestor,
+            i,
+            srch,
+            ancestors = $(item.target).parents();
+
+        for (i = 0; i < ancestors.length; i += 1) {
+            if (ancestors[i].nodeName === 'FORM') {
+                formAncestor = ancestors[i];
+                break;
+            }
+        }
+        if (formAncestor !== undefined) {
+            srch = $(formAncestor).find('*[name="reload"]');
+            if (srch.length > 0) {
+                $(srch[0]).attr('value', '1');
+            }
+            srch = $(formAncestor).find('*[name="usesubcorp"]');
+            if (srch.length > 0) {
+                $(srch[0]).attr('value', '');
+            }
+            formAncestor.submit();
+        }
+    };
+
     lib.misc = function (conf) {
         hideElem.targetedLinks();
         if (conf.focus) {
@@ -154,8 +180,12 @@ define(['win', 'jquery', 'hideelem', 'multiselect', 'tagbuilder', 'popupbox', 'j
             return false;
         });
 
-        $('#error button').bind('click', function (event) {
-            win.history.back();
+        $('#error a.close-icon').bind('click', function (event) {
+            $('#error').remove();
+        });
+
+        $('#notification a.close-icon').bind('click', function (event) {
+            $('#notification').remove();
         });
 
         $('img.plus-minus').each(function () {
