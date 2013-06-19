39e17943f2078cb8f5da717682b2a8bf9ca86019
Tomas Machalek <tomas.machalek@gmail.com>
2013-06-19 15:55:00 +0200

    Save a collocation list - fixes plus added support for CSV export. This is an experimental (untested) version.

---
+++ public/files/js/tpl/savecollForm.js
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2013 Institute of the Czech National Corpus
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2
+ * dated June, 1991.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+/**
+ * This module contains functionality related directly to the savecoll_form.tmpl template
+ */
+define(['jquery', 'tpl/document'], function ($, mainPage) {
+    'use strict';
+
+    var lib = {};
+
+
+    /**
+     * @param {jquery} jqForm jquery object representing parent form of the radio buttons
+     * @param {Element} currentElement
+     */
+    lib.updateExportTypeSwitch = function (jqForm, currentElement) {
+        var jqHeadingInput = $(jqForm).find('input[name="heading"]'),
+            jqHeadingRow = jqHeadingInput.closest('tr');
+
+            if ($(currentElement).val() === 'csv') {
+                jqHeadingInput.prop('disabled', true);
+                jqHeadingRow.hide();
+
+            } else {
+                jqHeadingInput.prop('disabled', false);
+                jqHeadingRow.show();
+            }
+    };
+
+    /**
+     *
+     */
+    lib.bindClicks = function () {
+        var jqForm = $('form[action="savecoll"]');
+
+        jqForm.find('input[name="saveformat"]').on('click', function (event) {
+            lib.updateExportTypeSwitch(jqForm, event.target);
+        });
+    };
+
+    /**
+     *
+     * @param conf
+     */
+    lib.init = function (conf) {
+        mainPage.init(conf);
+        lib.bindClicks(conf);
+    };
+
+    return lib;
+});
