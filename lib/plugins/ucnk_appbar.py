47f938979706386edcc1fdca14269d914e4385de
Tomas Machalek <tomas.machalek@gmail.com>
2013-09-25 15:27:12 +0200

    toolbar css

--- lib/plugins/ucnk_appbar.py
+++ lib/plugins/ucnk_appbar.py
@@ -5,15 +5,17 @@ def create_instance(settings):
     server = settings.get('plugins', 'appbar').get('server')
     path = settings.get('plugins', 'appbar').get('path', '')
     port = int(settings.get('plugins', 'appbar').get('port', 80))
-    return AppBar(server=server, path=path, port=port)
+    css_url = settings.get('plugins', 'appbar').get('css_url', None)
+    return AppBar(server=server, path=path, port=port, css_url=css_url)
 
 
 class AppBar(object):
 
-    def __init__(self, server, path, port):
+    def __init__(self, server, path, port, css_url):
         self.server = server
         self.path = path
         self.port = port if port else 80
+        self.css_url = css_url
         self.connection = httplib.HTTPConnection(self.server, port=self.port, timeout=3)
 
     def get_contents(self, id):
@@ -24,5 +26,6 @@ class AppBar(object):
             return response.read().decode('utf-8')
         else:
             import logging
-            logging.getLogger(__name__).warning('Failed to load toolbar data from authentication server. Session: %s' % id)
+            logging.getLogger(__name__).warning('Failed to load toolbar data from authentication server (%s %s). Session: %s'
+                                                % (response.status, response.reason, id))
             return None
