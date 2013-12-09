6cf04c915cb4e2872691bd480ea14512fd59fd45
Tomas Machalek <tomas.machalek@gmail.com>
2013-12-09 17:13:40 +0100

    Fixed #189 (IE8 vs. main menu)

--- public/files/js/tpl/document.js
+++ public/files/js/tpl/document.js
@@ -833,11 +833,17 @@ define(['win', 'jquery', 'hideelem', 'tagbuilder', 'popupbox', 'util', 'jquery.c
                 $(this).attr('href', '#');
             });
 
-            $('<link/>', {
-                rel: 'stylesheet',
-                type: 'text/css',
-                href: '../files/css/main-menu.css'
-            }).appendTo('head');
+
+            if (document.createStyleSheet) {
+                document.createStyleSheet('../files/css/main-menu.css');
+
+            } else {
+                $(win.document.createElement('link')).attr({
+                    rel: 'stylesheet',
+                    type: 'text/css',
+                    href: '../files/css/main-menu.css'
+                }).appendTo('head');
+            }
 
             $('#menu-level-1 a.trigger').each(function () {
                 $(this).on('mouseover', function (event) {
