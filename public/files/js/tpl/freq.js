e3386fd383fec31b112f0288f57ffb17e39cdc12
Tomas Machalek <tomas.machalek@gmail.com>
2013-12-12 11:50:28 +0100

    Updated 'close-icon' and related functionality

--- public/files/js/tpl/freq.js
+++ public/files/js/tpl/freq.js
@@ -77,8 +77,11 @@ define(['tpl/document', 'popupbox', 'jquery', 'kwicAlignUtils'], function (layou
         newLine.find('td:nth-child(4) select').attr('name', 'ml' + newLevelNum + 'ctx');
         newLine.find('td:nth-child(5) select').attr('id', 'kwic-alignment-' + newLevelNum);
         newLine.find('td input[name="freqlevel"]').val(newLevelNum);
+        // close icon
         newLine.find('td:last').empty().append('<a class="remove-level" title="' + lib.messages.remove_item + '">' +
-            '<img src="../files/img/close-icon.png" alt="' + lib.messages.remove_item + '" /></a>');
+            '<img class="over-img" src="../files/img/close-icon.png" alt="' + lib.messages.remove_item + '" data-alt-img="../files/img/close-icon_s.png" /></a>');
+        layoutModel.mouseOverImages(newLine);
+
         newLine.find('td:last a.remove-level').on('click', function (event) {
             lib.removeLevel($(event.target).closest('tr'));
         });
