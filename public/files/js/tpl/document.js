2883ec7bde5c308d8de0bed6741b10824cd242f6
Tomas Machalek <tomas.machalek@gmail.com>
2014-02-25 15:52:10 +0100

    Bugfix (the same stuff as in rev 4360a511d0df)

--- public/files/js/tpl/document.js
+++ public/files/js/tpl/document.js
@@ -818,7 +818,10 @@ define(['win', 'jquery', 'hideelem', 'tagbuilder', 'popupbox', 'util', 'jquery.c
             {
                 closeIcon : true,
                 type : 'plain',
-                timeout : null
+                timeout : null,
+                onClose : function () {
+                     $(win.document).off('keypress.withinBoxEnter');
+                }
             }
         );
     };
