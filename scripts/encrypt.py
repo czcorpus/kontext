13bc53ec9cf85c22df372fc8c3ea3742e7839cbe
Tomas Machalek <tomas.machalek@gmail.com>
2014-03-05 10:30:43 +0100

    Fixed encrypt helper script

--- scripts/encrypt.py
+++ scripts/encrypt.py
@@ -2,9 +2,9 @@
 
 if __name__ == '__main__':
     import sys
-
-    sys.path.insert(0, 'lib')
-    import ucnk_auth
+    import os
+    sys.path.insert(0, '%s/../lib' % os.path.dirname(os.path.abspath(__file__)))
+    from plugins import ucnk_auth
     import crypt
 
     if len(sys.argv) < 2:
