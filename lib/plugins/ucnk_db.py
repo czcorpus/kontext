c56d117d153d5cc20ec70064533e5af1308731f4
Tomas Machalek <tomas.machalek@gmail.com>
2014-03-19 10:46:07 +0100

    Database connection is now thread-local

--- lib/plugins/ucnk_db.py
+++ lib/plugins/ucnk_db.py
@@ -16,9 +16,38 @@ A custom database wrapper
 import MySQLdb
 import time
 import logging
+import threading
+
+
+_local = threading.local()
 
 
 class DbConnection(object):
+    """
+    Wraps MySQLdb connection with some additional functionality.
+
+    arguments:
+    db_conn -- MySQLdb database connection
+    conn_timeout -- how many seconds should the connection live
+    """
+    def __init__(self, db_conn, conn_timeout):
+        self.db_conn = db_conn
+        self.conn_time = time.time()
+        self.max_conn_time = conn_timeout
+
+    def is_old(self):
+        return time.time() - self.conn_time > self.max_conn_time
+
+    def open_status(self):
+        return self.db_conn.open
+
+    def try_close(self):
+        status = self.db_conn.stat().lower()
+        if self.open_status() >= 1 and 'gone away' not in status:
+            self.db_conn.close()
+
+
+class DbConnectionProvider(object):
 
     def __init__(self, conf):
         """
@@ -26,9 +55,6 @@ class DbConnection(object):
         conf -- a dictionary containing imported XML configuration of the plugin
         """
         self.conf = conf
-        self.conn = None
-        self.conn_time = 0
-        self.max_conn_time = float(conf['ucnk:connection_timeout'])
 
     def get(self, force_reconnect=False):
         """
@@ -36,34 +62,26 @@ class DbConnection(object):
         the connection_timeout has been exceeded then new one is created.
         Please note that this method does not monitor connection's state.
         """
-        # please note that conn.open == 0 only in case of explicit call of conn.close()
-        # i.e. in case MySQL restarts the value is still 1
-        if self.conn is None or self.conn.open < 1 or self._connection_is_old() or force_reconnect:
-            self.conn = self._open_connection()
-            self.conn_time = time.time()
-        return self.conn
+        if not hasattr(_local, 'connection') or _local.connection.open_status < 1 or _local.connection.is_old() \
+                or force_reconnect:
+            _local.connection = self._open_connection()
+        return _local.connection.db_conn
 
     def recover(self):
         """
         This is expected to be called in case some fatal error occurs, when the application cannot finish
         current action but there is a chance that
         """
-        logging.getLogger(__name__).warning('calling recover()')
-        if self.conn is not None:
-            try:
-                self.conn.close()
-            except MySQLdb.OperationalError:
-                pass
+        logging.getLogger(__name__).warning('Recovering lost database connection.')
+        _local.connection.try_close()
         self.get(force_reconnect=True)
 
     def _open_connection(self):
         conn = MySQLdb.connect(host=self.conf['ucnk:host'], user=self.conf['ucnk:username'],
                                passwd=self.conf['ucnk:password'], db=self.conf['ucnk:name'])
         conn.set_character_set('utf8')
-        return conn
-
-    def _connection_is_old(self):
-        return time.time() - self.conn_time > self.max_conn_time
+        conn_timeout = float(self.conf['ucnk:connection_timeout'])
+        return DbConnection(conn, conn_timeout)
 
 
 def create_instance(conf):
@@ -71,4 +89,4 @@ def create_instance(conf):
     Arguments:
     conf -- a dictionary containing imported XML configuration of the plugin
     """
-    return DbConnection(conf)
+    return DbConnectionProvider(conf)
