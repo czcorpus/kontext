1606ff220fe089798555ef3b6d3392d10d0ac723
Tomas Machalek <tomas.machalek@gmail.com>
2014-04-15 11:39:10 +0200

    Changed the way how client-side plugin implementation is attached to KonText

--- lib/conccgi.py
+++ lib/conccgi.py
@@ -698,7 +698,7 @@ class ConcCGI(CGIPublisher):
             result['app_bar_css_ie'] = None
 
         if plugins.has_plugin('live_attributes') and plugins.live_attributes.is_enabled_for(self.corpname):
-            result['live_attributes_js'] = 'plugins/%s' % plugins.live_attributes.get_js_module()
+            result['live_attributes_js'] = 'plugins/%s' % settings.get('plugins', 'live_attributes', {}).get('js_module')
         else:
             result['live_attributes_js'] = False
 
