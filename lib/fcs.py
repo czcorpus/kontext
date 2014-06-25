8356406d17943c6c2fb6a274554118518c1277a1
Tomas Machalek <tomas.machalek@gmail.com>
2014-06-25 11:49:07 +0200

    Experimental action controller for Federated Content Search

---
+++ lib/fcs.py
@@ -0,0 +1,47 @@
+# Copyright (c) 2014 Institute of the Czech National Corpus
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; version 2
+# dated June, 1991.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+from conccgi import ConcCGI
+from CGIPublisher import exposed
+
+
+class Actions(ConcCGI):
+    """
+    An action controller providing services related to Federated Content Search support
+    """
+
+    def __init__(self, environ, ui_lang):
+        """
+        arguments:
+        environ -- wsgi environment variable
+        ui_lang -- a language code in which current action's result will be presented
+        """
+        super(Actions, self).__init__(environ=environ, ui_lang=ui_lang)
+
+    def get_mapping_url_prefix(self):
+        """
+        This is required as it maps the controller to request URLs. In this case,
+        all the requests of the form /fcs/[action name]?[parameters] are mapped here.
+        """
+        return '/fcs/'
+
+    @exposed(return_type='xml')
+    def index(self):
+        return {}
+
+    @exposed(return_type='xml')
+    def scan(self):
+        return {}
+
+    @exposed(return_type='xml')
+    def search_retrieve(self):
+        return {}
