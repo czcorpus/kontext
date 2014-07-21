30993a7498d020751fc559c2850e90973b387800
Tomas Machalek <tomas.machalek@gmail.com>
2014-07-21 14:30:58 +0200

    Finalized functionality allowing loading of additional query history items

--- public/files/js/tpl/queryHistory.js
+++ public/files/js/tpl/queryHistory.js
@@ -30,46 +30,107 @@ define(['jquery', 'tpl/document', 'win'], function ($, layoutModel, win) {
     }
 
     function appendData(data) {
-        $.each(data.data, function (i, item) {
+        $.each(data.data, function () {
             $('table.query-history .expand-line').before('<tr class="data-item">'
-                + '<td class="query">' + item.query + '</td>'
-                + '<td class="corpname">' + item.humanCorpname + '</td>'
-                + '<td class="corpname">' + item.subcorpname + '</td>'
-                + '<td>' + item.query_type + '</td>'
-                + '<td class="date">' + item.created[1] + ' <strong>' + item.created[0] + '</strong></td>'
+                + '<td class="query">' + this.query + '</td>'
+                + '<td class="corpname">' + this.humanCorpname + '</td>'
+                + '<td class="corpname">' + this.subcorpname + '</td>'
+                + '<td>' + this.query_type + '</td>'
+                + '<td class="date">' + this.created[1] + ' <strong>' + this.created[0] + '</strong></td>'
                 + '<td><a>' + layoutModel.translate('use_query') + '</a></td>'
+                );
+        });
+    }
 
+    function getAjaxParams() {
+        var params = [],
+            form = $('.query-history-filter'),
+            queryType = form.find('select[name=\'query_type\']').val();
+
+        if (form.find('input[name=\'current_corpus\']:checked').length > 0) {
+            params.push('current_corpus=1');
+        }
+        if (queryType) {
+            params.push('query_type=' + encodeURIComponent(queryType));
+        }
+        params.push('offset=' + calcCurrNumberRows());
+        params.push('limit=' + lib.conf.page.page_append_records);
+        return params.join('&');
+    }
+
+    function dynamizeFormControls() {
+        var form = $('.query-history-filter');
+
+        form.find('button[type=\'submit\']').remove();
+        form.find('input[name=\'current_corpus\']').on('click', function () {
+            $(this).closest('form').submit();
+        });
+        form.find('select[name=\'query_type\']').on('change', function () {
+            $(this).closest('form').submit();
+        });
+    }
+
+    function addExpandLink() {
+        $('table.query-history').append('<tr class="expand-line"><td colspan="6"><a class="expand-list">'
+            + layoutModel.translate('Load more') + '</a></td></tr>');
+
+        $('table.query-history').find('a.expand-list').on('click', function () {
+            var prom,
+                linkElm,
+                actionCell = $('table.query-history .expand-line td'),
+                loaderImg;
+
+            prom = $.ajax('ajax_query_history?' + getAjaxParams(), {
+                dataType : 'json'
+            }).promise();
+
+            linkElm = actionCell.find('a').detach();
+            loaderImg = layoutModel.appendLoader(actionCell);
+
+            function cleanUpLoader(fn) {
+                loaderImg.remove();
+                if (typeof fn === 'function') {
+                    fn();
+                    win.setTimeout(function () {
+                        actionCell.empty().append(linkElm);
+                    }, 1000);
+
+                } else {
+                    actionCell.append(linkElm);
+                }
+            }
+
+            prom.then(
+                function (data) {
+                    if (data.hasOwnProperty('error')) {
+                        cleanUpLoader();
+                        lib.pluginApi.showMessage('error', data.error);
+
+                    } else {
+                        if (data.data.length > 0) {
+                            cleanUpLoader();
+
+                        } else {
+                            cleanUpLoader(function () {
+                                actionCell.append('[' + layoutModel.translate('No more lines') + ']');
+                            });
+                        }
+                        appendData(data);
+                    }
+                },
+                function (err) {
+                    cleanUpLoader();
+                    lib.pluginApi.showMessage('error', err.statusText);
+                }
             );
         });
     }
 
     lib.init = function (conf) {
         layoutModel.init(conf);
-
-        $('table.query-history').append('<tr class="expand-line"><td colspan="6"><a class="expand-list">' + 'load more' + '</a></td></tr>');
-
-        $('table.query-history').find('a.expand-list').on('click', function () {
-            var prom;
-
-            prom = $.ajax('ajax_query_history?offset=' + calcCurrNumberRows() + '&limit=' + conf.page.page_append_records, {
-                dataType : 'json'
-            }).promise();
-
-            prom.then(
-                function (data) {
-                    if (data.hasOwnProperty('error')) {
-                        lib.pluginApi.showMessage('error', data.error);
-                        // TODO
-
-                    } else {
-                        appendData(data);
-                    }
-                },
-                function (err) {
-                    lib.pluginApi.showMessage('error', err.statusText);
-                }
-            );
-        });
+        lib.conf = conf;
+        addExpandLink();
+        dynamizeFormControls();
     };
 
     return lib;
