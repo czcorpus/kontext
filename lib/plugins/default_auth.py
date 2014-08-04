8650dedc3147df038054a7e3c5ddb48811dc8d6f
Tomas Machalek <tomas.machalek@gmail.com>
2014-08-04 17:23:11 +0200

    Implemented default_auth plugin

--- lib/plugins/default_auth.py
+++ lib/plugins/default_auth.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2013 Institute of the Czech National Corpus
+# Copyright (c) 2014 Institute of the Czech National Corpus
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -11,19 +11,13 @@
 # GNU General Public License for more details.
 
 """
-A simple authentication module for development, single user and
-testing purposes.
+A simple authentication module to start with.
+It relies on default_db module which requires no database backend.
 """
+import hashlib
+
 from auth import AbstractAuth
 from translation import ugettext as _
-
-
-def create_instance(*args, **kwargs):
-    """
-    This function must be always implemented. KonText uses it to create an instance of your
-    authentication object. The settings module is passed as a parameter.
-    """
-    return DefaultAuthHandler()
 
 
 class DefaultAuthHandler(AbstractAuth):
@@ -31,51 +25,42 @@ class DefaultAuthHandler(AbstractAuth):
     Sample authentication handler
     """
 
-    def __init__(self):
-        self.corplist = ('syn2010', 'intercorp_cs', 'intercorp_en')
+    def __init__(self, db, sessions):
+        self.db = db
+        self.sessions = sessions
 
     def validate_user(self, username, password):
         """
-        DummyAuthHandler always allows logging in (i.e. it
-        always returns True)
+        arguments:
+        username -- user's login username
+        password -- user's password
 
-        Parameters
-        ----------
-        username : str
-
-        password : str
-
-        Returns
-        -------
-        bool : True on success, False on failure.
+        returns
+        True on success, False on failure.
         """
-        self.user = username
-        return {
-            'id': 333,
-            'user': 'Dummy',
-            'fullname': 'Dummy User'
-        }
+        user_data = self.db.find_by_username(username)['user']
+        pwd_hash = hashlib.md5(password).hexdigest()
+
+        if user_data and user_data['username'] == username and user_data['pwd_hash'] == pwd_hash:
+            return {
+                'id': user_data['id'],
+                'user': user_data['username'],
+                'fullname': '%s %s' % (user_data['firstname'], user_data['lastname'])
+            }
+        return self.anonymous_user()
 
     def logout(self, session_id):
         """
         """
-        pass
-
-    def auth_session(self, session_id):
-        """
-        Validates user's session ID in terms of his log-in status
-        """
-        return True
+        self.sessions.delete(session_id)
 
     def update_user_password(self, password):
         """
         Updates current user's password.
         There is no need to hash/encrypt the password - function does it automatically.
 
-        Parameters
-        ----------
-        password : str
-            new password
+        arguments:
+        password -- new password
         """
         pass
 
@@ -88,7 +73,8 @@ class DefaultAuthHandler(AbstractAuth):
         corplist : list
             list of corpora names (sorted alphabetically)
         """
-        return self.corplist
+        data = self.db.find_by_username(user)['user']
+        return data['corpora']
 
     def is_administrator(self):
         """
@@ -117,4 +103,18 @@ class DefaultAuthHandler(AbstractAuth):
         """
         """
         return _('Any string can be used.')
+
+    def get_login_url(self, root_url):
+        return '%slogin' % root_url
+
+    def get_logout_url(self, root_url):
+        return '%slogoutx' % root_url
+
+
+def create_instance(conf, sessions, db):
+    """
+    This function must be always implemented. KonText uses it to create an instance of your
+    authentication object. The settings module is passed as a parameter.
+    """
+    return DefaultAuthHandler(db, sessions)
 
