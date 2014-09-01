7ff10796c3744218ed2bc33a4bef2c034ff56dab
Tomas Machalek <tomas.machalek@gmail.com>
2014-09-02 01:49:48 +0200

    new plug-in (or 'loader' in this case) for exporting concordance data to a file

---
+++ lib/plugins/export/__init__.py
@@ -0,0 +1,52 @@
+# Copyright (c) 2014 Czech National Corpus
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; version 2
+# dated June, 1991.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+"""
+This is kind of a meta-plug-in which loads modules intended for
+exporting concordance data to a file. The plug-in itself cannot be
+changed/configured. On the other side, concrete export modules are
+free to be replaced/changed.
+"""
+
+
+class Loader(object):
+
+    def __init__(self, module_map):
+        self._module_map = module_map
+
+    def load_plugin(self, name):
+        """
+        Loads an export module specified by passed name.
+        In case you request non existing plug-in (= a plug-in
+        not set in config.xml) ValueError is raised.
+
+        arguments:
+        name -- name of the module
+
+        returns:
+        required module or nothing if module is not found
+        """
+        if name not in self._module_map:
+            raise ValueError('Cannot import __init__ module')
+        module_name = self._module_map[name]
+        module = __import__('plugins.export.%s' % module_name, fromlist=[module_name])
+        plugin = module.create_instance()
+        return plugin
+
+
+def create_instance(settings):
+    module_map = settings.get('plugins', 'export')
+    return Loader(module_map)
