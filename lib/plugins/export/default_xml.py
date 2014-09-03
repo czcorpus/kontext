4b0ae6840d088adc5c19f680e2ecb65d81ef0ea6
Tomas Machalek <tomas.machalek@gmail.com>
2014-09-03 15:50:28 +0200

    Fixed displaying line numbers

--- lib/plugins/export/default_xml.py
+++ lib/plugins/export/default_xml.py
@@ -45,17 +45,21 @@ class Document(object):
         right_context_elm = etree.SubElement(elm, 'right_context')
         right_context_elm.text = data.get('right_context', '')
 
-    def add_line(self, data):
+    def add_line(self, data, line_num=None):
         """
         Adds a single language row to the document
 
         arguments:
         data -- a dictionary of key->value pairs to be converted into XML elements <key>value</key>
+        line_num -- optional line number (if None, element is omitted)
         """
         line_elm = etree.SubElement(self._lines, 'line')
+        if line_num is not None:
+            line_num_elm = etree.SubElement(line_elm, 'num')
+            line_num_elm.text = str(line_num)
         self._append_lang(line_elm, data)
 
-    def add_multilang_line(self, lang_rows, corpnames):
+    def add_multilang_line(self, lang_rows, corpnames, line_num=None):
         """
         Adds multiple languages row to the document
 
@@ -63,8 +67,12 @@ class Document(object):
         lang_rows -- a list of dictionaries of key->value pairs to be converted into XML elements <key>value</key>
         corpnames -- a list of corpora names; the order must match the 'lang_rows' argument (i.e. 1st corpus name
                      should describe 1st record in 'lang_rows')
+        line_num -- optional line number (if None, element is omitted)
         """
         line_elm = etree.SubElement(self._lines, 'parallel_lines')
+        if line_num is not None:
+            line_num_elm = etree.SubElement(line_elm, 'num')
+            line_num_elm.text = str(line_num)
         for i in range(len(lang_rows)):
             lang_row = lang_rows[i]
             parline_elm = etree.SubElement(line_elm, 'parline')
@@ -99,9 +107,9 @@ class XMLExport(AbstractExport):
         if len(lang_rows) == 0:
             raise ValueError('empty line')
         elif len(lang_rows) == 1:  # single language has a slightly different XML structure
-            self._document.add_line(lang_rows[0])
+            self._document.add_line(lang_rows[0], line_num)
         else:
-            self._document.add_multilang_line(lang_rows, self._corpnames)
+            self._document.add_multilang_line(lang_rows, self._corpnames, line_num)
 
 
 def create_instance():
