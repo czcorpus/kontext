80300ed6d5b6c2f3268a98977ffac8501ce2465e
Tomas Machalek <tomas.machalek@gmail.com>
2014-09-09 16:44:28 +0200

    A simple concept of scheduled tasks

---
+++ lib/scheduled.py
@@ -0,0 +1,36 @@
+# Copyright (c) 2014 Institute of the Czech National Corpus
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; version 2
+# dated June, 1991.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+import shutil
+
+from translation import ugettext as _
+
+
+def add_subcorpus(**kwargs):
+    ans = {'error': None}
+    try:
+        shutil.copy(kwargs['src_path'], kwargs['dest_path'])
+        ans['message'] = _('A new sub-corpus has been added to your library')
+    except Exception as e:
+        ans['error'] = e
+    return ans
+
+
+def show_message(**kwargs):
+    return {
+        'error': None,
+        'message': kwargs['message']
+    }
