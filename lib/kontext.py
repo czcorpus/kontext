cd9d3f18bdb71db9bfbaa58b2c49a11e8c19f52e
Tomas Machalek <tomas.machalek@gmail.com>
2014-09-17 16:12:49 +0200

    1) accessing corpora list via user_id (instead of username) 2) default_auth - corpora list is now a separate key->value item in storage 3) no leading zeros in user-related storage keys 4) moved CorpusDependentPlugin class to plugins.abstract

--- lib/kontext.py
+++ lib/kontext.py
@@ -514,7 +514,7 @@ class Kontext(Controller):
         self._apply_general_user_settings(options, self._init_default_settings)
 
         # corpus access check
-        allowed_corpora = plugins.auth.get_corplist(self._session_get('user', 'user'))
+        allowed_corpora = plugins.auth.get_corplist(self._session_get('user', 'id'))
         if self._requires_corpus_access(path[0]):
             self.corpname, fallback_url = self._determine_curr_corpus(form, allowed_corpora)
             if fallback_url:
@@ -570,7 +570,7 @@ class Kontext(Controller):
         convert_types(na, self.clone_self())
         if selectorname:
             choose_selector(self.__dict__, getattr(self, selectorname))
-        self.cm = corplib.CorpusManager(plugins.auth.get_corplist(self._session_get('user', 'user')), self.subcpath)
+        self.cm = corplib.CorpusManager(plugins.auth.get_corplist(self._session_get('user', 'id')), self.subcpath)
         if getattr(self, 'refs') is None:
             self.refs = corpus_get_conf(self._corp(), 'SHORTREF')
         self.__dict__.update(na)
