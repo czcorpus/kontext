7b47265f4dbfc297d9a181618c7da5d2f44e6409
Tomas Machalek <tomas.machalek@gmail.com>
2014-09-30 16:12:12 +0200

    1) logging 2) solved charset issues

--- scripts/plugins/ucnk_remote_auth2/syncdb.py
+++ scripts/plugins/ucnk_remote_auth2/syncdb.py
@@ -1,21 +1,47 @@
 """
-Note: this is UCNK specific functionality
+Note:
+This script was created for specific needs of the Institute of the Czech National Corpus
+and it is not needed for default KonText installation.
 """
 import argparse
 import os
 import sys
+import logging
+from logging import handlers
+import json
 
-app_path = os.path.realpath('%s/../../..' % os.path.dirname(os.path.abspath(__file__)))
-sys.path.insert(0, '%s/lib' % app_path)
+APP_PATH = os.path.realpath('%s/../../..' % os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, '%s/lib' % APP_PATH)
 import settings
 from plugins import ucnk_remote_auth2 as auth
 import mysql2redis as m2r
 
+logger = logging.getLogger('syncdb')
 
 DEFAULT_CHECK_INTERVAL = 5
 
 
-class DbWatchdog(object):
+def load_conf():
+    conf_file = '%s/sync.json' % APP_PATH
+    if os.path.isfile(conf_file):
+        return json.load(open(conf_file, 'r'))
+    else:
+        return {}
+
+
+def setup_logger(conf):
+    if 'logging' in conf:
+        handler = handlers.RotatingFileHandler(conf.get('global', 'log_path'),
+                                               maxBytes=conf.get('global', 'log_file_size', 8000000),
+                                               backupCount=conf.get('global', 'log_num_files', 10))
+    else:
+        handler = logging.StreamHandler(sys.stdout)
+    handler.setFormatter(logging.Formatter('%(asctime)s [%(name)s] %(levelname)s: %(message)s'))
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO if not settings.is_debug_mode() else logging.DEBUG)
+
+
+class DbSync(object):
 
     def __init__(self, mysql_conn, redis_params, check_interval, db_name):
         self._mysql = mysql_conn
@@ -32,7 +58,7 @@ class DbWatchdog(object):
         return cursor
 
     def get_user_changes(self):
-        cursor = self.execute("SELECT user_id, created FROM user_changelog ORDER BY created ASC")
+        cursor = self.execute("SELECT user_id FROM user_changelog ORDER BY created ASC")
         return cursor.fetchall()
 
     def find_mass_changes(self):
@@ -55,14 +81,22 @@ class DbWatchdog(object):
         import_obj = m2r.create_import_instance(self._redis_params, dry_run=dry_run)
         if len(mass_ch) > 0:
             self.log_mass_change()
+        changed_users = 0
         for changed_user in self.get_user_changes():
-            data = export.run(changed_user[0])
-            if len(data) == 1:
-                import_obj.run(data)
+            try:
+                data = export.run(changed_user[0])
+                if len(data) == 1:
+                    import_obj.run(data)
+                    changed_users += 1
+            except Exception as e:
+                logger.error(e)
+        return changed_users
 
 
 if __name__ == '__main__':
-    settings.load('%s/config.xml' % app_path)
+    settings.load('%s/config.xml' % APP_PATH)
+    conf = load_conf()
+    setup_logger(conf)
     parser = argparse.ArgumentParser(description='Check for changes in UCNK database and synchronize with KonText')
     parser.add_argument('-t', '--interval', type=int, default=DEFAULT_CHECK_INTERVAL,
                         help='how often (in minutes) script runs (default=%d)' % DEFAULT_CHECK_INTERVAL)
@@ -70,12 +104,15 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     mysql_params = auth.create_auth_db_params(settings.get('plugins', 'auth'))
+    mysql_params.update(dict(charset='utf8', use_unicode=True))
     redis_params = settings.get('plugins', 'db')
     redis_params = dict(host=redis_params['default:host'],
                         port=redis_params['default:port'],
                         db_id=redis_params['default:id'])
-    w = DbWatchdog(mysql_conn=auth.connect_auth_db(**mysql_params),
-                   redis_params=redis_params,
-                   check_interval=args.interval,
-                   db_name=mysql_params['db'])
-    w(dry_run=args.dry_run)
+    w = DbSync(mysql_conn=auth.connect_auth_db(**mysql_params),
+               redis_params=redis_params,
+               check_interval=args.interval,
+               db_name=mysql_params['db'])
+    changed = w(dry_run=args.dry_run)
+    if changed > 0:
+        logger.info('Synchronized %d users.' % changed)
