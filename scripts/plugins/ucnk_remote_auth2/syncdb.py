064cb8ba32ebe277734f7af6642b1e3e91a623dd
Tomas Machalek <tomas.machalek@gmail.com>
2014-09-30 17:03:28 +0200

    Comments plus minor refactoring

--- scripts/plugins/ucnk_remote_auth2/syncdb.py
+++ scripts/plugins/ucnk_remote_auth2/syncdb.py
@@ -2,6 +2,9 @@
 Note:
 This script was created for specific needs of the Institute of the Czech National Corpus
 and it is not needed for default KonText installation.
+
+The script is expected to be run either by cron in regular intervals or manually in
+special cases.
 """
 import argparse
 import os
@@ -25,6 +28,9 @@ DEFAULT_NUM_LOG_FILES = 5
 
 
 def load_conf():
+    """
+    Loads an optional syncdb script configuration
+    """
     conf_file = '%s/syncdb.json' % APP_PATH
     if os.path.isfile(conf_file):
         return json.load(open(conf_file, 'r'))
@@ -32,8 +38,16 @@ def load_conf():
         return {}
 
 
-def setup_logger(conf):
-    if 'logging' in conf:
+def setup_logger(conf=None):
+    """
+    Configures logging parameters. If an emtpy conf is provided then
+    logging to the standard output is configured. Otherwise, file rotating
+    logger is instantiated.
+
+    arguments:
+    conf -- a dictionary containing syncdb configuration ('logging' key is searched here)
+    """
+    if conf is not None and 'logging' in conf:
         conf = conf['logging']
         handler = handlers.RotatingFileHandler(conf['path'],
                                                maxBytes=conf.get('file_size', DEFAULT_LOG_FILE_SIZE),
@@ -46,44 +60,91 @@ def setup_logger(conf):
 
 
 class DbSync(object):
+    """
+    A class which handles MySQL -> RedisDB synchronization.
+    The object is callable - by calling it, a single "check and update" action is run.
+    """
 
     def __init__(self, mysql_conn, redis_params, check_interval, db_name):
+        """
+        arguments:
+        mysql_conn -- a MySQLdb connection object
+        redis_params -- a dictionary containing redis connection information (host, db, port)
+        check_interval -- how old (in minutes) changes should syncdb script consider; this
+            applies only for mass changes as user_changelog is always read without any restriction
+        db_name -- a name of respective MySQL/MariaDB database
+        """
         self._mysql = mysql_conn
         self._redis_params = redis_params
         self._check_interval = check_interval
         self._db_name = db_name
 
-    def execute(self, sql, params=None):
+    def query(self, sql, params=None):
+        """
+        Runs a query.
+
+        arguments:
+        sql -- a SQL query
+        params -- parameters to be bound to the query (optional)
+
+        returns:
+        DB cursor
+        """
         cursor = self._mysql.cursor()
         if params:
-            cursor.execute(sql, params)
+            cursor.query(sql, params)
         else:
-            cursor.execute(sql)
+            cursor.query(sql)
         return cursor
 
     def get_user_changes(self):
-        cursor = self.execute("SELECT DISTINCT user_id FROM user_changelog ORDER BY created ASC")
+        """
+        Returns a list of records from user_changelog.
+        """
+        cursor = self.query("SELECT DISTINCT user_id FROM user_changelog ORDER BY created ASC")
         return cursor.fetchall()
 
     def find_mass_changes(self):
+        """
+        Searches for changes in tables related to the 'user' table and concerning potentially
+        all the users (e.g. a new corpus is added). There are certainly more effective
+       (and also more complicated) solutions but this should be OK in our case.
+
+        returns:
+        a list of tuples (user_id, last_change_datetime)
+        """
         sql = "SELECT table_name, update_time " \
               "FROM information_schema.tables " \
               "WHERE update_time > (NOW() - INTERVAL %s MINUTE) " \
               "AND table_schema = %s" \
               "AND table_name IN ('corplist', 'corpora', 'relation')"
-        cursor = self.execute(sql, (self._check_interval, self._db_name))
+        cursor = self.query(sql, (self._check_interval, self._db_name))
         return cursor.fetchall()
 
     def log_mass_change(self):
-        self.execute("DELETE FROM user_changelog")  # we do not need this as each user will be there in a moment
-        self.execute("INSERT INTO user_changelog (user_id, created) SELECT id, NOW() FROM user")
+        """
+        Writes a change flag to user_changelog for all users. Any previous
+        records are deleted first because all the users will be updated anyway.
+        """
+        self.query("DELETE FROM user_changelog")  # we do not need this as each user will be there in a moment
+        self.query("INSERT INTO user_changelog (user_id, created) SELECT id, NOW() FROM user")
         self._mysql.commit()
 
     def delete_log(self, user_id):
-        self.execute("DELETE FROM user_changelog WHERE user_id = %s", (user_id,))
+        """
+        Removes all records from user_changelog related to the user user_id
+
+        arguments:
+        user_id -- a DB ID of the user
+        """
+        self.query("DELETE FROM user_changelog WHERE user_id = %s", (user_id,))
         self._mysql.commit()
 
     def __call__(self, dry_run=False):
+        """
+        Performs an action containing search for changes and updating affected records
+        in RedisDB.
+        """
         mass_ch = self.find_mass_changes()
         export = m2r.Export(mysqldb=self._mysql, default_corpora=('susanne',))
         import_obj = m2r.create_import_instance(self._redis_params, dry_run=dry_run)
