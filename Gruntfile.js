1b415bd365e14f35fb39bd660bfb74f7e64b9b78
Tomas Machalek <tomas.machalek@gmail.com>
2015-02-03 10:54:16 +0100

    Refactored/improved Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,32 +1,6 @@
 module.exports = function (grunt) {
 
-
-    function loadPluginMap() {
-        var modules = {
-            'application_bar' : 'applicationBar',
-            'query_storage' : 'queryStorage',
-            'live_attributes' : 'liveAttributes'
-        };
-        var fs = require('fs');
-        var data = fs.readFileSync('./config.xml', {encoding: 'utf8'});
-        var DOMParser = require('xmldom').DOMParser;
-        var doc = new DOMParser().parseFromString(data);
-        var pluginMap = {
-            'win' : 'empty:',
-            'conf' : 'empty:',
-            'jquery' : 'empty:',
-            'SoundManager' : 'vendor/soundmanager2.min',
-            'vendor/jscrollpane' : 'vendor/jscrollpane.min'
-        }
-        for (var p in modules) {
-            var elms = doc.getElementsByTagName(p);
-            if (elms[0]) {
-                var jsElm = elms[0].getElementsByTagName('js_module');
-                pluginMap['plugins/' + modules[p]] = 'plugins/' + jsElm[0].textContent;
-            }
-        };
-        return pluginMap;
-    }
+    var kontext = require('./scripts/grunt/kontext');
 
     grunt.loadNpmTasks('grunt-exec');
     grunt.loadNpmTasks('assemble-less');
@@ -53,7 +27,13 @@ module.exports = function (grunt) {
                     './public/files/js/compiled/*',
                     './public/files/js/optimized/*'
                 ]
-            }                        
+            },
+            cleanup :  {
+                src : [
+                    './public/files/js/optimized/*',
+                    './public/files/js/compiled/*'
+                ]
+            }
         },
         exec : {
             compile_html_templates : {
@@ -186,44 +166,28 @@ module.exports = function (grunt) {
                     },
                     wrapShim : true,
                     optimize : 'none',
-                    paths : loadPluginMap(),
-                    modules : [
-                        { name : 'tpl/concDesc' },
-                        { name : 'tpl/corplist' },
-                        { name : 'tpl/filterForm' },
-                        { name : 'tpl/firstForm' },
-                        { name : 'tpl/freq' },
-                        { name : 'tpl/queryHistory' },
-                        { name : 'tpl/savecollForm' },
-                        { name : 'tpl/saveconcForm' },
-                        { name : 'tpl/savefreqForm' },
-                        { name : 'tpl/savewlForm' },
-                        { name : 'tpl/sort' },
-                        { name : 'tpl/stats' },
-                        { name : 'tpl/subcorp' },
-                        { name : 'tpl/subcorpForm' },
-                        { name : 'tpl/subcorpList' },
-                        { name : 'tpl/view' },
-                        { name : 'tpl/viewattrs' },
-                        { name : 'tpl/wordlist' },
-                        { name : 'tpl/wordlistForm' }
-                    ],
-
+                    paths : kontext.loadPluginMap('./config.xml'),
+                    modules : kontext.listPageModules('./public/files/js/tpl')
                 }
             }
         }
     });
 
-    grunt.registerTask('develjs', ['clean:javascript', 'typescript', 'copy:devel']);
+    // generates development-ready project (i.e. no minimizations/optimizations)
+    grunt.registerTask('devel', ['clean:all', 'typescript', 'copy:devel', 'clean:cleanup', 'exec']);
 
-    grunt.registerTask('devel', ['clean:all', 'typescript', 'copy:devel', 'exec']);
+    // regenerates JavaScript files for development-ready project (i.e. no min./optimizations and no Cheetah templates compiled)
+    grunt.registerTask('develjs', ['clean:javascript', 'typescript', 'copy:devel', 'clean:cleanup']);
 
+    // generates production-ready project with additional optimization of JavaScript files (RequireJS optimizer)
     grunt.registerTask('production-optimized', ['clean:all', 'less', 'typescript', 'copy:prepare', 'requirejs:compile',
-                       'copy:finishOptimized', 'uglify:optimized', 'exec']);
+                       'copy:finishOptimized', 'uglify:optimized', 'clean:cleanup', 'exec']);
 
+    // generates production-ready project where all JavaScript modules are loaded individually
     grunt.registerTask('production', ['clean:all', 'less', 'typescript', 'copy:prepare',
-                       'copy:finishNonOptimized', 'uglify:nonOptimized', 'exec']);    
+                       'copy:finishNonOptimized', 'uglify:nonOptimized', 'clean:cleanup', 'exec']);    
 
+    // generates production-like project with RequireJS optimization but without minimization
     grunt.registerTask('production-debug', ['clean:all', 'less', 'typescript', 'copy:prepare', 'requirejs:compile',
-                       'copy:finishOptimized', 'exec']);
+                       'copy:finishOptimized', 'clean:cleanup', 'exec']);
 };
