dd378da0f92803c72cf4e27a530eac0f6f6c07fe
Tomas Machalek <tomas.machalek@gmail.com>
2015-02-25 16:54:39 +0100

    1) sessions are now implemented in a 'werkzeug' way 2) fix - actions must have access_level = 0 by default 3) controller instances now obtain a request object instead of environ

--- lib/actions.py
+++ lib/actions.py
@@ -69,13 +69,13 @@ class Actions(Kontext):
     """
     This class specifies all the actions KonText offers to a user via HTTP
     """
-    def __init__(self, environ, ui_lang):
+    def __init__(self, request, ui_lang):
         """
         arguments:
-        environ -- wsgi environment variable
+        request -- werkzeug's Request obj.
         ui_lang -- a language code in which current action's result will be presented
         """
-        super(Actions, self).__init__(environ=environ, ui_lang=ui_lang)
+        super(Actions, self).__init__(request=request, ui_lang=ui_lang)
         self.contains_within = False
         self.disabled_menu_items = ()
 
