6f3052c7f1eae5e8bc88d4d8cc07899a26b27023
Tomas Machalek <tomas.machalek@gmail.com>
2015-10-02 04:27:59 +0200

    Fix displaying of the 'requestable' flag
    for anonymous user this does not make sense

--- lib/plugins/ucnk_corparch.py
+++ lib/plugins/ucnk_corparch.py
@@ -194,11 +194,12 @@ class UcnkCorpArch(CorpTree):
     def customize_corpus_info(self, corpus_info, node):
         corpus_info.requestable = self._decode_bool(node.attrib.get('requestable'))
 
-    def customize_search_result_item(self, item, corpus_info):
-        item['requestable'] = corpus_info.requestable
+    def customize_search_result_item(self, plugin_api, item, permitted_corpora, corpus_info):
+        item['requestable'] = corpus_info.requestable and corpus_info.id not in permitted_corpora \
+                              and not plugin_api.user_is_anonymous
 
-    def custom_filter(self, corpus_info, permitted_corpora):
-        return corpus_info.id in permitted_corpora or corpus_info.requestable
+    def custom_filter(self, plugin_api, corpus_info, permitted_corpora):
+        return corpus_info.id in permitted_corpora or (corpus_info.requestable and not plugin_api.user_is_anonymous)
 
     def get_list(self, user_allowed_corpora):
         """
