280c675fd23a660ebdf0ca2035de97776317d528
Tomas Machalek <tomas.machalek@gmail.com>
2016-03-17 11:26:39 +0100

    Update worker to operate using json results and messages

---
+++ conf/celeryconfig.sample.py
@@ -0,0 +1,5 @@
+BROKER_URL = 'redis://localhost:6379/2'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/2'
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_ACCEPT_CONTENT = ['json']
