46e4174fac30e57f25f83593037b3e6d3cccd79c
Tomas Machalek <tomas.machalek@gmail.com>
2016-04-07 10:06:08 +0200

    Loading footer code from templates

--- lib/plugins/lindat_footer_bar/__init__.py
+++ lib/plugins/lindat_footer_bar/__init__.py
@@ -14,17 +14,41 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
+"""
+Required plug-in configuration:
+
+element footer_bar {
+  element module { "lindat_footer_bar" }
+  element js_module { "lindatFooterBar" }
+}
+"""
+
+import os
+
 from plugins.abstract.footer_bar import AbstractFootbar
 
 
-class DefaultFootbar(AbstractFootbar):
+class FootBar(AbstractFootbar):
+
+    def __init__(self):
+        self._resource_dir = os.path.realpath(os.path.join(os.path.dirname(__file__), 'resources'))
+
+    def _get_resource_path(self, filename):
+        return os.path.join(self._resource_dir, filename)
 
     def get_contents(self, plugin_api, return_url=None):
-        return '<div class="ufal-footer">A custom LINDAT footer - TEST</div>'
+        if 'cs_CZ' == plugin_api.user_lang:
+            input_file = self._get_resource_path('cs/footer.html')
+        else:
+            input_file = self._get_resource_path('footer.html')
+        with open(input_file, mode='rb') as fin:
+            return fin.read().decode('utf-8')
 
     def get_css_url(self):
         return None
 
 
 def create_instance(conf):
-    return DefaultFootbar()
+    return FootBar()
+
+
