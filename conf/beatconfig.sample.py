7c4d77e32bacf9c6e06b31be43ecc608da1e1fcb
Tomas Machalek <tomas.machalek@gmail.com>
2016-05-05 15:20:46 +0200

    Update/fix beat sample config
    - missing 'minute' args
    - missing serializer settings!!!

--- conf/beatconfig.sample.py
+++ conf/beatconfig.sample.py
@@ -1,6 +1,7 @@
 from celery.schedules import crontab
 
 BROKER_URL = 'redis://localhost:6379/2'
+CELERY_TASK_SERIALIZER = 'json'
 
 CELERYBEAT_SCHEDULE = {
     'sync-user-database': {
@@ -10,20 +11,20 @@ CELERYBEAT_SCHEDULE = {
     },
     'archive-old-concordances': {
         'task': 'worker.archive_concordance',
-        'schedule': crontab(hour='*/1'),
+        'schedule': crontab(hour='*/1', minute=0),
         'kwargs': dict(cron_interval=20, key_prefix=None, dry_run=False)
     },
     'conc-cache-cleanup': {
         'task': 'worker.conc_cache_cleanup',
-        'schedule': crontab(hour='*/12'),
+        'schedule': crontab(hour='*/12', minute=10),
         'kwargs': dict(ttl=120, subdir='student', dry_run=False)
     },
     'freqs-cache-cleanup': {
         'task': 'worker.clean_freqs_cache',
-        'schedule': crontab(hour='*/1')
+        'schedule': crontab(hour='*/1', minute=20)
     },
     'colls-cache-cleanup': {
         'task': 'worker.clean_colls_cache',
-        'schedule': crontab(hour='*/1')
+        'schedule': crontab(hour='*/1', minute=30)
     }
 }
