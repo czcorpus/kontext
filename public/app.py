588f22e7fa0808597df5b73debe7829ca91e3020
Tomas Machalek <tomas.machalek@gmail.com>
2016-05-10 11:14:29 +0200

    Stop loading available translations on each request

--- public/app.py
+++ public/app.py
@@ -15,13 +15,11 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 """
-This module is an entry point of a WSGI application.
-It replaces run.cgi from the previous versions of bonito2
-and KonText which were CGI-based.
+This module is an entry point of KonText WSGI application.
 
 It can be run in two modes:
  1) as a standalone application (useful for development/testing purposes)
- 2) within a WSGI-enabled web server (Apache + mod_wsgi etc.).
+ 2) within a WSGI-enabled web server (Gunicorn, uwsgi, Apache + mod_wsgi)
 """
 
 import sys
@@ -51,102 +49,115 @@ locale.setlocale(locale.LC_ALL, 'en_US.utf-8')  # we ensure that the application
 logger = logging.getLogger('')  # root logger
 
 
-def setup_logger(conf):
-    """
-    Sets up file-based rotating logger. All the parameters are extracted
-    from conf argument:
-    path: /kontext/global/log_path
-    maximum file size (optional, default is 8MB): /kontext/global/log_file_size
-    number of backed-up files (optional, default is 10): /kontext/global/log_num_files
-    """
-    handler = handlers.RotatingFileHandler(conf.get('logging', 'path'),
-                                           maxBytes=conf.get_int('logging', 'file_size', 8000000),
-                                           backupCount=conf.get_int('logging', 'num_files', 10))
-    handler.setFormatter(logging.Formatter(fmt='%(asctime)s [%(name)s] %(levelname)s: %(message)s'))
-    logger.addHandler(handler)
-    logger.setLevel(logging.INFO if not settings.is_debug_mode() else logging.DEBUG)
+class WsgiApp(object):
 
-
-def cleanup_runtime_modules():
-    """
-    Makes app to forget previously faked modules which
-    ensures proper plugins initialization if not starting from scratch.
-    """
-    plugins.flush_plugins()
-
-
-def get_lang(environ):
-    """
-    Detects user's preferred language (either via the 'getlang' plugin or from HTTP_ACCEPT_LANGUAGE env value)
-
-    arguments:
-    environ -- WSGI environment variable
-
-    returns:
-    underscore-separated ISO 639 language code and ISO 3166 country code
-    """
-    installed = dict([(x.split('_')[0], x) for x in os.listdir('%s/../locale' % os.path.dirname(__file__))])
-    cookies = KonTextCookie(environ.get('HTTP_COOKIE', ''))
-
-    if plugins.has_plugin('getlang'):
-        lgs_string = plugins.get('getlang').fetch_current_language(cookies)
-    else:
-        lang_cookie = cookies.get('kontext_ui_lang')
-        if not lang_cookie:
-            lgs_string = parse_accept_header(environ.get('HTTP_ACCEPT_LANGUAGE')).best
-        else:
-            lgs_string = lang_cookie.value
-        if len(lgs_string) == 2:  # in case we obtain just an ISO 639 language code
-            lgs_string = installed.get(lgs_string)
-        else:
-            lgs_string = lgs_string.replace('-', '_')
-    if lgs_string is None:
-        lgs_string = 'en_US'
-    return lgs_string
-
-
-def load_controller_class(path_info):
-    """
-    Loads appropriate action controller class according to the provided
-    path info. Classes selection is based on path_info prefix (e.g. / prefix
-    maps to the main action controller actions.py, /fcs maps to a fcs.py
-    controller etc.).
-
-    Please note that currently there is no general automatized loading
-    (i.e. all the path->class mapping is hardcoded in this function).
-
-    arguments:
-    path_info -- a string as found in environment['PATH_INFO']
-
-    returns:
-    a class matching provided path_info
-    """
-    if path_info.startswith('/fcs'):
-        from actions.fcs import Actions as ControllerClass
-    elif path_info.startswith('/user'):
-        from actions.user import User as ControllerClass
-    elif path_info.startswith('/subcorpus'):
-        from actions.subcorpus import Subcorpus as ControllerClass
-    elif path_info.startswith('/options'):
-        from actions.options import Options as ControllerClass
-    elif path_info.startswith('/admin'):
-        from actions.admin import Admin as ControllerClass
-    elif path_info.startswith('/corpora'):
-        from actions.corpora import Corpora as ControllerClass
-    else:
-        from actions.concordance import Actions as ControllerClass
-    return ControllerClass
-
-
-class MaintenanceApp(object):
     def __init__(self):
-        setup_logger(settings)
+        self.setup_logger(settings)
+        self._installed_langs = dict([(x.split('_')[0], x)
+                                      for x in os.listdir('%s/../locale' % os.path.dirname(__file__))])
+
+    def __call__(self, environ, start_response):
+        raise NotImplementedError()
+
+    @staticmethod
+    def setup_logger(conf):
+        """
+        Sets up file-based rotating logger. All the parameters are extracted
+        from conf argument:
+        path: /kontext/global/log_path
+        maximum file size (optional, default is 8MB): /kontext/global/log_file_size
+        number of backed-up files (optional, default is 10): /kontext/global/log_num_files
+        """
+        handler = handlers.RotatingFileHandler(conf.get('logging', 'path'),
+                                               maxBytes=conf.get_int('logging', 'file_size', 8000000),
+                                               backupCount=conf.get_int('logging', 'num_files', 10))
+        handler.setFormatter(logging.Formatter(fmt='%(asctime)s [%(name)s] %(levelname)s: %(message)s'))
+        logger.addHandler(handler)
+        logger.setLevel(logging.INFO if not settings.is_debug_mode() else logging.DEBUG)
+
+    @staticmethod
+    def cleanup_runtime_modules():
+        """
+        Makes app to forget previously faked modules which
+        ensures proper plugins initialization if not starting from scratch.
+        """
+        plugins.flush_plugins()
+
+    def get_lang(self, environ):
+        """
+        Detects user's preferred language (either via the 'getlang' plugin or from HTTP_ACCEPT_LANGUAGE env value)
+
+        arguments:
+        environ -- WSGI environment variable
+
+        returns:
+        underscore-separated ISO 639 language code and ISO 3166 country code
+        """
+        cookies = KonTextCookie(environ.get('HTTP_COOKIE', ''))
+
+        if plugins.has_plugin('getlang'):
+            lgs_string = plugins.get('getlang').fetch_current_language(cookies)
+        else:
+            lang_cookie = cookies.get('kontext_ui_lang')
+            if not lang_cookie:
+                lgs_string = parse_accept_header(environ.get('HTTP_ACCEPT_LANGUAGE')).best
+            else:
+                lgs_string = lang_cookie.value
+            if len(lgs_string) == 2:  # in case we obtain just an ISO 639 language code
+                lgs_string = self._installed_langs.get(lgs_string)
+            else:
+                lgs_string = lgs_string.replace('-', '_')
+        if lgs_string is None:
+            lgs_string = 'en_US'
+        return lgs_string
+
+    @staticmethod
+    def load_controller_class(path_info):
+        """
+        Loads appropriate action controller class according to the provided
+        path info. Classes selection is based on path_info prefix (e.g. / prefix
+        maps to the main action controller actions.py, /fcs maps to a fcs.py
+        controller etc.).
+
+        Please note that currently there is no general automatized loading
+        (i.e. all the path->class mapping is hardcoded in this function).
+
+        arguments:
+        path_info -- a string as found in environment['PATH_INFO']
+
+        returns:
+        a class matching provided path_info
+        """
+        if path_info.startswith('/fcs'):
+            from actions.fcs import Actions as ControllerClass
+        elif path_info.startswith('/user'):
+            from actions.user import User as ControllerClass
+        elif path_info.startswith('/subcorpus'):
+            from actions.subcorpus import Subcorpus as ControllerClass
+        elif path_info.startswith('/options'):
+            from actions.options import Options as ControllerClass
+        elif path_info.startswith('/admin'):
+            from actions.admin import Admin as ControllerClass
+        elif path_info.startswith('/corpora'):
+            from actions.corpora import Corpora as ControllerClass
+        else:
+            from actions.concordance import Actions as ControllerClass
+        return ControllerClass
+
+
+class MaintenanceWsgiApp(WsgiApp):
+    """
+    This WSGI application shows only a single page informing about
+    current maintenance. It is activated via config.xml.
+    """
+    def __init__(self):
+        super(MaintenanceWsgiApp, self).__init__()
         translation.load_translations(settings.get('global', 'translations'))
         l10n.configure(settings.get('global', 'translations'))
 
     def __call__(self, environ, start_response):
         from maintenance import MaintenanceController
-        ui_lang = get_lang(environ)
+        ui_lang = self.get_lang(environ)
         translation.activate(ui_lang)
         l10n.activate(ui_lang)
         request = Request(environ)
@@ -156,27 +167,24 @@ class MaintenanceApp(object):
         return response(environ, start_response)
 
 
-class App(object):
+class KonTextWsgiApp(WsgiApp):
     """
-    WSGI application
+    KonText WSGI application
     """
 
     def __init__(self):
         """
         Initializes the application and persistent objects/modules (settings, plugins,...)
         """
-        setup_logger(settings)
-        cleanup_runtime_modules()
+        super(KonTextWsgiApp, self).__init__()
+        self.cleanup_runtime_modules()
         setup_plugins()
         translation.load_translations(settings.get('global', 'translations'))
         l10n.configure(settings.get('global', 'translations'))
         os.environ['MANATEE_REGISTRY'] = settings.get('corpora', 'manatee_registry')
 
     def __call__(self, environ, start_response):
-        """
-        Works as specified by the WSGI
-        """
-        ui_lang = get_lang(environ)
+        ui_lang = self.get_lang(environ)
         translation.activate(ui_lang)
         l10n.activate(ui_lang)
         environ['REQUEST_URI'] = wsgiref.util.request_uri(environ)  # TODO remove?
@@ -205,7 +213,7 @@ class App(object):
             headers = [('Location', environ['REQUEST_URI'].replace('/run.cgi/', '/'))]
             body = ''
         else:
-            controller_class = load_controller_class(environ['PATH_INFO'])
+            controller_class = self.load_controller_class(environ['PATH_INFO'])
             app = controller_class(request=request, ui_lang=ui_lang)
             status, headers, sid_is_valid, body = app.run()
         response = Response(response=body, status=status, headers=headers)
@@ -235,9 +243,9 @@ elif not os.path.exists(settings.get('corpora', 'calc_pid_dir')):
     os.makedirs(settings.get('corpora', 'calc_pid_dir'))
 
 if not settings.get_bool('global', 'maintenance'):
-    application = App()
+    application = KonTextWsgiApp()
 else:
-    application = MaintenanceApp()
+    application = MaintenanceWsgiApp()
 
 robots_path = os.path.join(os.path.dirname(__file__), 'files/robots.txt')
 if os.path.isfile(robots_path):
