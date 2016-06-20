b2c15ed0da7ee3c5d24aa1e8de01ecd923d4247d
Tomas Machalek <tomas.machalek@gmail.com>
2016-06-20 14:53:48 +0200

    Add forgotten file (#1148)

---
+++ public/files/js/types/views.d.ts
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2016 Institute of the Czech National Corpus
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2
+ * dated June, 1991.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+
+declare module "views/document" {
+    export function init(dispatcher:any, mixins:any, storeProvider:any):Kontext.LayoutViews;
+}
+
+
+declare module "views/concordance" {
+
+    export interface ConcordanceViews {
+        LineSelectionMenu:any;
+        LockedLineGroupsMenu:any;
+    }
+
+    export function init(dispatcher:any, mixins:any, lineSelectionStore:any, userInfoStore:any):ConcordanceViews;
+}
+
+
+
+declare module "views/subcorpForm" {
+
+    export interface SubcorpFormViews {
+        WithinBuilder:any;
+    }
+
+    export function init(dispatcher:any, mixins:any, subcorpFormStore:any):SubcorpFormViews;
+}
+
+
+
+declare module "views/textTypes" {
+
+    export interface TextTypesViews {
+        TextTypesPanel:any;
+    }
+
+    export function init(dispatcher:any, mixins:any, textTypesStore:any):TextTypesViews;
+
+}
+
+
+declare module "views/query/context" {
+
+    export interface QueryContextViews {
+        SpecifyKontextForm:any;
+    }
+
+    export function init(dispatcher, mixins):QueryContextViews;
+}
