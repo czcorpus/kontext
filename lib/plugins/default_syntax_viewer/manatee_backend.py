be007f0f277395ad08d8e1df786c978be2c4d6e5
Tomas Machalek <tomas.machalek@gmail.com>
2016-08-31 11:32:54 +0200

    Handle empty values add docstrings

--- lib/plugins/default_syntax_viewer/manatee_backend.py
+++ lib/plugins/default_syntax_viewer/manatee_backend.py
@@ -22,6 +22,7 @@ An expected configuration:
 {
     "syn2015": {
       "sentenceStruct": "s",
+      "emptyValuePlaceholders": ["-"],    # an empty string is implicit
       "trees": [
         {
           "id": "default",
@@ -124,6 +125,14 @@ class TreeConf(object):
 
     @property
     def attr_refs(self):
+        """
+        Returns (dict of list):
+            return attributes which should be interpreted as links
+            to other tokens. Values of dict are used as keys in
+            target nodes to retrieve their values. E.g. we can
+            specify that "eparent" attribute should retrieve
+            "word" from the referred node as follows: {"eparent":["word"]}
+        """
         return self._data.get('attrRefs', {})
 
     @property
@@ -157,6 +166,16 @@ class ManateeBackendConf(object):
     def get_sentence_struct(self, canonical_corpus_id):
         return self._data[canonical_corpus_id]['sentenceStruct']
 
+    def get_empty_value_placeholders(self, canonical_corpus_id):
+        """
+        Args:
+            canonical_corpus_id (str): corpus ID
+
+        Returns:
+            (list of str)
+        """
+        return self._data[canonical_corpus_id].get('emptyValuePlaceholders', [])
+
 
 class TreeNodeEncoder(json.JSONEncoder):
     """
@@ -170,7 +189,7 @@ class TreeNodeEncoder(json.JSONEncoder):
             data.update(obj.data)
             return {
                 'parent': obj.parent.id if obj.parent else None,
-                'hint': '',
+                'hint': None,
                 'labels': obj.node_labels,
                 'firstson': obj.children[0].id if len(obj.children) > 0 else None,
                 'id': obj.id,
@@ -187,16 +206,28 @@ class TreeNodeEncoder(json.JSONEncoder):
 class TreeNode(object):
     """
     Defines a syntax tree node.
+
+    Attributes:
+        id (str): node identifier used by js-treex-view
+        idx (int): node order in the list (zero based)
+        data (dict of str:any): a dict containing detailed information about the node
+        node_labels (list of str): a list of labels for the nodes
+        word (str): a "word" value of the node (i.e. the actual word the node represents)
+        parent (int): parent node index in sentence
+        children (list of TreeNode): child nodes
+        rbrother (TreeNode): nodes right neighbour
+        lbrother (TreeNode): nodes left neighbour
+        depth (int): depth of the node
     """
 
     def __init__(self, idx, data, node_labels, word, parent):
         """
-        arguments:
-        idx -- node order in the list (zero based)
-        data -- a dict containing detailed information about the node
-        node_labels -- a list of strings to be used as labels of the node
-        word -- a "word" value of the node (i.e. the actual word the node represents)
-        parent -- parent node (i.e. an another TreeNode instance)
+        Args:
+            idx (int): node order in the list (zero based)
+            data (dict of str:any): a dict containing detailed information about the node
+            node_labels (list of str): a list of labels for the nodes
+            word (str): a "word" value of the node (i.e. the actual word the node represents)
+            parent (int): parent node
         """
         self.id = 'n%d' % idx
         self.idx = idx
@@ -253,7 +284,14 @@ class TreeBuilder(object):
     """
 
     @staticmethod
-    def walk_through(root_node):
+    def attach_tree_params(root_node):
+        """
+        Traverses the tree (in a BFS manner) and attaches information
+        about left/right neighbours and depth.
+
+        Args:
+            root_node (TreeNode): a node to start in
+        """
         root_node.depth = 0
         queue = [root_node]
         while len(queue) > 0:
@@ -274,14 +312,13 @@ class TreeBuilder(object):
         """
         Runs the build process
 
-        arguments:
-        tree_conf -- a configuration for the tree
-        tree_data -- a list of dicts containg data fetched from Manatee with parent
-                references converted from relative ones to absolute ones plus
+        Args:
+            tree_conf (TreeConf): a configuration for the tree
+            data (list of dict of str:any): a list of dicts containing data fetched
+                from Manatee with parent references converted from relative ones to absolute ones plus
                 some other updates (see ManateeBackend class)
 
-        returns:
-        a 2-tuple (list_of_nodes, TreeNodeEncoder)
+        Returns (tuple(list_of_nodes, TreeNodeEncoder))
         """
         def export_labels(item):
             values = [v[1] for v in self._dict_portion(item, tree_conf.node_attrs)]
@@ -297,7 +334,7 @@ class TreeBuilder(object):
             if n.parent is not None:
                 nodes[n.parent].children.append(n)
                 n.parent = nodes[n.parent]
-        self.walk_through(nodes[0])
+        self.attach_tree_params(nodes[0])
         return nodes
 
 
@@ -309,14 +346,25 @@ class ManateeBackend(SearchBackend):
 
     def __init__(self, conf):
         """
-        arguments:
-        conf -- configuration dictionary as obtained by reading
+        Args:
+        conf (dict): configuration dictionary as obtained by reading
                 the configuration JSON and selecting the "corpora"
                 object (i.e. not the whole JSON data).
         """
         self._conf = ManateeBackendConf(conf)
 
     def _load_raw_sent(self, corpus, canonical_corpus_id, token_id, tree_attrs):
+        """
+        Retrieve a sentence via Manatee
+        Args:
+            corpus (manatee.Corpus): a corpus instance
+            canonical_corpus_id (str): canonical corpus ID
+            token_id (int): token number/id
+            tree_attrs (list of str): a list of positional attributes required by tree nodes/edges
+
+        Returns (str):
+            a string-encoded sentence and required attribute metadata
+        """
         encoding = corpus.get_conf('ENCODING')
         sentence_struct = self._conf.get_sentence_struct(canonical_corpus_id)
         conc = manatee.Concordance(corpus, '[#%d]' % token_id, 1, -1)
@@ -331,15 +379,40 @@ class ManateeBackend(SearchBackend):
                     for s in kl.get_left() + kl.get_kwic() + kl.get_right()]
 
     @staticmethod
-    def _parse_raw_sent(in_data, tree_attrs):
+    def _parse_raw_sent(in_data, tree_attrs, empty_val_placeholders):
+        """
+        Args:
+            in_data (list of str): a string-encoded sentence and required attribute metadata (see _load_raw_sent())
+            tree_attrs (list of str): a list of attributes used by nodes/edges of the tree
+            empty_val_placeholders (list of str): a list of values which may represent an empty
+                value in a raw sentence data
+
+        Returns (list of dict):
+            a list of dict items representing tree nodes
+        """
+        def import_raw_val(v):
+            return None if v in empty_val_placeholders or v == '' else v
+
         data = []
         for i in range(0, len(in_data), 4):
-            item = dict(zip(tree_attrs, in_data[i + 2].split('/')))
+            item = dict(zip(tree_attrs, [import_raw_val(x) for x in in_data[i + 2].split('/')]))
             item['word'] = in_data[i]
             data.append(item)
         return data
 
     def _get_abs_reference(self, curr_idx, item, ref_attr):
+        """
+        Transform a relative parent reference as defined in original nodes
+        into an sentence-absolute representation (e.g. 5th element referring
+        to '-2' translates into reference to '3').
+        Args:
+            curr_idx (int): current position within a sentence
+            item (dict): processed node
+            ref_attr (str): a node attribute used to refer to node's parent
+
+        Returns (int):
+            sentence-absolute position of the parent or None if nothing found
+        """
         if item[ref_attr]:
             rel_parent = self.import_parent_val(item[ref_attr])
             return curr_idx + rel_parent if rel_parent != 0 else None
@@ -347,16 +420,36 @@ class ManateeBackend(SearchBackend):
             return None
 
     def _process_attr_refs(self, data, curr_idx, attr_refs):
+        """
+        Process indirect node values (values which are actually
+        in referred nodes). Please do not confuse this with
+        references used to construct the tree. These are just
+        "indirect" text values.
+
+        Method updates the 'data' argument and returns None
+
+        Args:
+            data (list of dict): list of nodes representing a sentence
+            curr_idx (int): current position in the sentence
+            attr_refs (dict of str:list): configured indirect values
+
+        """
         for ident, items in attr_refs.items():
             abs_ref = self._get_abs_reference(curr_idx, data[curr_idx], ident)
             if abs_ref is None:
-                data[curr_idx][ident] = '-'
+                data[curr_idx][ident] = None
             else:
                 ref_item = data[abs_ref]
                 data[curr_idx][ident] = '%s (%s)' % (data[curr_idx][ident],
                                                      ', '.join(map(lambda ar: ref_item[ar], items)))
 
     def _decode_tree_data(self, data, parent_attr, attr_refs):
+        """
+        Args:
+            data (list of dict of str:any): parsed sentence
+            parent_attr (str): an attribute used to refer to node's parent
+            attr_refs (dict of str:list of str): indirect values
+        """
         for i in range(1, len(data)):
             abs_parent = self._get_abs_reference(i, data[i], parent_attr)
             # Please note that referring to the 0-th node
@@ -376,7 +469,8 @@ class ManateeBackend(SearchBackend):
         for tree in tree_id_list:
             conf = tree_configs[tree]
             raw_data = self._load_raw_sent(corpus, canonical_corpus_id, token_id, conf.all_attrs)
-            parsed_data = self._parse_raw_sent(raw_data, conf.all_attrs)
+            parsed_data = self._parse_raw_sent(raw_data, conf.all_attrs,
+                                               self._conf.get_empty_value_placeholders(canonical_corpus_id))
             if conf.root_node:
                 parsed_data = [conf.root_node] + parsed_data
             self._decode_tree_data(parsed_data, conf.parent_attr, conf.attr_refs)
