748caeb9e2fb089f8adfc77f23e03c7651604ab1
Tomas Machalek <tomas.machalek@gmail.com>
2017-04-05 09:27:15 +0200

    Fix 'on request' ipm calculation
    (supp633, total num of items must be calculated in a synchronous way)

--- lib/actions/concordance.py
+++ lib/actions/concordance.py
@@ -1817,7 +1817,7 @@ class Actions(Querying):
             query_pref = m.groups()[0]
             query_suff = m.groups()[1]
             self.args.q[0] = u'%s[] %s' % (query_pref, CQLDetectWithin().get_within_part(query_suff))
-            conc = self.call_function(conclib.get_conc, (self.corp, self._session_get('user', 'user')))
+            conc = self.call_function(conclib.get_conc, (self.corp, self._session_get('user', 'user')), async=0)
             conc.sync()
             return {'total': conc.fullsize() if conc else None}
         else:
