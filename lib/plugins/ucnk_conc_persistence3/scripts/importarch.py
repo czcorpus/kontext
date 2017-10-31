6f123467bdefe1e3284f2c2296814f87ca347b6e
petrduda <petrduda@seznam.cz>
2017-10-31 18:19:44 +0100

    add docstring

--- lib/plugins/ucnk_conc_persistence3/scripts/importarch.py
+++ lib/plugins/ucnk_conc_persistence3/scripts/importarch.py
@@ -1,3 +1,35 @@
+# Copyright (c) 2017 Charles University, Faculty of Arts,
+#                    Institute of the Czech National Corpus
+# Copyright (c) 2017 Tomas Machalek <tomas.machalek@gmail.com>
+# Copyright (c) 2017 Petr Duda <petrduda@seznam.cz>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; version 2
+# dated June, 1991.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+"""
+A helper script to split an existing archive into smaller archive files.
+The only necessary command line argument is the source database path, e.g.:
+
+python lib/plugins/ucnk_conc_persistence3/scripts/importarch.py /path/to/src.db
+
+Optional arguments:
+-si, --splitinto: number of files the source archive should be split into. If not specified, the number of files is
+                    determined based on the archive rows limit config value
+-c, --clear: delete all files from the archives directory before splitting
+-v, --verbose: verbose mode
+"""
+
 import argparse
 import os
 import sys
@@ -17,7 +49,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("source_archive", help="full path to the source archive db file")
 parser.add_argument("-si", "--splitinto", help="number of files the source archive should be split into")
 parser.add_argument("-c", "--clear", action="store_true", help="clear the archives directory before splitting")
-parser.add_argument("-v", "--verbose", action="store_true", help="clear the archives directory before splitting")
+parser.add_argument("-v", "--verbose", action="store_true", help="verbose mode")
 
 args = parser.parse_args()
 source_path = args.source_archive
