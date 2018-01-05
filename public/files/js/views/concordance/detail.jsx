a9c266f19ce6940925b63f71f4c4b3eede8944ff
Tomas Machalek <tomas.machalek@gmail.com>
2018-01-05 11:06:54 +0100

    Fix #2131

--- public/files/js/views/concordance/detail.jsx
+++ public/files/js/views/concordance/detail.jsx
@@ -32,21 +32,13 @@ export function init(dispatcher, he, concDetailStore, refsDetailStore, lineStore
     // ------------------------- <RefValue /> ---------------------------
 
     const RefValue = (props) => {
-
         if (props.val.indexOf('http://') === 0 || props.val.indexOf('https://') === 0) {
-            let val, title;
-            if ((props.val.length) > 50) {
-                val = props.val.substr(0, 50) + '\u2026';
-                title = props.val;
-
-            } else {
-                val = props.val;
-                title = '';
-            }
-            return <a className="external" href={props.val} title={title} target="_blank">{val}</a>;
+            return <a className="external" href={props.val} target="_blank">
+                <layoutViews.Shortener text={props.val} limit={20} />
+            </a>;
 
         } else {
-            return <span>{props.val}</span>
+            return <span>{props.val}</span>;
         }
     };
 
