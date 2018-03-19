137d2c0f4050674526916959638de5959004f40c
Tomas Machalek <tomas.machalek@gmail.com>
2018-03-19 12:50:38 +0100

    Improve kwic_connect - allow chunked loading

--- lib/plugins/default_kwic_connect/__init__.py
+++ lib/plugins/default_kwic_connect/__init__.py
@@ -16,6 +16,22 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
+"""
+
+Required XML configuration:
+
+element token_connect {
+  element module { "default_kwic_connect" }
+  element js_module { "defaultKwicConnect" }
+  element max_kwic_words {   # how many unique kwic words to explore (at most)
+    attribute extension-by { "default" }
+    { xsd:integer }
+  }
+  element load_chunk_size {  # how many kwic words' responses to load at once (0 ... max_kwic_words)
+    attribute extension-by { "default" }
+    { xsd:integer }
+  }
+"""
 
 from plugins.abstract.kwic_connect import AbstractKwicConnect
 from plugins.default_token_connect import setup_providers, ProviderWrapper
@@ -58,13 +74,18 @@ def fetch_external_kwic_info(self, request):
 
 class DefaultKwicConnect(ProviderWrapper, AbstractKwicConnect):
 
-    def __init__(self, providers, corparch):
+    def __init__(self, providers, corparch, max_kwic_words, load_chunk_size):
         super(DefaultKwicConnect, self).__init__(providers)
         self._corparch = corparch
+        self._max_kwic_words = max_kwic_words
+        self._load_chunk_size = load_chunk_size
 
     def is_enabled_for(self, plugin_api, corpname):
         corpus_info = self._corparch.get_corpus_info(plugin_api.user_lang, corpname)
         return len(corpus_info.kwic_connect.providers) > 0
+
+    def export(self, plugin_api):
+        return dict(max_kwic_words=self._max_kwic_words, load_chunk_size=self._load_chunk_size)
 
     def export_actions(self):
         return {concordance.Actions: [fetch_external_kwic_info]}
@@ -85,7 +106,9 @@ class DefaultKwicConnect(ProviderWrapper, AbstractKwicConnect):
 @plugins.inject(plugins.runtime.CORPARCH)
 def create_instance(settings, corparch):
     providers, cache_path = setup_providers(settings.get('plugins', 'token_connect'))
-    kwic_conn = DefaultKwicConnect(providers, corparch)
+    plg_conf = settings.get('plugins', 'kwic_connect')
+    kwic_conn = DefaultKwicConnect(providers, corparch, max_kwic_words=plg_conf['default:max_kwic_words'],
+                                   load_chunk_size=plg_conf['default:load_chunk_size'])
     if cache_path:
         kwic_conn.set_cache_path(cache_path)
     return kwic_conn
