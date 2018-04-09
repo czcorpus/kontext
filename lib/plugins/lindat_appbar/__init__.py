c9f60a5803352e006a6324f9f5da8842ba594f2c
Jozef M <jm@notebook>
2018-04-09 17:06:51 +0200

    test

--- lib/plugins/lindat_appbar/__init__.py
+++ lib/plugins/lindat_appbar/__init__.py
@@ -14,6 +14,7 @@ import os
 from plugins.abstract.appbar import AbstractApplicationBar
 from translation import ugettext as _
 from plugins import inject
+import plugins
 
 
 class LindatTopBar(AbstractApplicationBar):
@@ -76,7 +77,7 @@ class LindatTopBar(AbstractApplicationBar):
         return ''
 
 
-@inject('auth')
+@inject(plugins.runtime.AUTH)
 def create_instance(settings, auth):
     """
         Auth must provide `get_logout_url`
