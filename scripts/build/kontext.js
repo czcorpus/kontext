80b624e41ad8687201d6e86be9c86ba029b32d5b
Jozef M <jm@notebook>
2018-04-10 10:59:29 +0200

    adding cookies li

--- scripts/build/kontext.js
+++ scripts/build/kontext.js
@@ -167,6 +167,7 @@
             'vendor/d3-color': path.resolve(jsPath, 'vendor/d3-color.min'),
             'vendor/intl-messageformat': path.resolve(jsPath, 'vendor/intl-messageformat'),
             'vendor/SoundManager' : path.resolve(jsPath, 'vendor/soundmanager2.min'),
+            'vendor/cookies' : path.resolve(jsPath, 'vendor/cookies'),
             'cqlParser/parser': cqlParserPath,
             'misc/keyboardLayouts': path.resolve(jsPath, 'kb-layouts.json'),
             'styles': cssPath,
