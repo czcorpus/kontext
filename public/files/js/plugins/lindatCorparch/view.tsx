ce5b486737ba33086046d9dcdbdf3bea4798a9c4
Kira Droganova <kira.droganova@gmail.com>
2018-04-25 20:40:32 +0200

    button for pmltq

--- public/files/js/plugins/lindatCorparch/view.tsx
+++ public/files/js/plugins/lindatCorparch/view.tsx
@@ -259,13 +259,14 @@ export function init(dispatcher:ActionDispatcher, he:Kontext.ComponentHelpers,
 
         _access() {
             if (!this.props.permitted) {
-                return <span className="glyphicon glyphicon-lock"></span>
+                return <span className="glyphicon glyphicon-lock"></span>;
             }
         }
 
         _syntax() {
             if (this.props.features.includes('syntax')) {
-                return <span className="glyphicon lindat-pmltq-logo">&nbsp;</span>;
+                return <button className="btn btn-default lindat-pmltq-logo">
+                </button>
             }
             return null;
         }
