42346dc4b6b0830508ef6a7df3073e33ea0f1a38
Tomas Machalek <tomas.machalek@gmail.com>
2018-05-24 16:11:22 +0200

    Create a (formal) server-side part of the plug-in for Lindat

---
+++ lib/plugins/lindat_token_connect/__init__.py
@@ -0,0 +1,30 @@
+# Copyright (c) 2018 Charles University, Faculty of Arts,
+#                    Institute of the Czech National Corpus
+# Copyright (c) 2018 Tomas Machalek <tomas.machalek@gmail.com>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; version 2
+# dated June, 1991.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+import plugins
+
+from plugins.default_token_connect import DefaultTokenConnect, setup_providers
+
+
+@plugins.inject(plugins.runtime.CORPARCH)
+def create_instance(settings, corparch):
+    providers, cache_path = setup_providers(settings.get('plugins', 'token_connect'))
+    tok_det = DefaultTokenConnect(providers, corparch)
+    if cache_path:
+        tok_det.set_cache_path(cache_path)
+    return tok_det
