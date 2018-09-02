ad60de63567593e543c25d5770f93d9bba4e7d6c
Kira Droganova <kira.droganova@gmail.com>
2018-09-03 00:15:09 +0200

    mult source verb CzEngVallex v5

--- public/files/js/plugins/lindatTokenConnect/view.tsx
+++ public/files/js/plugins/lindatTokenConnect/view.tsx
@@ -410,7 +410,7 @@ export function init(dispatcher:ActionDispatcher, he:Kontext.ComponentHelpers) {
     }> {
         renderTargetVerbsInfo() {
             return this.props.verbTargetList.map((item, i) => {
-                return <Target key={i} num={i} verbTargetName={this.props.verbTargetName}
+                return <Target key={i} verbTargetName={this.props.verbTargetName}
                             verbSourceName={this.props.verbSourceName}
                             verbSourceID={this.props.verbSourceID}
                             verbTargetList={item} />
@@ -428,6 +428,7 @@ export function init(dispatcher:ActionDispatcher, he:Kontext.ComponentHelpers) {
     // ------------- <Target /> -------------------------------
 
     class Target extends React.Component<{
+        key:number;
         verbSourceName:string;
         verbTargetName:string;
         verbSourceID:VRD.VsourceID;
@@ -445,7 +446,7 @@ export function init(dispatcher:ActionDispatcher, he:Kontext.ComponentHelpers) {
         }
 
         _textHandler(props) {
-            return this.state.collapse ? `Show details for pair ${props.num}` : "Hide details";
+            return this.state.collapse ? `Show details for pair ${props.key}` : "Hide details";
         }
 
         _getStateDisplay() {
