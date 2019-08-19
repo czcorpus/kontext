80eb01485fa0fb967c5e4986331dc0d9f4ec3232
Martin Zimandl <martin.zimandl@gmail.com>
2019-08-19 17:06:59 +0200

    using isBusy property instead of isLoaded and showing loader

--- public/files/js/plugins/defaultTaghelper/ud/models.ts
+++ public/files/js/plugins/defaultTaghelper/ud/models.ts
@@ -24,7 +24,6 @@ import { TagBuilderBaseState } from '../common';
 
 export interface FeatureSelectProps {
     error:Error|null;
-    isLoaded:boolean;
     allFeatures:Immutable.Map<string, Immutable.List<string>>;
     availableFeatures:Immutable.Map<string, Immutable.List<string>>;
     filterFeaturesHistory:Immutable.List<Immutable.List<FilterRecord>>;
@@ -64,7 +63,6 @@ export interface UDTagBuilderModelState extends TagBuilderBaseState {
 
     // ...
     error: Error|null;
-    isLoaded: boolean;
     allFeatures: Immutable.Map<string, Immutable.List<string>>,
     availableFeatures: Immutable.Map<string, Immutable.List<string>>,
     filterFeaturesHistory: Immutable.List<Immutable.List<FilterRecord>>;
@@ -83,12 +81,11 @@ export class UDTagBuilderModel extends StatelessModel<UDTagBuilderModelState> {
             dispatcher,
             {
                 corpname:corpname,
-                isBusy: undefined,
+                isBusy: false,
                 insertRange: [0, 0],
                 canUndo: false,
                 displayPattern: '',
                 error: null,
-                isLoaded: false,
                 allFeatures: Immutable.Map({}),
                 availableFeatures: Immutable.Map({}),
                 filterFeaturesHistory: Immutable.List([Immutable.List([])]),
@@ -105,9 +102,13 @@ export class UDTagBuilderModel extends StatelessModel<UDTagBuilderModelState> {
                 newState.showCategory = action.payload['value'];
                 return newState;
             },
+            'TAGHELPER_GET_INITIAL_FEATURES': (state, action) => {
+                const newState = this.copyState(state);
+                newState.isBusy = true;
+                return newState;
+            },
             'TAGHELPER_GET_INITIAL_FEATURES_DONE': (state, action) => {
                 const newState = this.copyState(state);
-                newState.isLoaded = true;
                 if (!action.error) {
                     newState.allFeatures = Immutable.fromJS(action.payload['result']);
                     newState.availableFeatures = newState.allFeatures;
@@ -115,22 +116,24 @@ export class UDTagBuilderModel extends StatelessModel<UDTagBuilderModelState> {
                 } else {
                     newState.error = action.error;
                 }
+                newState.isBusy = false;
                 return newState;
             },
             'TAGHELPER_LOAD_FILTERED_DATA_DONE': (state, action) => {
                 const newState = this.copyState(state);
-                newState.isLoaded = true;
                 if (!action.error) {
                     newState.availableFeatures = Immutable.fromJS(action.payload['result']);
                 } else {
                     newState.error = action.error;
                 }
+                newState.isBusy = false;
                 return newState;
             },
             'TAGHELPER_ADD_FILTER': (state, action) => {
                 const newState = this.copyState(state);
                 const filter = new FilterRecord(action.payload);
                 const filterFeatures = newState.filterFeaturesHistory.last();
+                newState.isBusy = true;
                 if (filterFeatures.every(x => !x.equals(filter))) {
                     const newFilterFeatures = filterFeatures.push(filter);
                     newState.filterFeaturesHistory = newState.filterFeaturesHistory.push(newFilterFeatures);
@@ -147,6 +150,7 @@ export class UDTagBuilderModel extends StatelessModel<UDTagBuilderModelState> {
                 const newFilterFeatures = filterFeatures.filterNot((value) => value.equals(filter));
                 newState.filterFeaturesHistory = newState.filterFeaturesHistory.push(Immutable.List(newFilterFeatures))
                 newState.canUndo = true;
+                newState.isBusy = true;
                 newState.displayPattern = composePattern(newState);
 
                 return newState;
@@ -157,6 +161,7 @@ export class UDTagBuilderModel extends StatelessModel<UDTagBuilderModelState> {
                 if (newState.filterFeaturesHistory.size===1) {
                     newState.canUndo = false;
                 }
+                newState.isBusy = true;
                 newState.displayPattern = composePattern(newState);
                 return newState;
             },
