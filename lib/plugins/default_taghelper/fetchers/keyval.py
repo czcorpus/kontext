ddb1506e39568a7af5da060990ab0f1803860ec7
Martin Zimandl <martin.zimandl@gmail.com>
2019-08-20 10:45:19 +0200

    filtering request query by UD specification

--- lib/plugins/default_taghelper/fetchers/keyval.py
+++ lib/plugins/default_taghelper/fetchers/keyval.py
@@ -20,13 +20,65 @@
 
 from plugins.abstract.taghelper import AbstractValueSelectionFetcher
 
-EXCEPT_KEYS = {'corpname', }
+UD_KEYS = {  # taken from `universaldependencies.org` specification
+    'POS': 'part of speech',  # added artificially
+    'Abbr': 'abbreviation',
+    'AbsErgDatNumber': 'number agreement with absolutive / ergative / dative argument',
+    'AbsErgDatPerson': 'person agreement with absolutive / ergative / dative argument',
+    'AbsErgDatPolite': 'politeness agreement with absolutive / ergative / dative argument',
+    'AdpType': 'adposition type',
+    'AdvType': 'adverb type',
+    'Animacy': 'animacy',
+    'Aspect': 'aspect',
+    'Case': 'case',
+    'Clusivity': 'clusivity',
+    'ConjType': 'conjunction type',
+    'Definite': 'definiteness or state',
+    'Degree': 'degree of comparison',
+    'Echo': 'is this an echo word or a reduplicative?',
+    'ErgDatGender': 'gender agreement with ergative / dative argument',
+    'Evident': 'evidentiality',
+    'Foreign': 'is this a foreign word?',
+    'Gender': 'gender',
+    'Hyph': 'hyphenated compound or part of it',
+    'Mood': 'mood',
+    'NameType': 'type of named entity',
+    'NounClass': 'noun class',
+    'NounType': 'noun type',
+    'NumForm': 'numeral form',
+    'NumType': 'numeral type',
+    'NumValue': 'numeric value',
+    'Number': 'number',
+    'PartType': 'particle type',
+    'Person': 'person',
+    'Polarity': 'polarity',
+    'Polite': 'politeness',
+    'Poss': 'possessive',
+    'PossGender': "possessor's gender",
+    'PossNumber': "possessor's number",
+    'PossPerson': "possessor's person",
+    'PossedNumber': "possessed object's number",
+    'Prefix': 'Word functions as a prefix in a compund construction',
+    'PrepCase': 'case form sensitive to prepositions',
+    'PronType': 'pronominal type',
+    'PunctSide': 'which side of paired punctuation is this?',
+    'PunctType': 'punctuation type',
+    'Reflex': 'reflexive',
+    'Style': 'style or sublanguage to which this word form belongs',
+    'Subcat': 'subcategorization',
+    'Tense': 'tense',
+    'Typo': 'is this a misspelled word?',
+    'VerbForm': 'form of verb or deverbative',
+    'VerbType': 'verb type',
+    'Voice': 'voice',
+}
 
 
 class KeyvalSelectionFetcher(AbstractValueSelectionFetcher):
 
     def fetch(self, request):
-        return [(k, v) for k, v in request.args.items() if k not in EXCEPT_KEYS]
+        # using startswith, because some features can be layered using [], like `Gender[psor]`
+        return [(k, v) for k, v in request.args.items(multi = True) if any(k.startswith(ud_key) for ud_key in UD_KEYS)]
 
     def is_empty(self, val):
         return len(val) == 0
