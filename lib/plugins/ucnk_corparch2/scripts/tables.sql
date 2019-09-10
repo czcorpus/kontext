b9786c2f0f26009f46c568d068efbebb5b1d427c
Tomas Machalek <tomas.machalek@gmail.com>
2019-09-10 09:52:54 +0200

    corparch - tweak visuals, fix some sql issues

--- lib/plugins/ucnk_corparch2/scripts/tables.sql
+++ lib/plugins/ucnk_corparch2/scripts/tables.sql
@@ -308,3 +308,17 @@ SELECT user_id, kc.name, NULL AS variant, kc.name
 FROM corpora AS kc;
 END $$
 DELIMITER ;
+
+
+DROP TABLE IF EXISTS kontext_corpus_taghelper;
+CREATE TABLE kontext_corpus_taghelper (
+    corpus_name varchar(63) NOT NULL,
+    pos_attr varchar(63),
+    feat_attr varchar(63) NOT NULL,
+    tagset_type ENUM('positional', 'keyval', 'other') NOT NULL,
+    tagset_name varchar(63),
+    PRIMARY KEY (corpus_name, feat_attr)
+);
+ALTER TABLE `kontext_corpus_taghelper` COLLATE 'utf8_general_ci';
+ALTER TABLE kontext_corpus_taghelper ADD CONSTRAINT kontext_corpus_taghelper_pos_attr_id_fkey FOREIGN KEY (corpus_name, pos_attr) REFERENCES corpus_posattr(corpus_name, name);
+ALTER TABLE kontext_corpus_taghelper ADD CONSTRAINT kontext_corpus_taghelper_feat_attr_id_fkey FOREIGN KEY (corpus_name, feat_attr) REFERENCES corpus_posattr(corpus_name, name);
