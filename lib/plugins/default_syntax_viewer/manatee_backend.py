63e707855791269544b9741bfd192eb463433c97
Tomas Machalek <tomas.machalek@gmail.com>
2020-01-06 14:34:39 +0100

    Fix syntax viewer backend (a collision between UFAL and CNC versions)

--- lib/plugins/default_syntax_viewer/manatee_backend.py
+++ lib/plugins/default_syntax_viewer/manatee_backend.py
@@ -423,7 +423,7 @@ class ManateeBackend(SearchBackend):
             kwic_tk = kl.get_kwic()
             return dict(data=[import_string(s, from_encoding=encoding)
                               for s in left_tk + kwic_tk + kl.get_right()],
-                        kwic_pos=(len(left_tk) / 4, len(kwic_tk) / 4))
+                        kwic_pos=(len(left_tk) // 4, len(kwic_tk) // 4))
 
     @staticmethod
     def _parse_raw_sent(in_data, tree_attrs, empty_val_placeholders):
@@ -437,12 +437,10 @@ class ManateeBackend(SearchBackend):
         Returns (list of dict):
             a list of dict items representing tree nodes
         """
-        import logging
         def import_raw_val(v):
             return None if v in empty_val_placeholders or v == '' else v
 
         data = []
-        logging.getLogger(__name__).debug('in_data: {}'.format(in_data))
         for i in range(0, len(in_data), 4):
             parsed = [import_raw_val(x) for x in in_data[i + 2].split('/')]
             if len(parsed) > len(tree_attrs):
@@ -456,7 +454,6 @@ class ManateeBackend(SearchBackend):
                 item = dict(list(zip(tree_attrs, parsed)))
                 item['word'] = in_data[i]
                 data.append(item)
-        logging.getLogger(__name__).debug('>>>> {}'.format(len(data)))
         return data
 
     def _get_ord_reference(self, curr_idx, data, parent_attr, parent_type):
