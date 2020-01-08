3a40d7970950776bc6804d6b24f8d67cb30e3d72
mzimandl <martin.zimandl@gmail.com>
2020-01-08 13:52:46 +0100

    Python installation script (#2928)
    * initial commit
    
    * replacing subprocess with python alternatives
    
    * using separate script to install requirements
    
    * installing manatee from source
    
    * few bugfixes
    
    * printing info messages
    
    * done and abort methods implemented for SetupDefaulUsers step
    
    * download installation files only if necessary
    
    * fixed nginx config
    
    * using python-apt to install dependencies, fixed list of dependencies
    
    * no need to use python-apt, requirements install correctly now because of fixed colliding dependencies
    
    * installing and updating pip

---
+++ scripts/install/install.py
@@ -0,0 +1,76 @@
+from typing import List
+
+import os
+import sys
+import subprocess
+import inspect
+
+KONTEXT_PATH = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../..'))
+
+REQUIREMENTS = [
+    'python3-pip',
+    'wget',
+    'curl',
+    'openssh-server',
+    'net-tools',
+    'redis-server',
+    'build-essential',
+    'openssl',
+    'pkg-config',
+    'swig',
+    'nginx',
+    'npm',
+    'libltdl7',
+    'libpcre3',
+    'libicu-dev',
+    'libpcre++-dev',
+    'libxml2-dev',
+    'libxslt1-dev',
+    'libltdl-dev',
+    # required by manatee packages
+    'm4',
+    'parallel',
+    'locales-all'
+]
+
+
+if __name__ == "__main__":
+    stdout = open(os.devnull, 'wb')
+    print(sys.argv)
+    if '-v' in sys.argv:
+        stdout = None
+
+    # install prerequisites
+    print('Installing requirements...')
+    subprocess.check_call(['locale-gen', 'en_US.UTF-8'], stdout=stdout)
+    subprocess.check_call(['apt-get', 'update', '-y'], stdout=stdout)
+    subprocess.check_call(['apt-get', 'install', '-y'] + REQUIREMENTS, stdout=stdout)
+    subprocess.check_call(['python3', '-m', 'pip', 'install', 'pip', '--upgrade'], stdout=stdout)
+    subprocess.check_call(['pip3', 'install', 'simplejson', 'celery', 'signalfd', '-r', 'requirements.txt'], cwd=KONTEXT_PATH, stdout=stdout)
+
+    # import steps here, because some depend on packages installed by this script
+    import steps
+    # run installation steps
+    steps.SetupManatee(KONTEXT_PATH, stdout).run()
+    steps.SetupKontext(KONTEXT_PATH, stdout).run()
+    steps.SetupDefaultUsers(KONTEXT_PATH, stdout).run()
+    
+    # finalize instalation
+    print('Initializing celery and nginx services...')
+    subprocess.check_call(['systemctl', 'start', 'celery'], stdout=stdout)
+    subprocess.check_call(['systemctl', 'restart', 'nginx'], stdout=stdout)
+
+    # print final messages
+    print(inspect.cleandoc(f'''
+        {steps.bcolors.BOLD}{steps.bcolors.OKGREEN}
+        KonText installation successfully completed.
+        To start KonText, enter the following command in the KonText install root directory (i.e. {KONTEXT_PATH}):
+        
+            sudo -u {steps.WEBSERVER_USER} python3 public/app.py --address 127.0.0.1 --port 8080
+
+        (--address and --port parameters are optional; default serving address is 127.0.0.1:5000)
+        {steps.bcolors.ENDC}{steps.bcolors.ENDC}
+    '''))
+
+    for message in steps.InstallationStep.final_messages:
+        print(inspect.cleandoc(message))
