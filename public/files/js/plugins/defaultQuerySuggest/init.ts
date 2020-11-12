dca4804e05498e15234aebf5ad20081775323ab1
Tomas Machalek <tomas@machalek.xyz>
2020-11-12 12:38:15 +0100

    In case of selecting the more general attr, update also raw query

--- public/files/js/plugins/defaultQuerySuggest/init.ts
+++ public/files/js/plugins/defaultQuerySuggest/init.ts
@@ -78,6 +78,9 @@ export class DefaultQuerySuggest implements PluginInterfaces.QuerySuggest.IPlugi
                 if (value[2] && value[3]) {
                     query.queryParsed[tokenIdx].args.push(tuple(value[2], value[3]));
                     query.queryParsed[tokenIdx].value = value[3];
+
+                } else {
+                    query.queryParsed[tokenIdx].value = value[1];
                 }
                 query.query = List.map(v => v.value, query.queryParsed).join(' ');
 
