d01e47e44926c7dcd6b71b6a9561d1d59436b063
Tomas Machalek <tomas@machalek.xyz>
2021-01-22 15:40:29 +0100

    Add support for integration_db

--- lib/plugins/mysql_conc_persistence/scripts/import.py
+++ lib/plugins/mysql_conc_persistence/scripts/import.py
@@ -1,3 +1,23 @@
+# Copyright (c) 2021 Charles University, Faculty of Arts,
+#                    Institute of the Czech National Corpus
+# Copyright (c) 2021 Martin Zimandl <martin.zimandl@gmail.com>
+# Copyright (c) 2021 Tomas Machalek <tomas.machalek@gmail.com>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; version 2
+# dated June, 1991.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+
 import sys
 import os
 
@@ -7,35 +27,36 @@ sys.path.insert(0, os.path.realpath('%s/../../../../scripts/' %
 
 import sqlite3
 import autoconf
-import archive
 import argparse
 import datetime
+import initializer
+initializer.init_plugin('integration_db')
+import plugins
 
 
 def import_sqlite_db(db_path, chunk_size):
     ucnk_db = sqlite3.connect(db_path)
 
     cursor = ucnk_db.cursor()
-    mysql_db = archive.MySQLOps(archive.MySQLConf(autoconf.settings))
-
-    cursor.execute('SELECT id, data, created, num_access, last_access FROM archive')
-    while True:
-        data = cursor.fetchmany(chunk_size)
-        if len(data):
-            mysql_db.executemany(
-                'INSERT IGNORE INTO kontext_conc_persistence (id, data, created, num_access, last_access) '
-                'VALUES (%s, %s, %s, %s, %s)',
-                [(
-                    d[0],
-                    d[1],
-                    datetime.datetime.fromtimestamp(d[2]).isoformat(),
-                    d[3],
-                    datetime.datetime.fromtimestamp(d[4]).isoformat() if d[4] else None
-                ) for d in data]
-            )
-            mysql_db.commit()
-        else:
-            break
+    with plugins.runtime.INTEGRATION_DB as mysql_db:
+        cursor.execute('SELECT id, data, created, num_access, last_access FROM archive')
+        while True:
+            data = cursor.fetchmany(chunk_size)
+            if len(data):
+                mysql_db.executemany(
+                    'INSERT IGNORE INTO kontext_conc_persistence (id, data, created, num_access, last_access) '
+                    'VALUES (%s, %s, %s, %s, %s)',
+                    [(
+                        d[0],
+                        d[1],
+                        datetime.datetime.fromtimestamp(d[2]).isoformat(),
+                        d[3],
+                        datetime.datetime.fromtimestamp(d[4]).isoformat() if d[4] else None
+                    ) for d in data]
+                )
+                mysql_db.commit()
+            else:
+                break
 
     cursor.close()
     ucnk_db.close()
