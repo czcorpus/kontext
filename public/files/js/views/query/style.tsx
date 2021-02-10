221c2261e9b2e9034f22b078de0d0cc60eb50a24
Tomas Machalek <tomas@machalek.xyz>
2021-02-10 13:53:14 +0100

    Add missing files

---
+++ public/files/js/views/query/style.tsx
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2021 Charles University in Prague, Faculty of Arts,
+ *                    Institute of the Czech National Corpus
+ * Copyright (c) 2021 Tomas Machalek <tomas.machalek@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2
+ * dated June, 1991.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+import styled from 'styled-components';
+import * as theme from '../theme/default';
+
+
+export const ExpandableSectionLabel = styled.h2`
+    margin: 0 0 0.4em 0;
+    padding: 0.2em;
+    font-size: 1.05em;
+    font-weight: normal;
+
+    .ExpandButton {
+        margin-right: 0.7em;
+    }
+
+    a,
+    span {
+        font-weight: normal;
+        color: ${theme.colorLogoBlue};
+        text-decoration: none;
+    }
+
+    a:hover {
+        text-decoration: underline;
+    }
+`;
