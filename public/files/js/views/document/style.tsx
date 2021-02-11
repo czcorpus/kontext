2852a52892c8834d3945a07001a357b921aa688f
Martin Zimandl <martin.zimandl@gmail.com>
2021-02-11 12:29:00 +0100

    styled components improvements

--- public/files/js/views/document/style.tsx
+++ public/files/js/views/document/style.tsx
@@ -35,85 +35,6 @@ export const ModalOverlay = styled.div`
     width: 100%;
     height: 100%;
     background-color: RGBA(20, 20, 20, 0.70);
-
-    > .tooltip-box, .closeable-frame {
-        position: absolute;
-        top: 30%;
-        left: 50%;
-        transform: translate(-50%,-30%);
-    }
-
-    > .tooltip-box {
-        min-width: 70%;
-        box-shadow: 3px 3px 4px #444;
-        background-color: ${theme.colorSectionBg};
-    }
-
-    .tooltip-box.async-task-list {
-        width: 90%;
-        font-size: 0.8em;
-
-        table {
-            border-spacing: 0;
-
-            td, th {
-                text-align: left;
-                padding: 0.3em 0.7em 0.3em 0.8em;
-                border-color: ${theme.colorDefaultGreen};
-                border-width: 0 1pt 0 0;
-                border-style: solid;
-            }
-
-            td {
-                min-width: 10em;
-                max-width: 0;
-                overflow: hidden;
-                text-overflow: ellipsis;
-                white-space: nowrap;
-            }
-
-            td.status {
-                width: 5%;
-                img {
-                    width: 1em;
-                    margin-left: 0.4em;
-                }
-            }
-
-            td.task-type {
-                width: 15%;
-            }
-
-            td.label {
-                width: 20%;
-            }
-
-            td.datetime {
-                width: 15%;
-            }
-
-            td.msg {
-                width: 45%;
-            }
-
-            th {
-                color: ${theme.colorDefaultText};
-                background-color: ${theme.colorDefaultGreen};
-                font-weight: bold;
-                padding-top: 1em;
-                padding-bottom: 0.5em;
-            }
-
-            tr:nth-child(odd) {
-                background-color: ${theme.colorLightGreen};
-            }
-        }
-
-        div.options {
-            margin-top: 1em;
-            margin-bottom: 1em;
-        }
-    }
 `;
 
 // ---------------- <CloseableFrame /> --------------------------------------
@@ -127,6 +48,13 @@ export const CloseableFrame = styled.section`
     display: flex;
     flex-direction: column;
     flex-wrap: nowrap;
+
+    ${ModalOverlay} > & {
+        position: absolute;
+        top: 30%;
+        left: 50%;
+        transform: translate(-50%,-30%);
+    }
 
     > .heading {
         padding: 0.6em 0.8em;
@@ -166,6 +94,14 @@ export const CloseableFrame = styled.section`
         .padded-contents {
             padding: 1.7em 1.4em 1em 1.4em;
         }
+    }
+
+    @media screen and (max-width: 1366px) {
+        max-width: 90%;
+    }
+
+    @media screen and (max-width: 479px) {
+        max-width: 100%;
     }
 `;
 
@@ -247,5 +183,120 @@ export const TooltipBox = styled.section`
 
     code {
         color: ${theme.colorLogoPink};
+    }
+
+    &.hint {
+        max-width: 35em;
+    }
+
+    &.metadata-detail {
+        ul {
+            margin: 0;
+            padding: 0;
+            list-style-type: none;
+
+            li {
+                padding: 0.1em 0;
+                margin: 0;
+            }
+        }
+        .message {
+            margin: 0.6em;
+            background-image: url(../img/warning-icon.svg);
+            background-repeat: no-repeat;
+            background-size: 1em;
+            background-position: 0 0.2em;
+
+            p:first-letter {
+                padding-left: 1.6em;
+            }
+        }
+    }
+
+    &.hidden {
+        display: none;
+    }
+
+    &.centered {
+        left: 19%;
+        right: 19%;
+    }
+
+    ${ModalOverlay} {
+        > & {
+            position: absolute;
+            top: 30%;
+            left: 50%;
+            transform: translate(-50%,-30%);
+            min-width: 70%;
+            box-shadow: 3px 3px 4px #444;
+            background-color: ${theme.colorSectionBg};
+        }
+
+        &.async-task-list {
+            width: 90%;
+            font-size: 0.8em;
+
+            table {
+                border-spacing: 0;
+
+                td, th {
+                    text-align: left;
+                    padding: 0.3em 0.7em 0.3em 0.8em;
+                    border-color: ${theme.colorDefaultGreen};
+                    border-width: 0 1pt 0 0;
+                    border-style: solid;
+                }
+
+                td {
+                    min-width: 10em;
+                    max-width: 0;
+                    overflow: hidden;
+                    text-overflow: ellipsis;
+                    white-space: nowrap;
+                }
+
+                td.status {
+                    width: 5%;
+                    img {
+                        width: 1em;
+                        margin-left: 0.4em;
+                    }
+                }
+
+                td.task-type {
+                    width: 15%;
+                }
+
+                td.label {
+                    width: 20%;
+                }
+
+                td.datetime {
+                    width: 15%;
+                }
+
+                td.msg {
+                    width: 45%;
+                }
+
+                th {
+                    color: ${theme.colorDefaultText};
+                    background-color: ${theme.colorDefaultGreen};
+                    font-weight: bold;
+                    padding-top: 1em;
+                    padding-bottom: 0.5em;
+                }
+
+                tr:nth-child(odd) {
+                    background-color: ${theme.colorLightGreen};
+                }
+            }
+
+            div.options {
+                margin-top: 1em;
+                margin-bottom: 1em;
+            }
+        }
     }
 `;
