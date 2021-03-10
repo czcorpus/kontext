96be43e5a558eb17c92a9acf9871ccdacbc463e6
Tomas Machalek <tomas@machalek.xyz>
2021-03-10 11:57:41 +0100

    Fix styled component

--- public/files/js/views/freqs/dataRows/style.tsx
+++ public/files/js/views/freqs/dataRows/style.tsx
@@ -24,7 +24,7 @@ import * as theme from '../../theme/default';
 
 // -------------- <DataRowTR /> --------------
 
-export const DataRowTR = styled.form`
+export const DataRowTR = styled.tr`
 
     .bar {
         background-color: ${theme.colorLogoBlue};
