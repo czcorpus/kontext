52898939f5197d0abea6d48eb3f8c2d0bdae7861
Tomas Machalek <tomas@machalek.xyz>
2021-04-01 11:44:07 +0200

    Fix #4033

--- public/files/js/models/query/actions.ts
+++ public/files/js/models/query/actions.ts
@@ -359,8 +359,8 @@ export namespace Actions {
         sourceId:string;
         query:string;
         insertRange:[number, number]|null;
-        rawAnchorIdx:number;
-        rawFocusIdx:number;
+        rawAnchorIdx:number|null;
+        rawFocusIdx:number|null;
     }> {
         name:ActionName.QueryInputSetQuery;
     }
