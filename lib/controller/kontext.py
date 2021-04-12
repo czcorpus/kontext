d56690cb4f423abdb89cefd236d75ccfd4712036
Martin Zimandl <martin.zimandl@gmail.com>
2021-04-12 12:56:02 +0200

    ws docker support

--- lib/controller/kontext.py
+++ lib/controller/kontext.py
@@ -1009,7 +1009,8 @@ class Kontext(Controller):
             'HTTP_USER_AGENT', '').lower()
         if 'popup_server_messages' not in result:
             result['popup_server_messages'] = True
-        result['job_status_service_url'] = settings.get('calc_backend', 'status_service_url', None)
+        result['job_status_service_url'] = os.environ.get('STATUS_SERVICE_URL', settings.get('calc_backend', 'status_service_url', None))
+        logging.error(os.environ.get('STATUS_SERVICE_URL'))
         return result
 
     def _human_readable_corpname(self):
