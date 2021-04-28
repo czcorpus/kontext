ca1cd2bb014f4da131f11cd693bb9efb4b2893e7
Martin Zimandl <martin.zimandl@gmail.com>
2021-04-28 17:36:09 +0200

    Added build and instalation script for minimal ffmpeg support

---
+++ scripts/install/install-min-ffmpeg.sh
@@ -0,0 +1,8 @@
+cd /opt
+sudo apt-get install libmp3lame-dev
+wget -O ffmpeg-snapshot.tar.bz2 https://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2
+tar xjvf ffmpeg-snapshot.tar.bz2
+cd ffmpeg
+./configure --disable-everything --disable-network --disable-autodetect --enable-small --enable-decoder=aac*,ac3*,opus,vorbis --enable-muxer=mp3,mp4 --enable-protocol=file --enable-libmp3lame --enable-encoder=libmp3lame --enable-filter=aresample --disable-x86asm
+make
+make install
