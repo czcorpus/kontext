6a4c3328ba0e6af10c1434bf6ebf3c501a58535f
Martin Zimandl <martin.zimandl@gmail.com>
2021-08-02 15:46:51 +0200

    query history shows always and never queries

--- lib/plugins/default_query_history/__init__.py
+++ lib/plugins/default_query_history/__init__.py
@@ -216,6 +216,7 @@ class QueryHistory(AbstractQueryHistory):
                     tmp = {'corpname': stored['corpora'][0], 'aligned': []}
                     tmp['human_corpname'] = corpora.corpus(tmp['corpname']).get_conf('NAME')
                     q_join = []
+
                     for q in stored.get('form', {}).get('conc_ids', []):
                         stored_q = self._query_persistence.open(q)
                         if stored_q is None:
@@ -224,6 +225,33 @@ class QueryHistory(AbstractQueryHistory):
                         else:
                             for qs in stored_q.get('lastop_form', {}).get('curr_queries', {}).values():
                                 q_join.append(f'{{ {qs} }}')
+
+                    q_subset = stored.get('form', {}).get('conc_subset_complements', None)
+                    if q_subset is not None:
+                        logging.error(q_subset)
+                        for q in q_subset.get('conc_ids', []):
+                            max_ratio = q_subset.get('max_non_matching_ratio', 0)
+                            stored_q = self._query_persistence.open(q)
+                            logging.error(stored_q)
+                            if stored_q is None:
+                                logging.getLogger(__name__).warning(
+                                    'Missing conc for pquery subset: {}'.format(q))
+                            else:
+                                query = stored_q['lastop_form']['query']
+                                q_join.append(f'!{max_ratio if max_ratio else ""}{{ {query} }}')
+
+                    q_superset = stored.get('form', {}).get('conc_superset', None)
+                    if q_superset is not None:
+                        for q in q_superset.get('conc_ids', []):
+                            max_ratio = q_superset.get('max_non_matching_ratio', 0)
+                            stored_q = self._query_persistence.open(q)
+                            if stored_q is None:
+                                logging.getLogger(__name__).warning(
+                                    'Missing conc for pquery superset: {}'.format(q))
+                            else:
+                                query = stored_q['lastop_form']['query']
+                                q_join.append(f'?{max_ratio if max_ratio else ""}{{ {query} }}')
+
                     tmp['query'] = ' && '.join(q_join)
                     tmp.update(item)
                     tmp.update(stored)
