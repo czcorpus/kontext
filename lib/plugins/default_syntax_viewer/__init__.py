48484a652807976e976c7a01bc3ffabb54cf0e9f
Tomas Machalek <tomas@machalek.xyz>
2021-08-04 12:58:17 +0200

    Handle parsing errors

--- lib/plugins/default_syntax_viewer/__init__.py
+++ lib/plugins/default_syntax_viewer/__init__.py
@@ -114,12 +114,20 @@ def load_plugin_conf_from_file(plugin_conf):
 
 
 def load_plugin_conf_from_db(db: IntegrationDatabase, corp_table='kontext_corpus') -> Dict[str, str]:
+
+    def parse_conf(corp, src):
+        try:
+            return json.loads(src)
+        except Exception as ex:
+            logging.getLogger(__name__).warning(f'Failed to load syntax viewer conf for {corp}: {ex}')
+            return None
+
     cursor = db.cursor()
     cursor.execute(
         'SELECT name, syntax_viewer_conf_json '
         f'FROM {corp_table} '
         'WHERE syntax_viewer_conf_json IS NOT NULL')
-    return {row['name']: json.loads(row['syntax_viewer_conf_json']) for row in cursor}
+    return {row['name']: parse_conf(row['syntax_viewer_conf_json']) for row in cursor}
 
 
 @plugins.inject(plugins.runtime.AUTH, plugins.runtime.INTEGRATION_DB)
