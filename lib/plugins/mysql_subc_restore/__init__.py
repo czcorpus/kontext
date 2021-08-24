3d60945b93870c45a176af7614ff38bc7dc9e9d3
Martin Zimandl <martin.zimandl@gmail.com>
2021-08-24 13:18:28 +0200

    added mysql_subc_restore module and removed ucnk_subc_restore module

--- lib/plugins/mysql_subc_restore/__init__.py
+++ lib/plugins/mysql_subc_restore/__init__.py
@@ -1,4 +1,6 @@
-# Copyright (c) 2014 Institute of the Czech National Corpus
+# Copyright (c) 2021 Charles University in Prague, Faculty of Arts,
+#                    Institute of the Czech National Corpus
+# Copyright (c) 2021 Martin Zimandl <martin.zimandl@gmail.com>
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -10,101 +12,90 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
-"""
-A SQLite3-based subc_restore implementation.
 
-required schema:
-
-CREATE TABLE subc_archive (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  user_id INTEGER NOT NULL,
-  corpname TEXT NOT NULL,
-  subcname TEXT NOT NULL,
-  cql TEXT NOT NULL,
-  timestamp INTEGER NOT NULL
-);
-
-required entry in config.xml: please see config.rng
-
-"""
-
-import time
+from controller.plg import PluginCtx
+from datetime import datetime
+from typing import Any, Dict, List, Optional
 import urllib.request
 import urllib.parse
 import urllib.error
 import logging
+from mysql.connector.connection import MySQLConnection
 
 import werkzeug.urls
 import plugins
+from plugins.abstract.corparch import AbstractCorporaArchive
 from plugins.abstract.subc_restore import AbstractSubcRestore
 from plugins import inject
-import sqlite3
 
 
-class SQLite3Ops(object):
-    def __init__(self, db_path):
-        self._db = sqlite3.connect(db_path)
-        self._db.row_factory = sqlite3.Row
-
-    def execute(self, sql, args):
-        cursor = self._db.cursor()
-        cursor.execute(sql, args)
-        return cursor
-
-    def commit(self):
-        self._db.commit()
-
-
-class UCNKSubcRestore(AbstractSubcRestore):
+class MySQLSubcRestore(AbstractSubcRestore):
     """
     For the documentation of individual methods, please see AbstractSubcRestore class
     """
 
-    COLS = ('id', 'user_id', 'corpname', 'subcname', 'cql', 'timestamp')
+    TABLE_NAME = 'kontext_subc_archive'
 
-    def __init__(self, conf, corparch):
-        self._conf = conf
+    def __init__(self, plugin_conf: Dict[str, Any], corparch: AbstractCorporaArchive, db: MySQLConnection):
+        self._conf = plugin_conf
         self._corparch = corparch
-        self._db = SQLite3Ops(self._conf.get('plugins')['subc_restore']['db_path'])
+        self._db = db
 
-    def store_query(self,  user_id, corpname, subcname, cql):
-        self._db.execute('INSERT INTO subc_archive (user_id, corpname, subcname, cql, timestamp) '
-                         'VALUES (?, ?, ?, ?, ?)', (user_id, corpname, subcname, cql, int(time.time())))
+    def store_query(self, user_id: int, corpname: str, subcname: str, cql: str):
+        with self._db.cursor() as cursor:
+            cursor.execute(
+                f'INSERT INTO {self.TABLE_NAME} '
+                '(user_id, corpname, subcname, cql, created) '
+                'VALUES (%s, %s, %s, %s, %s)',
+                (user_id, corpname, subcname, cql, datetime.now())
+            )
         self._db.commit()
 
-    def delete_query(self, user_id, corpname, subcname):
-        self._db.execute('DELETE FROM subc_archive WHERE user_id = ? AND corpname = ? AND subcname = ?',
-                         (user_id, corpname, subcname))
+    def delete_query(self, user_id: int, corpname: str, subcname: str):
+        with self._db.cursor() as cursor:
+            cursor.execute(
+                f'DELETE FROM {self.TABLE_NAME} '
+                'WHERE user_id = %s AND corpname = %s AND subcname = %s',
+                (user_id, corpname, subcname)
+            )
         self._db.commit()
 
-    def list_queries(self, user_id, from_idx, to_idx=None):
-        to_idx = -1 if to_idx is None else to_idx - from_idx
-        ans = self._db.execute('SELECT %s ' % ', '.join(self.COLS) +
-                               'FROM subc_archive WHERE user_id = ? ORDER BY id LIMIT ?, ?',
-                               (user_id, from_idx, to_idx)).fetchall()
-        result = []
-        for item in ans:
-            result.append(dict(list(zip(self.COLS, item))))
-        return result
-
-    def get_info(self, user_id, corpname, subcname):
-        ans = self._db.execute('SELECT %s ' % ', '.join(self.COLS) +
-                               'FROM subc_archive WHERE user_id = ? AND corpname = ? AND subcname = ? ' +
-                               'ORDER BY timestamp DESC LIMIT 1', (user_id, corpname, subcname, )).fetchone()
-        if ans:
-            return dict(list(zip(self.COLS, ans)))
+    def list_queries(self, user_id: int, from_idx: int, to_idx: Optional[int] = None) -> List[Dict[str, Any]]:
+        sql = [
+            'SELECT * FROM kontext_subc_archive',
+            'WHERE user_id = %s ORDER BY id',
+        ]
+        args = (user_id,)
+        if to_idx is not None:
+            sql.append('LIMIT %s, %s')
+            args += (from_idx, to_idx - from_idx)
         else:
-            return None
+            sql.append('OFFSET %s ROWS')
+            args += (from_idx,)
 
-    def get_query(self, query_id):
-        ans = self._db.execute('SELECT %s ' % ', '.join(self.COLS) +
-                               'FROM subc_archive WHERE id = ?', (query_id, )).fetchone()
-        if ans:
-            return dict(list(zip(self.COLS, ans)))
-        else:
-            return None
+        with self._db.cursor() as cursor:
+            cursor.execute(' '.join(sql), args)
+            return cursor.fetchall()
 
-    def extend_subc_list(self, plugin_ctx, subc_list, filter_args, from_idx, to_idx=None, include_cql=False):
+    def get_info(self, user_id: int, corpname: str, subcname: str) -> Dict[str, Any]:
+        with self._db.cursor() as cursor:
+            cursor.execute(
+                f'SELECT * FROM {self.TABLE_NAME} '
+                'WHERE user_id = %s AND corpname = %s AND subcname = %s '
+                'ORDER BY created',
+                (user_id, corpname, subcname)
+            )
+            return cursor.fetchone()
+
+    def get_query(self, query_id: int) -> Dict[str, Any]:
+        with self._db.cursor() as cursor:
+            cursor.execute(
+                f'SELECT * FROM {self.TABLE_NAME} '
+                'WHERE id = %s', (query_id, )
+            )
+            return cursor.fetchone()
+
+    def extend_subc_list(self, plugin_ctx: PluginCtx, subc_list: List[Dict[str, Any]], filter_args: Dict[str, Any], from_idx: int, to_idx: Optional[int]=None, include_cql: bool=False) -> List[Dict[str, Any]]:
         """
         Enriches KonText's original subcorpora list by the information about queries which
         produced these subcorpora. It it also able to insert an information about deleted
@@ -123,7 +114,7 @@ class UCNKSubcRestore(AbstractSubcRestore):
         Returns:
             list of dict: a new list containing both the original subc_list and also the extended part
         """
-        def get_user_subcname(rec):
+        def get_user_subcname(rec: Dict[str, Any]) -> str:
             return rec.get('orig_subcname') if rec.get('orig_subcname') else rec.get('usesubcorp')
 
         subc_queries = self.list_queries(plugin_ctx.user_id, from_idx, to_idx)
@@ -137,11 +128,11 @@ class UCNKSubcRestore(AbstractSubcRestore):
         else:
             deleted_keys = []
 
-        def corpname_matches(cn):
+        def corpname_matches(cn: str) -> bool:
             filter_cn = filter_args.get('corpname', None)
             return not filter_cn or cn == filter_cn
 
-        def escape_subcname(s):
+        def escape_subcname(s: str) -> str:
             return werkzeug.urls.url_quote(s, unsafe='+')
 
         deleted_items = []
@@ -153,7 +144,7 @@ class UCNKSubcRestore(AbstractSubcRestore):
                     deleted_items.append({
                         'name': '{0} / {1}'.format(corpus_info.id, subc_queries_map[dk]['subcname']),
                         'size': None,
-                        'created': subc_queries_map[dk]['timestamp'],
+                        'created': int(subc_queries_map[dk]['created'].timestamp()),
                         'human_corpname': corpus_info.name,
                         'corpname': subc_queries_map[dk]['corpname'],
                         'usesubcorp': escape_subcname(subc_queries_map[dk]['subcname']),
@@ -176,6 +167,11 @@ class UCNKSubcRestore(AbstractSubcRestore):
         return subc_list + deleted_items
 
 
-@inject(plugins.runtime.CORPARCH)
-def create_instance(conf, corparch):
-    return UCNKSubcRestore(conf, corparch)
+@inject(plugins.runtime.CORPARCH, plugins.runtime.INTEGRATION_DB)
+def create_instance(conf, corparch, integ_db):
+    plugin_conf = conf.get('plugins', 'subc_restore')
+    if integ_db.is_active:
+        logging.getLogger(__name__).info(f'mysql_subc_restore uses integration_db[{integ_db.info}]')
+        return MySQLSubcRestore(plugin_conf, corparch, integ_db)
+    else:
+        logging.getLogger(__name__).error('mysql_subc_restore - integration DB not provided!')
