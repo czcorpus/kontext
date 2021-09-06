7e8a093acb38d9be7fcb0e9b5af8ba0ededb97c1
Martin Zimandl <martin.zimandl@gmail.com>
2021-09-06 17:13:26 +0200

    get overlaping regions of details to get elements in between them

--- public/files/js/plugins/defaultKwicConnect/renderers.tsx
+++ public/files/js/plugins/defaultKwicConnect/renderers.tsx
@@ -20,7 +20,7 @@
 import * as React from 'react';
 import * as Kontext from '../../types/kontext';
 import { IActionDispatcher } from 'kombo';
-import { List, pipe, tuple } from 'cnc-tskit';
+import { List } from 'cnc-tskit';
 import { Actions as QueryActions } from '../../models/query/actions';
 
 
