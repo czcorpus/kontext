1a8391c1236e1be578a392f7f43132988260dca6
Martin Zimandl <martin.zimandl@gmail.com>
2021-11-08 16:11:43 +0100

    test line shuffle

--- cypress/integration/concordance/generalViewOptions.js
+++ cypress/integration/concordance/generalViewOptions.js
@@ -12,13 +12,12 @@ describe('Concordance - general view options', () => {
         cy.get('.simple-input').type('and');
         cy.get('.query .default-button').click();
 
-        cy.hoverNthMenuItem(8);
-        cy.clickMenuItem(8, 3);
-
         // check default page size
         cy.get('#conc-dashboard-mount table tbody tr').should('have.length', 40);
 
         // set page size to 10
+        cy.hoverNthMenuItem(8);
+        cy.clickMenuItem(8, 3);
         cy.get('#view-options-mount .FieldsetConcordance #conc-page-size').clear().type(10);
         cy.get('#view-options-mount div.buttons button').click();
 
@@ -30,14 +29,13 @@ describe('Concordance - general view options', () => {
         cy.get('.simple-input').type('and');
         cy.get('.query .default-button').click();
 
-        cy.hoverNthMenuItem(8);
-        cy.clickMenuItem(8, 3);
-
         // check default kwic size
         cy.get('#conc-dashboard-mount table tbody tr').first().find('td.lc').children().should('have.length', 10);
         cy.get('#conc-dashboard-mount table tbody tr').first().find('td.rc').children().should('have.length', 10);
 
         // set kwic size to 5
+        cy.hoverNthMenuItem(8);
+        cy.clickMenuItem(8, 3);
         cy.get('#view-options-mount .FieldsetConcordance #conc-kwic-size').clear().type(5);
         cy.get('#view-options-mount div.buttons button').click();
 
@@ -50,15 +48,14 @@ describe('Concordance - general view options', () => {
         cy.get('.simple-input').type('and');
         cy.get('.query .default-button').click();
 
-        cy.hoverNthMenuItem(8);
-        cy.clickMenuItem(8, 3);
-
         // check default line numbers options
         cy.get('#conc-dashboard-mount table tbody tr:nth-child(1) td.line-num').should('contain.text', '');
         cy.get('#conc-dashboard-mount table tbody tr:nth-child(2) td.line-num').should('contain.text', '');
         cy.get('#conc-dashboard-mount table tbody tr:nth-child(3) td.line-num').should('contain.text', '');
 
         // turn on line numbers
+        cy.hoverNthMenuItem(8);
+        cy.clickMenuItem(8, 3);
         cy.get('#view-options-mount .FieldsetConcordance label[for="show-line-numbers-input"]').click();
         cy.get('#view-options-mount div.buttons button').click();
 
@@ -66,6 +63,29 @@ describe('Concordance - general view options', () => {
         cy.get('#conc-dashboard-mount table tbody tr:nth-child(1) td.line-num').should('contain.text', '1');
         cy.get('#conc-dashboard-mount table tbody tr:nth-child(2) td.line-num').should('contain.text', '2');
         cy.get('#conc-dashboard-mount table tbody tr:nth-child(3) td.line-num').should('contain.text', '3');
+    });
+
+    it('tests line shuffle', () => {
+        cy.get('.simple-input').type('and');
+        cy.get('.query .default-button').click();
+
+        // line shuffeling
+        cy.hoverNthMenuItem(8);
+        cy.clickMenuItem(8, 3);
+        cy.get('#view-options-mount .FieldsetConcordance label[for="always-shuffle"]').click();
+        cy.get('#view-options-mount div.buttons button').click();
+        
+        // take value of first left kwic
+        cy.get('#conc-dashboard-mount table tbody tr:nth-child(1) td.lc').invoke('text').then(text => {
+            // run new query
+            cy.hoverNthMenuItem(1);
+            cy.clickMenuItem(1, 1);
+            cy.get('.simple-input').type('and');
+            cy.get('.query .default-button').click();
+
+            // check new line numbers options
+            cy.get('#conc-dashboard-mount table tbody tr:nth-child(1) td.lc').should('not.have.text', text);
+        });
     });
 
 });
