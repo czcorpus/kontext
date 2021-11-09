c34fc3089f813d4fe15cf27f04f31f930e9e69f2
Martin Zimandl <martin.zimandl@gmail.com>
2021-11-09 16:27:55 +0100

    query history supertype filter test

---
+++ cypress/integration/queryHistory.js
@@ -0,0 +1,71 @@
+/// <reference types="cypress" />
+
+
+describe('Query History', () => {
+
+    before(() => {
+        cy.actionLogin();
+        
+        // concordance query
+        cy.hoverNthMenuItem(1);
+        cy.clickMenuItem(1, 1);
+        cy.get('.simple-input').type('London');
+        cy.get('.query .default-button').click().then(() => {
+            
+            // paradigmatic query
+            cy.hoverNthMenuItem(1);
+            cy.clickMenuItem(1, 2);
+            cy.get('#pquery-form-mount .cql-input').eq(0).type('[word="e.*"]');
+            cy.get('#pquery-form-mount .cql-input').eq(1).type('[tag="N.*"]');
+            cy.get('#pquery-form-mount .submit').click().then(() => {
+                
+                // word list query
+                cy.hoverNthMenuItem(1);
+                cy.clickMenuItem(1, 3);
+                cy.get('#wl-pattern-input').type('.*ining');
+                cy.get('#wordlist-form-mount .default-button').click();
+            });
+        });
+
+        cy.actionLogout();
+    });
+
+    beforeEach(() => {
+        cy.actionLogin();
+        
+        cy.hoverNthMenuItem(1);
+        cy.clickMenuItem(1, 4);
+    });
+
+    it('tests supertype filter', () => {
+
+        // test any supertype
+        let history = cy.get('#query-history-mount .history-entries .supertype');
+        history.should('not.be.empty');
+        history.should('contain.text', 'concordance');
+        history.should('contain.text', 'word list');
+        history.should('contain.text', 'paradigmatic query');
+
+        // test concordance supertype
+        cy.get('#query-history-mount fieldset label select').select('concordance');
+        history = cy.get('#query-history-mount .history-entries .supertype');
+        history.should('contain.text', 'concordance');
+        history.should('not.contain.text', 'paradigmatic query');
+        history.should('not.contain.text', 'word list');
+
+        // test paradigmatic query supertype
+        cy.get('#query-history-mount fieldset label select').select('paradigmatic query');
+        history = cy.get('#query-history-mount .history-entries .supertype');
+        history.should('not.contain.text', 'concordance');
+        history.should('contain.text', 'paradigmatic query');
+        history.should('not.contain.text', 'word list');
+
+        // test word list supertype
+        cy.get('#query-history-mount fieldset label select').select('word list');
+        history = cy.get('#query-history-mount .history-entries .supertype');
+        history.should('not.contain.text', 'concordance');
+        history.should('not.contain.text', 'paradigmatic query');
+        history.should('contain.text', 'word list');
+    });
+
+});
