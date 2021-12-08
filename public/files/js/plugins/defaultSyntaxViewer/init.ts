25cece936035fd7f6d2e1adcb23187cf85abbb9e
Martin Zimandl <martin.zimandl@gmail.com>
2021-12-08 11:19:42 +0100

    added corpus select to syntax viewer

--- public/files/js/plugins/defaultSyntaxViewer/init.ts
+++ public/files/js/plugins/defaultSyntaxViewer/init.ts
@@ -32,6 +32,7 @@ import './js-treex-view';
 import { IFullActionControl } from 'kombo';
 import { concatMap } from 'rxjs/operators';
 import { HTTP, List, pipe } from 'cnc-tskit';
+import { Actions } from './actions';
 import { Actions as ConcActions } from '../../models/concordance/actions';
 import { IPluginApi } from '../../types/plugins/common';
 require('./style.css'); // webpack
@@ -51,6 +52,8 @@ export class SyntaxTreeViewer extends StatefulModel<SyntaxTreeViewerState> imple
     private resizeThrottleTimer:number;
 
     private errorHandler:(e:Error)=>void;
+
+    private corpusSelectHandler:(e)=>void;
 
     constructor(dispatcher:IFullActionControl, pluginApi:IPluginApi) {
         super(
@@ -85,6 +88,25 @@ export class SyntaxTreeViewer extends StatefulModel<SyntaxTreeViewerState> imple
                 this.render(this.state);
             }
         );
+
+        this.addActionHandler<typeof Actions.SwitchCorpus>(
+            Actions.SwitchCorpus.name,
+            action => {
+                this.changeState(state => {
+                    state.selected = action.payload.corpusId;
+                });
+                this.render(this.state);
+            }
+        );
+
+        this.corpusSelectHandler = (e) => {
+            dispatcher.dispatch<typeof Actions.SwitchCorpus>({
+                name: Actions.SwitchCorpus.name,
+                payload: {
+                    corpusId: e.target.value
+                }
+            });
+        };
     }
 
     isActive():boolean {
@@ -160,11 +182,25 @@ export class SyntaxTreeViewer extends StatefulModel<SyntaxTreeViewerState> imple
         while (target.firstChild) {
             target.removeChild(target.firstChild);
         }
+
+        const corpusSwitch = window.document.createElement('select');
+        corpusSwitch.onchange = this.corpusSelectHandler;
+        for (let index = 0; index < List.size(this.state.corpnames); index++) {
+            const value = this.state.corpnames[index];
+            const option = window.document.createElement('option');
+            option.value = value;
+            option.label = value;
+            option.selected = value === this.state.selected;
+            $(corpusSwitch).append(option);                       
+        }
+
         const treexFrame = window.document.createElement('div');
         treexFrame.style['width'] = `${(window.innerWidth - 55).toFixed(0)}px`;
         treexFrame.style['height'] = `${(window.innerHeight - 70).toFixed(0)}px`;
         treexFrame.style['overflow'] = 'auto';
 
+        $(target).append(corpusSwitch);
+        $(target).append(window.document.createElement('hr'));
         $(target).append(treexFrame);
         $(treexFrame).treexView(this.state.data);
     }
