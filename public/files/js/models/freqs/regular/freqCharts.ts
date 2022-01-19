593b00ffc09b3d6bacc39226de181ed5048ac5b1
Martin Zimandl <martin.zimandl@gmail.com>
2022-01-19 10:41:18 +0100

    request fixes

--- public/files/js/models/freqs/regular/freqCharts.ts
+++ public/files/js/models/freqs/regular/freqCharts.ts
@@ -280,16 +280,16 @@ export class FreqChartsModel extends StatelessModel<FreqChartsModelState> {
         });
     }
 
-    getSubmitArgs(state:FreqChartsModelState, sourceId:string):FreqServerArgs {
+    getSubmitArgs(state:FreqChartsModelState, fcrit:string):FreqServerArgs {
         return {
             ...this.pageModel.getConcArgs(),
-            fcrit: state.freqCrit,
+            fcrit,
             flimit: parseInt(state.flimit),
-            freq_sort: state.sortColumn[sourceId],
-            fpage: state.currentPage[sourceId],
+            freq_sort: state.sortColumn[fcrit],
+            fpage: state.currentPage[fcrit],
             ftt_include_empty: state.ftt_include_empty,
             freqlevel: 1,
-            fmaxitems: state.fmaxitems,
+            fmaxitems: state.fmaxitems[fcrit],
             format: 'json'
         };
     }
