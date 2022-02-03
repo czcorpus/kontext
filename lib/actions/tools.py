dfafc968348c8c875dc526e6193e7cf9dcfaaceb
Tomas Machalek <tomas@machalek.xyz>
2022-02-03 11:19:04 +0100

    Add some SVG pre-processing for nicer results
    (there were e.g. missing parts in the margin area)

--- lib/actions/tools.py
+++ lib/actions/tools.py
@@ -14,10 +14,42 @@
 
 from werkzeug import Request
 from cairosvg import svg2png, svg2svg
+from lxml import etree
+from io import BytesIO
+from typing import Tuple
 
 from controller import exposed
 from controller.errors import UserActionException
 from controller.kontext import Kontext
+
+
+def parse_viewbox(vb: str) -> Tuple[int, int, int, int]:
+    items = [int(v) for v in vb.split(' ')]
+    try:
+        return items[0], items[1], items[2], items[3]
+    except IndexError:
+        raise UserActionException(f'Invalid SVG viewBox: {vb}', code=422)
+
+
+def normalize_viewbox(vb: Tuple[int, int, int, int]) -> str:
+    return f'{vb[0]} {vb[1]} {vb[2]+80} {vb[3]+40}'
+
+
+def normalize_svg(src: bytes):
+    doc = etree.parse(BytesIO(src))
+    g_list = doc.findall('./{http://www.w3.org/2000/svg}g')
+    svg_root = doc.getroot()
+    svg_root.attrib['width'] = '100%'
+    svg_root.attrib['height'] = '100%'
+    x1, y1, x2, y2 = parse_viewbox(svg_root.attrib['viewBox'])
+    svg_root.attrib['viewBox'] = normalize_viewbox((x1, y1, x2, y2))
+    root_g = etree.SubElement(doc.getroot(), 'g')
+    root_g.attrib['fill'] = '#ffffff'
+    root_g.attrib['transform'] = 'translate(40, 20)'
+    for item in g_list:
+        item.getparent().remove(item)
+        root_g.append(item)
+    return etree.tostring(doc.getroot())
 
 
 class Tools(Kontext):
@@ -27,12 +59,13 @@ class Tools(Kontext):
 
     @exposed(return_type='plain', http_method='POST', skip_corpus_init=True)
     def convert_svg(self, request: Request):
+        svg_src = normalize_svg(request.get_data())
         if request.args.get('outFormat', '') == 'png':
             self._response.set_header('Content-Type', 'image/png')
-            return svg2png(bytestring=request.get_data(), scale=6, background_color='#FFFFFF')
+            return svg2png(bytestring=svg_src, scale=6, background_color='#FFFFFF')
         elif request.args.get('outFormat', '') == 'svg':
             self._response.set_header('Content-Type', 'image/svg+xml')
-            return svg2svg(bytestring=request.get_data(), scale=6, background_color='#FFFFFF')
+            return svg2svg(bytestring=svg_src, scale=6, background_color='#FFFFFF')
         else:
             raise UserActionException('Invalid data format', code=422)
 
