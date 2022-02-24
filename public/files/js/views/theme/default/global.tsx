fb6e7d81eede4b64cb5646116b9719e193400a33
Martin Zimandl <martin.zimandl@gmail.com>
2022-02-24 12:03:59 +0100

    replace external link png by svg variant

--- public/files/js/views/theme/default/global.tsx
+++ public/files/js/views/theme/default/global.tsx
@@ -21,7 +21,7 @@
 import { createGlobalStyle } from 'styled-components';
 import * as theme from './index';
 
-import externalLinkImg from '../../../../img/external-link.png';
+import externalLinkImg from '../../../../img/external-link.svg';
 
 // ----------------- <GlobalStyle /> -------------------------
 
