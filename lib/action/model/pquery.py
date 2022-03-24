145fbd1dcf710d95b212de7cc3833117ad3acb72
Martin Zimandl <martin.zimandl@gmail.com>
2022-03-24 15:37:18 +0100

    tools view and missing files

---
+++ lib/action/model/pquery.py
@@ -0,0 +1,159 @@
+# Copyright(c) 2022 Charles University, Faculty of Arts,
+#                   Institute of the Czech National Corpus
+# Copyright(c) 2022 Martin Zimandl <martin.zimandl@gmail.com>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; version 2
+# dated June, 1991.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+from typing import Dict, Any, Optional, List, Tuple
+
+import plugins
+from action import ActionProps
+from action.krequest import KRequest
+from action.response import KResponse
+from action.argmapping.conc.query import QueryFormArgs
+from action.argmapping.conc.filter import FilterFormArgs
+from action.argmapping.pquery import PqueryFormArgs
+from action.model.corpus import CorpusActionModel, CorpusPluginCtx
+from action.errors import UserActionException
+from main_menu.model import EventTriggeringItem, MainMenu
+from texttypes.model import TextTypesCache
+import settings
+
+
+class PQueryPluginCtx(CorpusPluginCtx):
+    pass
+
+
+class ParadigmaticQueryActionModel(CorpusActionModel):
+
+    PQUERY_QUICK_SAVE_MAX_LINES = 10000
+
+    TASK_TIME_LIMIT = settings.get_int('calc_backend', 'task_time_limit', 300)
+
+    def __init__(self, req: KRequest, resp: KResponse, action_props: ActionProps, tt_cache: TextTypesCache):
+        super().__init__(req, resp, action_props, tt_cache)
+        self._curr_pquery_args: Optional[PqueryFormArgs] = None
+        self._plugin_ctx: Optional[PQueryPluginCtx] = None
+
+    @property
+    def plugin_ctx(self):
+        if self._plugin_ctx is None:
+            self._plugin_ctx = PQueryPluginCtx(self, self._req, self._resp)
+        return self._plugin_ctx
+
+    async def load_conc_queries(self, conc_ids: List[str], corpus_id: str,
+                                form_type: str) -> Tuple[Dict[str, Any], Dict[str, Any]]:
+        """
+        Load both conc. query forms and respective raw Manatee queries
+
+        form_type is either 'query' or 'filter'
+        """
+        forms = {}
+        raw_queries = {}
+        with plugins.runtime.QUERY_PERSISTENCE as qs:
+            for conc_id in conc_ids:
+                data = await qs.open(conc_id)
+                if data is None:
+                    raise UserActionException(
+                        'Source concordance query does not exist: {}'.format(conc_id))
+                if qs.stored_form_type(data) != form_type:
+                    raise UserActionException('Invalid source query used: {}'.format(conc_id))
+                if form_type == 'query':
+                    args = (await QueryFormArgs.create(
+                        plugin_ctx=self.plugin_ctx, corpora=[corpus_id], persist=True)).updated(data['lastop_form'], conc_id)
+                elif form_type == 'filter':
+                    args = (await FilterFormArgs.create(
+                        plugin_ctx=self.plugin_ctx, maincorp=corpus_id, persist=True)).updated(data['lastop_form'], conc_id)
+                forms[args.op_key] = args.to_dict()
+                raw_queries[args.op_key] = data['q']
+        return forms, raw_queries
+
+    async def export_form_args(self, result):
+        if self._curr_pquery_args:
+            result['pquery_form'] = self._curr_pquery_args.to_dict()
+            result['conc_forms'], _ = await self.load_conc_queries(
+                self._curr_pquery_args.conc_ids, self.args.corpname, 'query')
+            if self._curr_pquery_args.conc_subset_complements:
+                s_forms, _ = await self.load_conc_queries(
+                    self._curr_pquery_args.conc_subset_complements.conc_ids,
+                    self.args.corpname, 'query')
+                result['conc_forms'].update(s_forms)
+            if self._curr_pquery_args.conc_superset:
+                s_forms, _ = await self.load_conc_queries(
+                    [self._curr_pquery_args.conc_superset.conc_id],
+                    self.args.corpname, 'query')
+                result['conc_forms'].update(s_forms)
+        else:
+            result['pquery_form'] = None
+            result['conc_forms'] = {}
+
+    async def get_tagsets(self):
+        corp_info = await self.get_corpus_info(self.args.corpname)
+        return [tagset.to_dict() for tagset in corp_info.tagsets]
+
+    def get_default_attr(self):
+        attrs = self.corp.get_posattrs()
+        return 'lemma' if 'lemma' in attrs else attrs[0]
+
+    async def pre_dispatch(self, action_props: ActionProps):
+        ans = await super().pre_dispatch(action_props)
+        if self._active_q_data is not None:
+            if self._active_q_data.get('form', {}).get('form_type') != 'pquery':
+                raise UserActionException('Invalid search session for a paradimatic query')
+            self._curr_pquery_args = PqueryFormArgs(
+                corpname=self.corp.corpname,
+                attr=self.get_default_attr(),
+                position='0<0~0>0'
+            )
+            self._curr_pquery_args.from_dict(self._active_q_data['form'])
+
+        return ans
+
+    async def post_dispatch(self, action_props: ActionProps, result, err_desc):
+        await super().post_dispatch(action_props, result, err_desc)
+        # create and store concordance query key
+        if action_props.mutates_result:
+            with plugins.runtime.QUERY_HISTORY as qh, plugins.runtime.QUERY_PERSISTENCE as qp:
+                query_id = await qp.store(user_id=self.session_get('user', 'id'),
+                                          curr_data=dict(form=self._curr_pquery_args.to_qp(),
+                                                         corpora=[self._curr_pquery_args.corpname],
+                                                         usesubcorp=self._curr_pquery_args.usesubcorp))
+                ts = await qh.store(
+                    user_id=self.session_get('user', 'id'),
+                    query_id=query_id, q_supertype='pquery')
+                for fn in self._on_query_store:
+                    fn([query_id], ts, result)
+
+    def _add_save_menu_item(self, label: str, save_format: Optional[str] = None, hint: Optional[str] = None):
+        if save_format is None:
+            event_name = 'MAIN_MENU_SHOW_SAVE_FORM'
+            self._dynamic_menu_items.append(
+                EventTriggeringItem(MainMenu.SAVE, label, event_name, key_code=83, key_mod='shift',
+                                    hint=hint).mark_indirect())  # key = 's'
+
+        else:
+            event_name = 'MAIN_MENU_DIRECT_SAVE'
+            self._dynamic_menu_items.append(
+                EventTriggeringItem(
+                    MainMenu.SAVE, label, event_name, hint=hint).add_args(('saveformat', save_format)))
+
+    def add_save_menu(self):
+        self._add_save_menu_item('CSV', save_format='csv',
+                                 hint=self._req.translate(f'Saves at most {self.PQUERY_QUICK_SAVE_MAX_LINES} items. Use "Custom" for more options.'))
+        self._add_save_menu_item('XLSX', save_format='xlsx',
+                                 hint=self._req.translate(f'Saves at most {self.PQUERY_QUICK_SAVE_MAX_LINES} items. Use "Custom" for more options.'))
+        self._add_save_menu_item('XML', save_format='xml',
+                                 hint=self._req.translate(f'Saves at most {self.PQUERY_QUICK_SAVE_MAX_LINES} items. Use "Custom" for more options.'))
+        self._add_save_menu_item(self._req.translate('Custom'))
---
+++ lib/views/pquery.py
@@ -0,0 +1,203 @@
+from typing import Optional
+from dataclasses import dataclass
+import sys
+import time
+
+from sanic import Blueprint
+
+from action.decorators import http_action
+from action.krequest import KRequest
+from action.response import KResponse
+from action.argmapping.pquery import PqueryFormArgs
+from action.errors import NotFoundException
+from action.model.pquery import ParadigmaticQueryActionModel
+import bgcalc
+from bgcalc.pquery import require_existing_pquery
+from bgcalc.pquery.errors import PqueryResultNotFound
+from bgcalc.task import AsyncTaskStatus
+from main_menu import MainMenu
+from main_menu.model import MenuItemInternal
+import plugins
+import settings
+
+
+bp = Blueprint('pquery', url_prefix='pquery')
+
+
+@bp.route('/index')
+@http_action(template='pquery/index.html', page_model='pquery', action_model=ParadigmaticQueryActionModel)
+async def index(amodel: ParadigmaticQueryActionModel, req: KRequest, resp: KResponse):
+    amodel.disabled_menu_items = (MainMenu.FILTER, MainMenu.FREQUENCY,
+                                  MainMenu.COLLOCATIONS, MainMenu.SAVE, MainMenu.CONCORDANCE,
+                                  MainMenu.VIEW('kwic-sent-switch'))
+    ans = {
+        'corpname': amodel.args.corpname,
+        'tagsets': await amodel.get_tagsets(),
+        'pquery_default_attr': amodel.get_default_attr(),
+    }
+    await amodel.export_form_args(ans)
+    amodel.export_subcorpora_list(amodel.args.corpname, amodel.args.usesubcorp, ans)
+    amodel.add_save_menu()
+    return ans
+
+
+@bp.route('/result')
+@http_action(template='pquery/result.html', page_model='pqueryResult', action_model=ParadigmaticQueryActionModel)
+async def result(amodel: ParadigmaticQueryActionModel, req: KRequest, resp: KResponse):
+    pagesize = amodel.args.pqueryitemsperpage
+    page = 1
+    offset = (page - 1) * pagesize
+    corp_info = await amodel.get_corpus_info(amodel.args.corpname)
+    try:
+        total_num_lines, freqs = require_existing_pquery(
+            amodel._curr_pquery_args, offset, pagesize, corp_info.collator_locale, 'freq', True)
+        data_ready = True
+    except PqueryResultNotFound:
+        total_num_lines = 0
+        freqs = []
+        data_ready = False
+    ans = {
+        'corpname': amodel.args.corpname,
+        'usesubcorp': amodel.args.usesubcorp,
+        'query_id': amodel.q_code,
+        'freqs': freqs,
+        'page': page,
+        'pagesize': pagesize,
+        'total_num_lines': total_num_lines,
+        'data_ready': data_ready
+    }
+    await amodel.export_form_args(ans)
+    amodel.export_subcorpora_list(amodel.args.corpname, amodel.args.usesubcorp, ans)
+    amodel.add_save_menu()
+    amodel.disabled_menu_items = (
+        MainMenu.FILTER, MainMenu.FREQUENCY, MainMenu.COLLOCATIONS,
+        MainMenu.VIEW('kwic-sent-switch'))
+    for i, conc_id in enumerate(amodel._curr_pquery_args.conc_ids):
+        amodel.dynamic_menu_items.append(
+            MenuItemInternal(
+                MainMenu.CONCORDANCE, req.translate(
+                    'Go to the constituent concordance #{}').format(i + 1), 'view'
+            ).add_args(('q', f'~{conc_id}'))
+        )
+    return ans
+
+
+@bp.route('/freq_intersection', ['POST'])
+@http_action(return_type='json', mutates_result=True, action_model=ParadigmaticQueryActionModel)
+async def freq_intersection(amodel: ParadigmaticQueryActionModel, req: KRequest, resp: KResponse):
+    """
+    Run a paradigmatic query out of existing concordances.
+
+    submitted JSON structure - see models.pquery.common.FreqIntersectionArgs
+    """
+    worker = bgcalc.calc_backend_client(settings)
+    corp_info = await amodel.get_corpus_info(amodel.args.corpname)
+
+    amodel._curr_pquery_args = PqueryFormArgs(
+        corpname=amodel.corp.corpname,
+        attr=amodel.get_default_attr(),
+        position='0<0~0>0')
+    amodel._curr_pquery_args.update_by_user_query(req.json)
+    conc_forms, raw_queries = await amodel.load_conc_queries(
+        amodel._curr_pquery_args.conc_ids, amodel.args.corpname, 'query')
+    if amodel._curr_pquery_args.conc_subset_complements:
+        conc_forms2, raw_queries2 = await amodel.load_conc_queries(
+            amodel._curr_pquery_args.conc_subset_complements.conc_ids, amodel.args.corpname, 'query')
+        raw_queries.update(raw_queries2)
+    if amodel._curr_pquery_args.conc_superset:
+        conc_forms3, raw_queries3 = await amodel.load_conc_queries(
+            [amodel._curr_pquery_args.conc_superset.conc_id], amodel.args.corpname, 'query')
+        raw_queries.update(raw_queries3)
+
+    calc_args = (
+        amodel._curr_pquery_args,
+        raw_queries,
+        amodel.subcpath,
+        amodel.session_get('user', 'id'),
+        corp_info.collator_locale if corp_info.collator_locale else 'en_US')
+    task_status = await worker.send_task(
+        'calc_merged_freqs', object.__class__, args=calc_args, time_limit=amodel.TASK_TIME_LIMIT)
+    sq_items = []
+    for conc_id in amodel._curr_pquery_args.conc_ids:
+        sq_items.append(conc_forms[conc_id]['curr_queries'][amodel.args.corpname])
+    shortened_q = ' && '.join(f'{{{q}}}' for q in sq_items)
+    shortened_q = f'{shortened_q} -> {amodel._curr_pquery_args.attr}'
+
+    def on_query_store(query_ids, history_ts, result):
+        async_task = AsyncTaskStatus(
+            status=task_status.status, ident=task_status.id,
+            category=AsyncTaskStatus.CATEGORY_PQUERY,
+            label=shortened_q,
+            args=dict(query_id=query_ids[0], last_update=time.time()),
+            url=req.create_url('pquery/result', dict(q=f'~{query_ids[0]}')))
+        amodel.store_async_task(async_task)
+        result['task'] = async_task.to_dict()
+        if history_ts:
+            amodel.store_last_search('pquery', query_ids[0])
+
+    amodel.on_query_store(on_query_store)
+    return {}
+
+
+@bp.route('/get_results')
+@http_action(return_type='json', action_model=ParadigmaticQueryActionModel)
+async def get_results(amodel: ParadigmaticQueryActionModel, req: KRequest, resp: KResponse):
+    page_id = int(req.args.get('page')) - 1
+    sort = req.args.get('sort')
+    reverse = bool(int(req.args.get('reverse')))
+    offset = page_id * amodel.args.pqueryitemsperpage
+    corp_info = await amodel.get_corpus_info(amodel.args.corpname)
+    try:
+        total_num_lines, freqs = require_existing_pquery(
+            amodel._curr_pquery_args, offset, amodel.args.pqueryitemsperpage, corp_info.collator_locale, sort, reverse)
+    except PqueryResultNotFound:
+        raise NotFoundException('pquery__result_no_more_avail_for_download_pls_update')
+    return dict(rows=freqs)
+
+
+@dataclass
+class SavePQueryArgs:
+    from_line: int = 1
+    to_line: Optional[int] = None
+    saveformat: str = ''
+    reverse: bool = False
+    sort: str = 'value'
+    heading: bool = False
+    colheaders: bool = False
+
+
+@bp.route('/download')
+@http_action(access_level=1, return_type='plain', action_model=ParadigmaticQueryActionModel, mapped_args=SavePQueryArgs)
+async def download(amodel: ParadigmaticQueryActionModel, req: KRequest[SavePQueryArgs], resp: KResponse):
+    """
+    dawnload a paradigmatic query results
+    """
+    from_line = req.mapped_args.from_line - 1
+    to_line = req.mapped_args.to_line if req.mapped_args.to_line else sys.maxsize
+    corp_info = await amodel.get_corpus_info(amodel.args.corpname)
+    try:
+        _, freqs = require_existing_pquery(
+            amodel._curr_pquery_args, from_line, to_line - from_line,
+            corp_info.collator_locale, req.mapped_args.sort, req.mapped_args.reverse)
+    except PqueryResultNotFound:
+        raise NotFoundException('pquery__result_no_more_avail_for_download_pls_update')
+
+    def mkfilename(suffix):
+        return f'{amodel.args.corpname}-pquery.{suffix}'
+
+    writer = plugins.runtime.EXPORT.instance.load_plugin(
+        req.mapped_args.saveformat, subtype='pquery', translate=req.translate)
+    writer.set_col_types(int, str, float)
+
+    resp.set_header('Content-Type', writer.content_type())
+    resp.set_header('Content-Disposition',
+                    f'attachment; filename="{mkfilename(req.mapped_args.saveformat)}"')
+
+    if req.mapped_args.colheaders or req.mapped_args.heading:
+        writer.writeheading(['', 'value', 'freq'])
+
+    for (i, row) in enumerate(freqs):
+        writer.writerow(i + 1, row)
+
+    output = writer.raw_content()
+    return output
