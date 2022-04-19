a7fdd8c0645c26f872e0a467a2432cb478e7fbaa
Tomas Machalek <tomas@machalek.xyz>
2022-04-19 12:03:00 +0200

    Fix argument injection

--- lib/plugins/mysql_corparch/backend.py
+++ lib/plugins/mysql_corparch/backend.py
@@ -292,9 +292,9 @@ class Backend(DatabaseBackend):
                 'GROUP BY c.name '
                 'HAVING num_match_keys >= %s ) '
                 'UNION ').format(where1=' AND '.join('(' + wc + ')' for wc in where_cond1))
-        where.extend(values_cond2)
         total_acc_sql, total_acc_args = self._total_access_query(user_id)
         where.extend(total_acc_args)
+        where.extend(values_cond2)
         sql += (
             '(SELECT c.name as id, c.web, '
             'IF (rc.locale IS NOT NULL, rc.locale, c.collator_locale) as collator_locale, '
