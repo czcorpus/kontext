6dffd96646891c78c2b7faaf7cafdbbf5f20a619
Martin Zimandl <martin.zimandl@gmail.com>
2022-08-03 16:14:06 +0200

    json dumps in suggestions norm_str

--- lib/plugins/default_query_suggest/backends/couch.py
+++ lib/plugins/default_query_suggest/backends/couch.py
@@ -34,10 +34,11 @@ from plugins.abstract.query_suggest import AbstractBackend
 import couchdb
 from plugins.default_query_suggest.formats.cnc_sublemma import CncSublemmaSuggestion, SuggestionLemmaData
 from typing import Dict
+import json
 
 
-def norm_str(s):
-    return f'"{s.lower()}"'
+def norm_str(s: str):
+    return f'"{json.dumps(s.lower())}"'
 
 
 class CouchDBBackend(AbstractBackend[Dict[str, CncSublemmaSuggestion]]):
@@ -57,7 +58,8 @@ class CouchDBBackend(AbstractBackend[Dict[str, CncSublemmaSuggestion]]):
     def find_suggestion(
             self, ui_lang, user_id, maincorp, corpora, subcorpus, value, value_type, value_subformat,
             query_type, p_attr, struct, s_attr) -> CncSublemmaSuggestion:
-        tmp = self.db.view(self._conf['view'], start_key=norm_str(value), end_key=norm_str(value), include_docs=True)
+        tmp = self.db.view(self._conf['view'], start_key=norm_str(
+            value), end_key=norm_str(value), include_docs=True)
         merged = {}
         for item in tmp:
             lemma = item['doc']['lemma']
