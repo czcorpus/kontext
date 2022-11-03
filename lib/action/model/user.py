66e5ecfab7fa90bfa49a387fd85a023f9265d0c9
Tomas Machalek <tomas.machalek@gmail.com>
2022-11-03 12:44:10 +0100

    Add missing template property (document.html requries it)

--- lib/action/model/user.py
+++ lib/action/model/user.py
@@ -401,6 +401,7 @@ class UserActionModel(BaseActionModel, AbstractUserModel):
         result['conc_url_ttl_days'] = None
         result['explicit_conc_persistence_ui'] = False
         result['corpus_ident'] = {}
+        result['corp_sample_size'] = None
         result['Globals'] = {}
         result['base_attr'] = BaseActionModel.BASE_ATTR
         result['user_info'] = self._req.ctx.session.get('user', {'fullname': None})
