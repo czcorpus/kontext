2e690b0731dacc29593fffaf731c896d0b7676b4
Martin Zimandl <martin.zimandl@gmail.com>
2023-01-18 15:25:48 +0100

    subcorpus test improvements

--- cypress/integration/subcorpora.js
+++ cypress/integration/subcorpora.js
@@ -11,6 +11,39 @@ describe('Subcorpora', () => {
         cy.actionLogout();
     });
 
+    const createSubcorpus = (name, description, texttypes, isDraft) => {
+        // open create subcorpus
+        cy.hoverNthMenuItem(2);
+        cy.clickMenuItem(2, 4);
+
+        // create subcorpus from text type
+        cy.get('#subcorp-form-mount table.form .subcname input').type(name);
+        if (!!description) {
+            cy.get('#subcorp-form-mount table.form textarea').type(description);
+        }
+        texttypes.forEach(v => {
+            cy.get('#subcorp-form-mount div.data-sel div.grid > div').eq(1).get('input[type=checkbox]').check(v);
+        });
+        if (isDraft) {
+            cy.get('#subcorp-form-mount p.submit-buttons').contains('button', 'Save draft').click();
+        } else {
+            cy.get('#subcorp-form-mount p.submit-buttons').contains('button', 'Create subcorpus').click();
+        }
+    }
+
+    const openProperties = (name) => {
+        cy.get('#my-subcorpora-mount table.data tbody tr').contains('tr', name).find('a.properties-subc').click();
+    }
+
+    const closeProperties = () => {
+        cy.get('.closeable-frame .heading .control img').click();
+    }
+
+    const deleteSubcorpus = (name) => {
+        openProperties(name);
+        cy.get('.closeable-frame').contains('button', 'Delete').click();
+    }
+
     it('tests empty subcorpora list', () => {
         // open my subcorpora list
         cy.hoverNthMenuItem(2);
@@ -21,14 +54,7 @@ describe('Subcorpora', () => {
     });
 
     it('creates subcorpus', () => {
-        // open create subcorpus
-        cy.hoverNthMenuItem(2);
-        cy.clickMenuItem(2, 4);
-
-        // create subcorpus from text type
-        cy.get('#subcorp-form-mount table.form .subcname input').type('sus1');
-        cy.get('#subcorp-form-mount div.data-sel div.grid > div').eq(1).get('input[type=checkbox]').check('1');
-        cy.get('#subcorp-form-mount p.submit-buttons button').last().click();
+        createSubcorpus('sus1', '', ['1'], false);
 
         // table contains `sus1` subcorpus
         cy.url().should('include', '/subcorpus/list');
@@ -38,14 +64,7 @@ describe('Subcorpora', () => {
     });
 
     it('creates subcorpus draft', () => {
-        // open create subcorpus
-        cy.hoverNthMenuItem(2);
-        cy.clickMenuItem(2, 4);
-
-        // create subcorpus draft from text type
-        cy.get('#subcorp-form-mount table.form .subcname input').type('sus2');
-        cy.get('#subcorp-form-mount div.data-sel div.grid > div').eq(1).get('input[type=checkbox]').check('1');
-        cy.get('#subcorp-form-mount p.submit-buttons').contains('button', 'Save draft').click();
+        createSubcorpus('sus2', '', ['1'], true);
 
         // contains one subcorpus
         cy.url().should('include', '/subcorpus/list');
@@ -145,41 +164,76 @@ describe('Subcorpora', () => {
     });
 
     it('creates subcorpus draft, edits it and removes it', () => {
-        // open create subcorpus
-        cy.hoverNthMenuItem(2);
-        cy.clickMenuItem(2, 4);
+        createSubcorpus('sus-sub', 'description', ['1'], true);
 
-        // create subcorpus draft from text type
-        cy.get('#subcorp-form-mount table.form .subcname input').type('sus-sub');
-        cy.get('#subcorp-form-mount div.data-sel div.grid > div').eq(1).get('input[type=checkbox]').check('1');
-        cy.get('#subcorp-form-mount p.submit-buttons').contains('button', 'Save draft').click();
-
-        // contains one subcorpus
+        // check values and edit draft in properties
         cy.url().should('include', '/subcorpus/list');
-        cy.get('#my-subcorpora-mount table.data tbody tr').contains('tr', 'sus-sub').find('a.properties-subc').click();
-        cy.contains('button', 'Subcorpus structure').click();
-        cy.get('input[value=1]').should('be.checked');
-        cy.get('input[value=2]').should('not.be.checked');
-        cy.get('input[type=checkbox]').check('2');
-        cy.contains('button', 'Save draft').click();
-        cy.get('.closeable-frame .heading .control img').click();
-        cy.get('#my-subcorpora-mount table.data tbody tr a').contains('sus-sub').click();
+        openProperties('sus-sub');
+        cy.get('.closeable-frame').contains('button', 'Name and public description').click();
+        cy.get('.closeable-frame input').should('have.value', 'sus-sub');
+        cy.get('.closeable-frame input[type=text]').clear().type('new-name');
+        cy.get('.closeable-frame textarea').should('have.value', 'description');
+        cy.get('.closeable-frame textarea').clear().type('new description');
+        cy.get('.closeable-frame').contains('button', 'Update name and public description').click();
 
+        cy.get('.closeable-frame').contains('button', 'Subcorpus structure').click();
+        cy.get('.closeable-frame').get('input[value=1]').should('be.checked');
+        cy.get('.closeable-frame').get('input[value=2]').should('not.be.checked');
+        cy.get('.closeable-frame').get('input[type=checkbox]').check('2');
+        cy.get('.closeable-frame').contains('button', 'Save draft').click();
+
+        closeProperties();
+
+        // check change and edit draft on subcorpus/new page
+        cy.get('#my-subcorpora-mount table.data tbody tr a').contains('new-name').click();
         cy.url().should('include', '/subcorpus/new');
+        cy.get('#subcorp-form-mount table.form .subcname input').should('have.value', 'new-name');
+        cy.get('#subcorp-form-mount table.form .subcname input').clear().type('old-name');
+        // TODO - for some reason textarea is empty, manually it works
+        // cy.get('#subcorp-form-mount table.form textarea').should('have.value', 'new description');
+        cy.get('#subcorp-form-mount table.form textarea').clear().type('old description');
         cy.get('input[value=1]').should('be.checked');
         cy.get('input[value=2]').should('be.checked');
         cy.get('input[type=checkbox]').uncheck('1');
         cy.contains('button', 'Save draft').click();
 
+        // check change in properties
         cy.url().should('include', '/subcorpus/list');
-        cy.get('#my-subcorpora-mount table.data tbody tr').contains('tr', 'sus-sub').find('a.properties-subc').click();
-        cy.contains('button', 'Subcorpus structure').click();
-        cy.get('input[value=1]').should('not.be.checked');
-        cy.get('input[value=2]').should('be.checked');
+        openProperties('old-name');
+        cy.get('.closeable-frame').contains('button', 'Subcorpus structure').click();
+        cy.get('.closeable-frame').get('input[value=1]').should('not.be.checked');
+        cy.get('.closeable-frame').get('input[value=2]').should('be.checked');
+        cy.get('.closeable-frame').contains('button', 'Name and public description').click();
+        cy.get('.closeable-frame input').should('have.value', 'old-name');
+        cy.get('.closeable-frame textarea').should('have.value', 'old description');
+        closeProperties();
 
-        cy.contains('button', 'File').click();
-        cy.contains('button', 'Delete').click();
+        deleteSubcorpus('old-name');
+    });
 
-        cy.get('#my-subcorpora-mount table.data tbody tr').should('have.length', 1);
+    it('tests archiving subcorpus', () => {
+        createSubcorpus('archive-sub', '', ['1'], false);
+
+        // table contains `archive-sub` subcorpus
+        cy.url().should('include', '/subcorpus/list');
+        cy.get('#my-subcorpora-mount table.data tbody tr').contains('tr', 'archive-sub').should('have.length', 1);
+
+        // archive it
+        openProperties('archive-sub');
+        cy.get('.closeable-frame').contains('button', 'Archive').click();
+        closeProperties();
+
+        // check archived subcorpora
+        cy.get('#my-subcorpora-mount table.data tbody tr').contains('archived').should('have.length', 0);
+        cy.get('#my-subcorpora-mount div.inputs input[type=checkbox]').check(); // show archived corpora
+        cy.get('#my-subcorpora-mount table.data tbody tr').contains('archived').should('have.length', 1);
+
+        // restore archived subcorpus
+        openProperties('archive-sub');
+        cy.get('.closeable-frame').contains('button', 'Restore').click();
+        closeProperties();
+        cy.get('#my-subcorpora-mount table.data tbody tr').contains('archived').should('have.length', 0);
+
+        deleteSubcorpus('archive-sub');
     });
 });
