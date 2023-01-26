czcorpus/kontext

commit 39c7f7d59cd5c58b89909baf4ac1a535165a6ef1

Add query history - auto-correction of possibly broken records
(it was possible to store a non-query, non-filter query to
user history)

lib/plugins/default_query_history/__init__.py
@@ -22,7 +22,7 @@ import logging
 import random
 import time
 from datetime import datetime
-from typing import Callable
+from typing import Dict, Tuple
 
 import plugins
 from corplib.fallback import EmptyCorpus
@@ -144,8 +144,15 @@ class QueryHistory(AbstractQueryHistory):
         return await self._query_persistence.open(q_id) is not None
 
     async def _merge_conc_data(self, data):
+        async def extract_id(item_id: str, item_data: Dict) -> Tuple[str, Dict]: return item_id, item_data
         q_id = data['query_id']
         edata = await self._query_persistence.open(q_id)
+
+        form_type = edata.get('lastop_form', {}).get('form_type', None)
+        if form_type not in ('query', 'filter'):
+            logging.getLogger(__name__).warning(f'Fixing broken query history record of invalid type {form_type}')
+            ops = await self._query_persistence.map_pipeline_ops(q_id, extract_id)
+            q_id, edata = ops[0]
 
         def get_ac_val(data, name, corp): return data[name][corp] if name in data else None
 

lib/plugins/mysql_query_history/__init__.py
@@ -31,7 +31,7 @@ configuration.
 
 import logging
 from datetime import datetime
-from typing import Callable
+from typing import Dict, Tuple
 
 import plugins
 from corplib.abstract import AbstractKCorpus
@@ -137,7 +137,6 @@ class MySqlQueryHistory(AbstractQueryHistory):
         return await self._query_persistence.open(q_id) is not None
 
     async def _merge_conc_data(self, data, qdata):
-
         def get_ac_val(data, name, corp): return data[name][corp] if name in data else None
 
         if qdata and 'lastop_form' in qdata:
@@ -187,6 +186,9 @@ class MySqlQueryHistory(AbstractQueryHistory):
         see the super-class
         """
 
+        async def extract_id(item_id: str, item_data: Dict) -> Tuple[str, Dict]:
+            return item_id, item_data
+
         where_dict = {
             'user_id = %s': user_id,
             'created >= %s': from_date if from_date else None,
@@ -230,6 +232,14 @@ class MySqlQueryHistory(AbstractQueryHistory):
                     continue
                 qdata = qdata_map[q_id]
                 if q_supertype == 'conc':
+                    # test we have actually the 'query' or 'filter' type and if not then move
+                    # to the first query in chain (this fixes possibly broken query history records)
+                    form_type = qdata.get('lastop_form', {}).get('form_type', None)
+                    if form_type not in ('query', 'filter'):
+                        logging.getLogger(__name__).warning(
+                            f'Fixing broken query history record of invalid type {form_type}')
+                        ops = await self._query_persistence.map_pipeline_ops(q_id, extract_id)
+                        q_id, qdata = ops[0]
                     tmp = await self._merge_conc_data(item, qdata)
                     if not tmp:
                         continue

commit de3f193bf70974add92a0129c1a4ad11f5da95c6

Fix deleting of query history items...
... caused by 39c7f7d59cd5c. Runtime corrected query ID
refers to a proper operation but not to a correct history record.
So we have to keep both values...

lib/plugins/default_query_history/__init__.py
@@ -152,7 +152,9 @@ class QueryHistory(AbstractQueryHistory):
         if form_type not in ('query', 'filter'):
             logging.getLogger(__name__).warning(f'Fixing broken query history record {q_id} of invalid type {form_type}')
             ops = await self._query_persistence.map_pipeline_ops(q_id, extract_id)
+            lastop_qid = q_id
             q_id, edata = ops[0]
+            edata['lastop_query_id'] = lastop_qid
 
         def get_ac_val(data, name, corp): return data[name][corp] if name in data else None
 

lib/plugins/mysql_query_history/__init__.py
@@ -145,6 +145,7 @@ class MySqlQueryHistory(AbstractQueryHistory):
             form_data = qdata['lastop_form']
             main_corp = qdata['corpora'][0]
             if form_data['form_type'] == 'query':
+                ans['lastop_query_id'] = qdata['lastop_query_id']
                 ans['query_type'] = form_data['curr_query_types'][main_corp]
                 ans['query'] = form_data['curr_queries'][main_corp]
                 ans['corpname'] = main_corp
@@ -238,9 +239,14 @@ class MySqlQueryHistory(AbstractQueryHistory):
                     if form_type not in ('query', 'filter'):
                         ops = await self._query_persistence.map_pipeline_ops(q_id, extract_id)
                         logging.getLogger(__name__).warning(
-                            f'Fixing broken query history record {q_id} of invalid type "{form_type}" (proper id: {ops[0][0]})')
+                            'Runtime patching broken query history record '
+                            f'{q_id} of invalid type "{form_type}" (proper id: {ops[0][0]})')
+                        lastop_qid = q_id
                         q_id, qdata = ops[0]
                         qdata['query_id'], item['query_id'] = q_id, q_id
+                        qdata['lastop_query_id'] = lastop_qid
+                    else:
+                        qdata['lastop_query_id'] = q_id
                     tmp = await self._merge_conc_data(item, qdata)
                     if not tmp:
                         continue

public/files/js/models/searchHistory/common.ts
@@ -58,10 +58,21 @@ interface AbstractQueryHistoryItem {
 }
 
 export interface ConcQueryHistoryItem extends AbstractQueryHistoryItem {
+
     q_supertype:'conc';
+
+    /**
+     * lastop_query_id refers to the last operation in chain starting with
+     * the ID stored as a query history record
+     */
+    lastop_query_id:string;
+
     lpos:string;
+
     qmcase:string;
+
     pcq_pos_neg:string;
+
     default_attr:string;
 
     /**

public/files/js/models/searchHistory/index.ts
@@ -29,8 +29,12 @@ import { Actions } from './actions';
 import { Actions as MainMenuActions } from '../mainMenu/actions';
 import { QueryType } from '../query/query';
 import { PageModel } from '../../app/page';
-import { GetHistoryResponse, SaveItemResponse, SearchHistoryModelState,
-    QueryHistoryItem, isConcQueryHistoryItem } from './common';
+import {
+    GetHistoryResponse,
+    SaveItemResponse,
+    SearchHistoryModelState,
+    QueryHistoryItem,
+    isConcQueryHistoryItem } from './common';
 
 
 
@@ -83,13 +87,13 @@ export class SearchHistoryModel extends StatefulModel<SearchHistoryModelState> {
         );
         this.pageModel = pageModel;
 
-        this.addActionHandler<typeof Actions.SelectItem>(
-            Actions.SelectItem.name,
+        this.addActionHandler(
+            Actions.SelectItem,
             action => {this.changeState(state => {state.currentItem = action.payload.value})}
         );
 
-        this.addActionHandler<typeof Actions.HistorySetCurrentCorpusOnly>(
-            Actions.HistorySetCurrentCorpusOnly.name,
+        this.addActionHandler(
+            Actions.HistorySetCurrentCorpusOnly,
             action => {
                 this.changeState(state => {
                     state.isBusy = true;
@@ -99,8 +103,8 @@ export class SearchHistoryModel extends StatefulModel<SearchHistoryModelState> {
             }
         );
 
-        this.addActionHandler<typeof Actions.HistorySetArchivedOnly>(
-            Actions.HistorySetArchivedOnly.name,
+        this.addActionHandler(
+            Actions.HistorySetArchivedOnly,
             action => {
                 this.changeState(state => {
                     state.isBusy = true;
@@ -110,8 +114,8 @@ export class SearchHistoryModel extends StatefulModel<SearchHistoryModelState> {
             }
         );
 
-        this.addActionHandler<typeof Actions.HistorySetQuerySupertype>(
-            Actions.HistorySetQuerySupertype.name,
+        this.addActionHandler(
+            Actions.HistorySetQuerySupertype,
             action => {
                 this.changeState(state => {
                     state.querySupertype = action.payload.value;
@@ -120,8 +124,8 @@ export class SearchHistoryModel extends StatefulModel<SearchHistoryModelState> {
             }
         );
 
-        this.addActionHandler<typeof Actions.HistoryLoadMore>(
-            Actions.HistoryLoadMore.name,
+        this.addActionHandler(
+            Actions.HistoryLoadMore,
             action => {
                 this.changeState(state => {
                     state.isBusy = true;
@@ -131,8 +135,8 @@ export class SearchHistoryModel extends StatefulModel<SearchHistoryModelState> {
             }
         );
 
-        this.addActionHandler<typeof Actions.ToggleQueryHistoryWidget, typeof MainMenuActions.ShowQueryHistory>(
-            [Actions.ToggleQueryHistoryWidget.name, MainMenuActions.ShowQueryHistory.name],
+        this.addActionHandler(
+            [Actions.ToggleQueryHistoryWidget, MainMenuActions.ShowQueryHistory],
             action => {
                 this.changeState(state => {
                     state.isBusy = true
@@ -145,16 +149,16 @@ export class SearchHistoryModel extends StatefulModel<SearchHistoryModelState> {
             }
         );
 
-        this.addActionHandler<typeof Actions.HistoryOpenQueryForm>(
-            Actions.HistoryOpenQueryForm.name,
+        this.addActionHandler(
+            Actions.HistoryOpenQueryForm,
             action => {
                 this.openQueryForm(action.payload.idx);
                 // page leaves here
             }
         );
 
-        this.addActionHandler<typeof Actions.HistorySetEditedItem>(
-            Actions.HistorySetEditedItem.name,
+        this.addActionHandler(
+            Actions.HistorySetEditedItem,
             action => {
                 this.changeState(state => {
                     state.itemsToolbars[action.payload.itemIdx] = tuple(true, true);
@@ -165,8 +169,8 @@ export class SearchHistoryModel extends StatefulModel<SearchHistoryModelState> {
             }
         );
 
-        this.addActionHandler<typeof Actions.HistoryCloseEditedItem>(
-            Actions.HistoryCloseEditedItem.name,
+        this.addActionHandler(
+            Actions.HistoryCloseEditedItem,
             action => {
                 this.changeState(state => {
                     state.itemsToolbars[action.payload.itemIdx] = tuple(true, false);
@@ -174,8 +178,8 @@ export class SearchHistoryModel extends StatefulModel<SearchHistoryModelState> {
             }
         );
 
-        this.addActionHandler<typeof Actions.HistoryEditorSetName>(
-            Actions.HistoryEditorSetName.name,
+        this.addActionHandler(
+            Actions.HistoryEditorSetName,
             action => {
                 this.changeState(state => {
                     state.data[action.payload.itemIdx].name = action.payload.value;
@@ -183,8 +187,8 @@ export class SearchHistoryModel extends StatefulModel<SearchHistoryModelState> {
             }
         );
 
-        this.addActionHandler<typeof Actions.HistoryDoNotArchive>(
-            Actions.HistoryDoNotArchive.name,
+        this.addActionHandler(
+            Actions.HistoryDoNotArchive,
             action => {
                 this.saveItem(action.payload.itemIdx, null).subscribe(
                     (msg) => {
@@ -203,8 +207,8 @@ export class SearchHistoryModel extends StatefulModel<SearchHistoryModelState> {
             }
         );
 
-        this.addActionHandler<typeof Actions.HistoryEditorClickSave>(
-            Actions.HistoryEditorClickSave.name,
+        this.addActionHandler(
+            Actions.HistoryEditorClickSave,
             action => {
                 const item = this.state.data[action.payload.itemIdx];
                 if (!item.name) {
@@ -227,8 +231,8 @@ export class SearchHistoryModel extends StatefulModel<SearchHistoryModelState> {
             }
         );
 
-        this.addActionHandler<typeof Actions.ToggleRowToolbar>(
-            Actions.ToggleRowToolbar.name,
+        this.addActionHandler(
+            Actions.ToggleRowToolbar,
             action => {
                 this.changeState(state => {
                     const [status,] = state.itemsToolbars[action.payload.rowIdx];
@@ -242,8 +246,8 @@ export class SearchHistoryModel extends StatefulModel<SearchHistoryModelState> {
             }
         );
 
-        this.addActionHandler<typeof Actions.RemoveItemFromList>(
-            Actions.RemoveItemFromList.name,
+        this.addActionHandler(
+            Actions.RemoveItemFromList,
             action => {
                 this.changeState(state => {
                     state.isBusy = true;
@@ -330,11 +334,12 @@ export class SearchHistoryModel extends StatefulModel<SearchHistoryModelState> {
 
     private deleteItem(itemIdx:number):Observable<GetHistoryResponse> {
         const item = this.state.data[itemIdx];
+        const query_id = isConcQueryHistoryItem(item) ? item.lastop_query_id : item.query_id;
         return this.pageModel.ajax$<any>(
             HTTP.Method.POST,
             this.pageModel.createActionUrl('delete_query'),
             {
-                query_id: item.query_id,
+                query_id,
                 created: item.created
             },
             {contentType: 'application/json'}