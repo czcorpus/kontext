3f942622e3659fae615f55e3a75352153594e972
Tomas Machalek <tomas@machalek.xyz>
2023-02-14 22:01:13 +0100

    Add share icon to the concordance permalink widget

--- public/files/js/views/query/style.tsx
+++ public/files/js/views/query/style.tsx
@@ -68,13 +68,17 @@ export const SaveHintParagraph = styled.p`
 
 export const PersistentConcordanceForm = styled.form`
 
-    input {
-        font-size: 1.5em;
-        width: 30em;
-        display: inline-block;
+    .link {
+        display: flex;
+        align-items: center;
         margin-top: 0.4em;
         margin-bottom: 1em;
-        color: ${theme.colorSuperlightText};
+
+        input {
+            font-size: 1.5em;
+            width: 30em;
+            color: ${theme.colorSuperlightText};
+        }
     }
 
     input.archived {
