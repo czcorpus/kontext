6786e6d5d032765ad8563ef7cd404d9699aede74
Tomas Machalek <tomas.machalek@gmail.com>
2024-07-18 14:49:57 +0200

    Fix custom CNC query - should use ID and not corpus name

--- lib/plugins/ucnk_remote_auth6/backend/__init__.py
+++ lib/plugins/ucnk_remote_auth6/backend/__init__.py
@@ -18,7 +18,7 @@ from plugins.mysql_corparch.backend import Backend
 class UCNKBackend(Backend):
     def custom_parallel_access_subquery(self, user_id):
         return (
-            "SELECT c.name AS corpus_id, gpc.limited AS limited "
+            "SELECT c.id AS corpus_id, gpc.limited AS limited "
             "FROM group_parallel_corpus AS gpc "
             "JOIN `group` AS g ON gpc.group_id = g.id "
             "JOIN parallel_corpus AS pc ON pc.id = gpc.parallel_corpus_id "
@@ -30,10 +30,9 @@ class UCNKBackend(Backend):
 
     def custom_access_subquery(self, user_id):
         return (
-            "SELECT c.name AS corpus_id, group_corpus.limited AS limited "
+            "SELECT group_corpus.corpus_id AS corpus_id, group_corpus.limited AS limited "
             "FROM group_corpus "
             "JOIN `group` ON group_corpus.group_id = `group`.id "
-            "JOIN corpora AS c ON c.id = group_corpus.corpus_id "
             "JOIN user_group ON `group`.id = user_group.group_id "
             "WHERE user_group.user_id = %s",
             [user_id]
