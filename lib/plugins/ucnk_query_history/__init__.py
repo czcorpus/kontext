0645bdea4a3204026b8ffafabf5a1acb55472a34
Tomas Machalek <tomas.machalek@gmail.com>
2024-09-26 13:41:37 +0200

    WIP 1

--- lib/plugins/ucnk_query_history/__init__.py
+++ lib/plugins/ucnk_query_history/__init__.py
@@ -18,11 +18,13 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 import logging
+from datetime import datetime
 
 import plugins
 from plugin_types.query_persistence import AbstractQueryPersistence
 from plugin_types.subc_storage import AbstractSubcArchive
 from plugin_types.auth import AbstractAuth
+from plugin_types.general_storage import KeyValueStorage
 from plugins import inject
 from plugins.mysql_query_history import MySqlQueryHistory
 from plugins.mysql_integration_db import MySqlIntegrationDb
@@ -32,27 +34,65 @@ from plugins.common.mysql import MySQLConf
 
 class UcnkQueryHistory(MySqlQueryHistory):
 
+    def __init__(
+            self,
+            conf,
+            db: MySqlIntegrationDb,
+            kvdb: KeyValueStorage,
+            query_persistence: AbstractQueryPersistence,
+            subc_archive: AbstractSubcArchive,
+            auth: AbstractAuth):
+        super().__init__(conf, db, query_persistence, subc_archive, auth)
+        self._kvdb = kvdb
+        self._del_queue_key = conf.get('plugins', 'query_history').get(
+            'fulltext_deleting_queue', 'query_history_fulltext_del')
+
     def supports_fulltext_search(self):
         return True
+
+    async def delete_old_records(self):
+        """
+        Deletes records older than ttl_days. Named records are
+        kept intact.
+        now - created > ttl
+        now - ttl  > created
+        """
+        # TODO remove also named but unpaired history entries
+        async with self._db.cursor() as cursor:
+            logging.getLogger(__name__).warning(f'SELECT id FROM {self.TABLE_NAME} WHERE created < %s AND name IS NULL')
+            await cursor.execute(
+                #f'SELECT query_id, user_id, created FROM {self.TABLE_NAME} WHERE created < %s AND name IS NULL',
+                f'SELECT query_id, user_id, created FROM {self.TABLE_NAME} WHERE name IS NULL',
+                #   (int(datetime.utcnow().timestamp()) - self.ttl_days * 3600 * 24,)
+            )
+            for row in await cursor.fetchall():
+                logging.getLogger(__name__).warning('>>>> row to del: {}'.format(row))
+                await cursor.execute(
+                    f'DELETE FROM {self.TABLE_NAME} WHERE id = %s',
+                    (int(datetime.utcnow().timestamp()) - self.ttl_days * 3600 * 24,))
+                await self._kvdb.list_append(self._del_queue_key, row)
+
 
 
 @inject(
     plugins.runtime.INTEGRATION_DB,
     plugins.runtime.QUERY_PERSISTENCE,
     plugins.runtime.SUBC_STORAGE,
-    plugins.runtime.AUTH
+    plugins.runtime.AUTH,
+    plugins.runtime.DB
 )
 def create_instance(
         conf,
         integ_db: MySqlIntegrationDb,
         query_persistence: AbstractQueryPersistence,
         subc_archive: AbstractSubcArchive,
-        auth: AbstractAuth
+        auth: AbstractAuth,
+        kvdb: KeyValueStorage
 ):
-    plugin_conf = conf.get('plugins', 'auth')
-    if integ_db and integ_db.is_active and 'mysql_host' not in plugin_conf:
+    auth_plg_conf = conf.get('plugins', 'auth')
+    if integ_db and integ_db.is_active and 'mysql_host' not in auth_plg_conf:
         db = integ_db
         logging.getLogger(__name__).info(f'ucnk_query_history uses integration_db[{integ_db.info}]')
     else:
-        db = AdhocDB(MySQLConf.from_conf(plugin_conf))
-    return UcnkQueryHistory(conf, db, query_persistence, subc_archive, auth)
+        db = AdhocDB(MySQLConf.from_conf(auth_plg_conf))
+    return UcnkQueryHistory(conf, db, kvdb, query_persistence, subc_archive, auth)
