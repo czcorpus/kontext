c744a9888d9b2f9d14e2f38d8bef0fa3ba289591
Martin Zimandl <martin.zimandl@gmail.com>
2024-10-23 16:23:14 +0200

    add keywords to cypress

--- cypress/e2e/queryHistory.cy.js
+++ cypress/e2e/queryHistory.cy.js
@@ -10,27 +10,36 @@ describe('Query History', () => {
         // concordance query
         cy.hoverNthMenuItem(1);
         cy.clickMenuItem(1, 1);
+        cy.url().should('contain', '/query?');
         cy.get('.simple-input').type('London');
-        cy.get('.query .default-button').click().then(() => {
+        cy.get('.query .default-button').click();
+        cy.url().should('contain', '/view?');
 
-            // paradigmatic query
-            cy.hoverNthMenuItem(1);
-            cy.clickMenuItem(1, 2);
-            cy.get('#pquery-form-mount .cql-input').eq(0).type('[word="e.*"]');
-            cy.get('#pquery-form-mount .cql-input').eq(1).type('[tag="N.*"]');
-            cy.get('#pquery-form-mount .submit').click().then(() => {
+        // paradigmatic query
+        cy.hoverNthMenuItem(1);
+        cy.clickMenuItem(1, 2);
+        cy.url().should('contain', '/pquery/index?');
+        cy.get('#pquery-form-mount .cql-input').eq(0).type('[word="e.*"]');
+        cy.get('#pquery-form-mount .cql-input').eq(1).type('[tag="N.*"]');
+        cy.get('#pquery-form-mount .submit').click();
+        cy.url().should('contain', '/pquery/result?');
 
-                // word list query
-                cy.hoverNthMenuItem(1);
-                cy.clickMenuItem(1, 3);
-                cy.get('#wl-pattern-input').type('.*ining');
-                cy.get('#wordlist-form-mount .default-button').click().then(() => {
+        // word list query
+        cy.hoverNthMenuItem(1);
+        cy.clickMenuItem(1, 3);
+        cy.url().should('contain', '/wordlist/form?');
+        cy.get('#wl-pattern-input').type('.*ining');
+        cy.get('#wordlist-form-mount .default-button').click();
+        cy.url().should('contain', '/wordlist/result?');
                     
-                    cy.wait(1000);
-                });
-            });
-        });
-
+        // keywords
+        cy.hoverNthMenuItem(1);
+        cy.clickMenuItem(1, 4);
+        cy.url().should('contain', '/keywords/form?');
+        cy.get('#kw-pattern').clear().type('.*ining');
+        cy.get('#keywords-form-mount .default-button').click();
+        cy.url().should('contain', '/keywords/result?');
+        
         cy.actionLogout();
     });
 
@@ -60,6 +69,7 @@ describe('Query History', () => {
         history.should('contain.text', 'concordance');
         history.should('contain.text', 'word list');
         history.should('contain.text', 'paradigmatic query');
+        history.should('contain.text', 'keywords');
 
         // test concordance supertype
         cy.get('#query-history-mount fieldset.basic select').select('concordance');
@@ -67,6 +77,7 @@ describe('Query History', () => {
         history.should('contain.text', 'concordance');
         history.should('not.contain.text', 'paradigmatic query');
         history.should('not.contain.text', 'word list');
+        history.should('not.contain.text', 'keywords');
 
         // test paradigmatic query supertype
         cy.get('#query-history-mount fieldset.basic select').select('paradigmatic query');
@@ -74,6 +85,7 @@ describe('Query History', () => {
         history.should('not.contain.text', 'concordance');
         history.should('contain.text', 'paradigmatic query');
         history.should('not.contain.text', 'word list');
+        history.should('not.contain.text', 'keywords');
 
         // test word list supertype
         cy.get('#query-history-mount fieldset.basic select').select('word list');
@@ -81,6 +93,15 @@ describe('Query History', () => {
         history.should('not.contain.text', 'concordance');
         history.should('not.contain.text', 'paradigmatic query');
         history.should('contain.text', 'word list');
+        history.should('not.contain.text', 'keywords');
+
+        // test keywords supertype
+        cy.get('#query-history-mount fieldset.basic select').select('keywords');
+        history = cy.get('#query-history-mount .history-entries .supertype');
+        history.should('not.contain.text', 'concordance');
+        history.should('not.contain.text', 'paradigmatic query');
+        history.should('not.contain.text', 'word list');
+        history.should('contain.text', 'keywords');
 
         // need to close history widget, so logout can be clicked
         cy.get('#query-history-mount img.close-icon').click();
