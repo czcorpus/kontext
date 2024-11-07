62b2dd76124637c610c2d3e8a116ae56ecd1a87f
Martin Zimandl <martin.zimandl@gmail.com>
2024-11-07 11:41:40 +0100

    testing concordance detail search

--- cypress/e2e/queryHistory.cy.js
+++ cypress/e2e/queryHistory.cy.js
@@ -3,10 +3,7 @@
 
 describe('Query History', () => {
 
-    // fill in some history items
-    before(() => {
-        cy.actionLogin();
-
+    const createTestSubcorpus = () => {
         cy.get('#query-form-mount')
         .then($mount => {
             let options = $mount.find('#subcorp-selector option').filter(':contains("test-subc")');
@@ -23,6 +20,12 @@ describe('Query History', () => {
                 cy.get('#subcorp-form-mount p.submit-buttons').contains('button', 'Create subcorpus').click();     
             }
         });
+    }
+
+    // fill in some history items
+    before(() => {
+        cy.actionLogin();
+        createTestSubcorpus();
 
         // concordance query
         cy.hoverNthMenuItem(1);
@@ -33,11 +36,23 @@ describe('Query History', () => {
         cy.get('.query .default-button').click();
         cy.url().should('contain', '/view?');
 
+        // another concordance query
+        cy.hoverNthMenuItem(1);
+        cy.clickMenuItem(1, 1);
+        cy.url().should('contain', '/query?');
+        cy.get('#subcorp-selector').select('--whole corpus--');
+        cy.get('.simple-input').type('the');
+        cy.get('.DefaultAttrSelect').select("lemma");
+        cy.get('#section-specify-text-types button.ExpandButton').click();
+        cy.get('.specify-text-types div.grid > div input[type=checkbox]').check("1");
+        cy.get('.specify-text-types div.grid > div input[type=checkbox]').check("maj");
+        cy.get('.query .default-button').click();
+        cy.url().should('contain', '/view?');
+
         // paradigmatic query
         cy.hoverNthMenuItem(1);
         cy.clickMenuItem(1, 2);
         cy.url().should('contain', '/pquery/index?');
-        cy.get('#subcorp-selector').select('--whole corpus--');
         cy.get('#pquery-form-mount .cql-input').eq(0).type('[word="e.*"]');
         cy.get('#pquery-form-mount .cql-input').eq(1).type('[tag="N.*"]');
         cy.get('#pquery-form-mount .submit').click();
@@ -249,7 +264,7 @@ describe('Query History', () => {
         cy.get('.query .default-button').click();
         cy.get('.query .default-button').should('be.visible');
 
-        cy.wait(5000); // wait for saving query to index
+        cy.wait(5000); // wait for saving queries to index
 
         // open history
         cy.openHistory();
@@ -315,13 +330,63 @@ describe('Query History', () => {
         cy.get('#query-history-mount input').eq(2).type('test-subc');
         cy.get('#query-history-mount').contains('button', 'Search').click();
 
-        // test concordance supertype
-        let history = cy.get('#query-history-mount .history-entries .supertype');
-        history.should('not.be.empty');
-        history.should('contain.text', 'concordance');
-        history.should('not.contain.text', 'word list');
-        history.should('not.contain.text', 'paradigmatic query');
-        history.should('not.contain.text', 'keywords');
+        // test concordance subcorpus
+        cy.get('#query-history-mount .history-entries .heading').each($item => {
+            cy.wrap($item).should('contain.text', 'test-subc');
+        });
+    });
+
+    it('tests concordance detailed search in extended search', () => {
+        cy.get('#query-history-mount').contains('button', 'Extended search').click();
+        cy.get('#query-history-mount select').first().select('concordance');
+        cy.get('#query-history-mount #searchHistory_QueryCQLProps').check();
+        cy.get('#query-history-mount input').eq(5).clear();
+        cy.get('#query-history-mount input').eq(4).type('London');
+        cy.get('#query-history-mount').contains('button', 'Search').click();
+        cy.get('#query-history-mount .history-entries li span.query').each($item => {
+            cy.wrap($item).should('contain.text', 'London');
+        });
+
+        cy.get('#query-history-mount input').eq(4).clear().type('lon');
+        cy.get('#query-history-mount').contains('button', 'Search').click();
+        cy.get('#query-history-mount').should('contain.text', 'No data found.');
+
+        cy.get('#query-history-mount select').eq(1).select('Token/position (substring)');
+        cy.get('#query-history-mount').contains('button', 'Search').click();
+        cy.get('#query-history-mount .history-entries li span.query').each($item => {
+            cy.wrap($item).should('contain.text', 'London');
+        });
+
+        cy.get('#query-history-mount input').eq(4).clear();
+        cy.get('#query-history-mount input').eq(5).clear().type('word');
+        cy.get('#query-history-mount').contains('button', 'Search').click();
+        cy.get('#query-history-mount .history-entries li span.query').each($item => {
+            cy.wrap($item).should('contain.text', 'London');
+        });
+
+        cy.get('#query-history-mount input').eq(4).clear();
+        cy.get('#query-history-mount input').eq(5).clear().type('lemma');
+        cy.get('#query-history-mount').contains('button', 'Search').click();
+        cy.get('#query-history-mount .history-entries li span.query').each($item => {
+            cy.wrap($item).should('contain.text', 'the');
+        });
+
+        cy.get('#query-history-mount input').eq(5).clear();
+        cy.get('#query-history-mount input').eq(6).type('maj');
+        cy.get('#query-history-mount').contains('button', 'Search').click();
+        cy.get('#query-history-mount .history-entries li span.query').each($item => {
+            cy.wrap($item).should('contain.text', 'the');
+        });
+
+        cy.get('#query-history-mount input').eq(6).clear().type('m');
+        cy.get('#query-history-mount').contains('button', 'Search').click();
+        cy.get('#query-history-mount').should('contain.text', 'No data found.');
+
+        cy.get('#query-history-mount select').eq(2).select('Text type (substring)');
+        cy.get('#query-history-mount').contains('button', 'Search').click();
+        cy.get('#query-history-mount .history-entries li span.query').each($item => {
+            cy.wrap($item).should('contain.text', 'the');
+        });
     });
 
 });
