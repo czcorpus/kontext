czcorpus/kontext

commit d40eb6d940ac72e773e4beda7574f087f7f480ac

conc download uses rq notification task to show backend error messages

public/files/js/models/wordlist/save.ts
@@ -271,7 +271,7 @@ export class WordlistSaveModel extends StatelessModel<WordlistSaveModelState> {
         this.saveLinkFn(
             undefined,
             state.saveFormat,
-            this.layoutModel.createActionUrl('wordlist/savewl', submitArgs),
+            taskId => this.layoutModel.createActionUrl('wordlist/savewl', {task_id: taskId, submitArgs}),
         );
         // TODO
         return rxOf({});

public/files/js/models/coll/save.ts
@@ -206,7 +206,7 @@ export class CollResultsSaveModel extends StatelessModel<CollResultsSaveModelSta
                 this.saveLinkFn(
                     undefined,
                     state.saveformat,
-                    this.layoutModel.createActionUrl('savecoll', args)
+                    taskId => this.layoutModel.createActionUrl('savecoll', {task_id: taskId, ...args}),
                 );
             }
         );

public/files/js/models/concordance/save.ts
@@ -212,7 +212,7 @@ export class ConcSaveModel extends StatefulModel<ConcSaveModelState> {
         this.saveLinkFn(
             undefined,
             this.state.saveformat,
-            this.layoutModel.createActionUrl('saveconc', args)
+            taskId => this.layoutModel.createActionUrl('saveconc', {task_id: taskId, ...args}),
         );
     }
 }

public/files/js/models/freqs/regular/save.ts
@@ -204,7 +204,7 @@ export class FreqResultsSaveModel extends StatefulModel<FreqResultsSaveModelStat
         this.saveLinkFn(
             undefined,
             this.state.saveformat,
-            this.layoutModel.createActionUrl('savefreq', args)
+            taskId => this.layoutModel.createActionUrl('savefreq', {task_id: taskId, ...args}),
         );
     }
 }

public/files/js/models/keywords/save.ts
@@ -189,7 +189,7 @@ export class KeywordsResultsSaveModel extends StatefulModel<KeywordsResultsSaveM
         this.saveLinkFn(
             undefined,
             this.state.saveformat,
-            this.layoutModel.createActionUrl(
+            taskId => this.layoutModel.createActionUrl(
                 'keywords/download',
                 {
                     q: '~' + keywordsResultArgs.queryId,
@@ -197,8 +197,9 @@ export class KeywordsResultsSaveModel extends StatefulModel<KeywordsResultsSaveM
                     colheaders: this.state.includeColHeaders,
                     heading: this.state.includeHeading,
                     from_line: this.state.fromLine.value,
-                    to_line: isNaN(parseInt(this.state.toLine.value)) ? '' : this.state.toLine.value
-                }
+                    to_line: isNaN(parseInt(this.state.toLine.value)) ? '' : this.state.toLine.value,
+                    task_id: taskId,
+                },
             )
         );
     }

public/files/js/models/pquery/save.ts
@@ -189,7 +189,7 @@ export class PqueryResultsSaveModel extends StatefulModel<PqueryResultsSaveModel
         this.saveLinkFn(
             undefined,
             this.state.saveformat,
-            this.layoutModel.createActionUrl(
+            taskId => this.layoutModel.createActionUrl(
                 'pquery/download',
                 {
                     q: '~' + pqueryResultArgs.queryId,
@@ -199,7 +199,8 @@ export class PqueryResultsSaveModel extends StatefulModel<PqueryResultsSaveModel
                     colheaders: this.state.includeColHeaders,
                     heading: this.state.includeHeading,
                     from_line: this.state.fromLine.value,
-                    to_line: isNaN(parseInt(this.state.toLine.value)) ? '' : this.state.toLine.value
+                    to_line: isNaN(parseInt(this.state.toLine.value)) ? '' : this.state.toLine.value,
+                    task_id: taskId,
                 }
             )
         );

lib/action/control.py
@@ -28,7 +28,7 @@ import settings
 from action.argmapping.action import create_mapped_args
 from action.errors import (
     AlignedCorpusForbiddenException, CorpusForbiddenException,
-    ForbiddenException, ImmediateRedirectException, UserReadableException)
+    ForbiddenException, ImmediateRedirectException, UnavailableForLegalReasons, UserReadableException)
 from action.krequest import KRequest
 from action.model import ModelsSharedData
 from action.model.abstract import AbstractPageModel, AbstractUserModel

lib/bgcalc/adapter/abstract.py
@@ -39,7 +39,7 @@ class AbstractBgClient(abc.ABC):
 
     @abc.abstractmethod
     async def send_task(
-            self, name, ans_type: Type[T], args=None, time_limit=None, soft_time_limit=None) -> AbstractResultWrapper:
+            self, name, ans_type: Type[T], args=None, time_limit=None, soft_time_limit=None, task_id=None) -> AbstractResultWrapper:
         pass
 
     @abc.abstractmethod

commit 68d430908b2a4f5cdac90899c1cd2544571aa505

Fix incorrect args spread (caused by rev d40eb6d940)

public/files/js/models/wordlist/save.ts
@@ -271,7 +271,7 @@ export class WordlistSaveModel extends StatelessModel<WordlistSaveModelState> {
         this.saveLinkFn(
             undefined,
             state.saveFormat,
-            taskId => this.layoutModel.createActionUrl('wordlist/savewl', {task_id: taskId, submitArgs}),
+            taskId => this.layoutModel.createActionUrl('wordlist/savewl', {task_id: taskId, ...submitArgs}),
         );
         // TODO
         return rxOf({});