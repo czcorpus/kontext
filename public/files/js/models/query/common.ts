d6ebba1e5f8fed87a4f7a2db8ff88ac88a67e188
Tomas Machalek <tomas@machalek.xyz>
2026-09-10 17:23:51 +0200

    Add a comment explaining the logic behind the rev c43958d5f0ac70 fix

--- public/files/js/models/query/common.ts
+++ public/files/js/models/query/common.ts
@@ -934,6 +934,14 @@ export abstract class QueryFormModel<T extends QueryFormModelState> extends Stat
                     {
                         formType: action.payload?.formType,
                         sourceId: action.payload?.sourceId,
+                        // The condition below tries to handle otherwise hard to
+                        // solve issue with the contenteditable PRE area adding new lines
+                        // differently when no previous newline was inserted vs. there was
+                        // none.
+                        // Just inserting a single newline char would lead to cursor not
+                        // jumping to the next line when shift+ENTER is first hit.
+                        // So we model the behavior and set it differently for the first
+                        // time (or after when the query was completely deleted).
                         chunk: prevTouched ? '\n' : '\n\n'
                     }
                 );
